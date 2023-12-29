7df6fbe145f729d1f020c8438226277bc5680b01
abelanger5 <belanger@sas.upenn.edu>
2023-12-29 14:35:20 -0500

    feat: add initial docs site (#14)
    * feat: add initial docs site
    
    * docs: create initial documentation and update readme

---
+++ frontend/docs/theme.config.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
+
+const config: DocsThemeConfig = {
+  logo: (
+    <Image src="/hatchet_logo.png" alt="Hatchet logo" width={120} height={35} />
+  ),
+  primaryHue: 210,
+  primarySaturation: 10,
+  project: {
+    link: "https://github.com/hatchet-dev/hatchet",
+  },
+  chat: {
+    link: "https://discord.gg/ZMeUafwH89",
+  },
+  docsRepositoryBase: "https://github.com/hatchet-dev/hatchet/frontend/docs",
+  footer: {
+    text: "Hatchet",
+  },
+  head: (
+    <>
+      <link rel="icon" type="image/png" href="/favicon.ico" />
+      <title>Hatchet Documentation</title>{" "}
+    </>
+  ),
+};
+
+export default config;
