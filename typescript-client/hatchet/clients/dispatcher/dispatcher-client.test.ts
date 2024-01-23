c4668545fce752c5c284d21f3c8e0cd10f63515c
g <gabriel.ruttner@gmail.com>
2024-01-23 14:45:29 -0700

    feat: dispatcher client

--- typescript-client/hatchet/clients/dispatcher/dispatcher-client.test.ts
+++ typescript-client/hatchet/clients/dispatcher/dispatcher-client.test.ts
@@ -1,1 +1,102 @@
-describe('DispatcherClient', () => {});
+import { ActionEventType } from '@protoc/dispatcher';
+import { DispatcherClient } from './dispatcher-client';
+
+let client: DispatcherClient;
+
+describe('DispatcherClient', () => {
+  it('should create a client', () => {
+    const x = new DispatcherClient({
+      tenant_id: 'TENANT_ID',
+      host_port: 'HOST_PORT',
+      tls_config: {
+        cert_file: 'TLS_CERT_FILE',
+        key_file: 'TLS_KEY_FILE',
+        ca_file: 'TLS_ROOT_CA_FILE',
+        server_name: 'TLS_SERVER_NAME',
+      },
+    });
+
+    expect(x).toBeDefined();
+  });
+
+  beforeEach(() => {
+    client = new DispatcherClient({
+      tenant_id: 'TENANT_ID',
+      host_port: 'HOST_PORT',
+      tls_config: {
+        cert_file: 'TLS_CERT_FILE',
+        key_file: 'TLS_KEY_FILE',
+        ca_file: 'TLS_ROOT_CA_FILE',
+        server_name: 'TLS_SERVER_NAME',
+      },
+    });
+  });
+
+  describe('get_action_listener', () => {
+    it('should register the worker', async () => {
+      const clientSpy = jest.spyOn(client.client, 'register').mockResolvedValue({
+        workerId: 'WORKER_ID',
+        tenantId: 'TENANT_ID',
+        workerName: 'WORKER_NAME',
+      });
+
+      const listenerSpy = jest.spyOn(client.client, 'listen');
+
+      const listener = await client.get_action_listener({
+        workerName: 'WORKER_NAME',
+        services: ['SERVICE'],
+        actions: ['ACTION'],
+      });
+
+      expect(clientSpy).toHaveBeenCalledWith({
+        tenantId: 'TENANT_ID',
+        workerName: 'WORKER_NAME',
+        services: ['SERVICE'],
+        actions: ['ACTION'],
+      });
+
+      expect(listenerSpy).toHaveBeenCalledWith({
+        tenantId: 'TENANT_ID',
+        workerId: 'WORKER_ID',
+      });
+
+      expect(listener).toBeDefined();
+      expect(listener.workerId).toEqual('WORKER_ID');
+    });
+  });
+
+  describe('send_action_event', () => {
+    it('should send action events', () => {
+      const clientSpy = jest.spyOn(client.client, 'sendActionEvent').mockResolvedValue({
+        tenantId: 'TENANT_ID',
+        workerId: 'WORKER_ID',
+      });
+
+      client.send_action_event({
+        tenantId: 'TENANT_ID',
+        workerId: 'WORKER_ID',
+        actionId: 'ACTION_ID',
+        eventType: ActionEventType.STEP_EVENT_TYPE_COMPLETED,
+        eventPayload: '{"foo":"bar"}',
+        eventTimestamp: new Date(),
+        jobId: 'a',
+        jobRunId: 'b',
+        stepId: 'c',
+        stepRunId: 'd',
+      });
+
+      expect(clientSpy).toHaveBeenCalledWith({
+        tenantId: 'TENANT_ID',
+        workerId: 'WORKER_ID',
+        actionId: 'ACTION_ID',
+        eventType: ActionEventType.STEP_EVENT_TYPE_COMPLETED,
+        eventPayload: '{"foo":"bar"}',
+        jobId: 'a',
+        jobRunId: 'b',
+        stepId: 'c',
+        stepRunId: 'd',
+        eventTimestamp: expect.any(Date),
+      });
+    });
+  });
+});
--- typescript-client/hatchet/clients/dispatcher/dispatcher-client.ts
+++ typescript-client/hatchet/clients/dispatcher/dispatcher-client.ts
@@ -1,9 +1,55 @@
-import { ClientConfig } from '../client/client-config';
+import { createChannel, createClient } from 'nice-grpc';
+import {
+  DispatcherClient as PbDispatcherClient,
+  DispatcherDefinition,
+  ActionEvent,
+} from '@protoc/dispatcher';
+import { ClientConfig } from '@clients/client/client-config';
+import HatchetError from '@util/errors/hatchet-error';
+import { ActionListener } from './action-listener';
+
+interface GetActionListenerOptions {
+  workerName: string;
+  services: string[];
+  actions: string[];
+}
 
 export class DispatcherClient {
   config: ClientConfig;
+  client: PbDispatcherClient;
 
   constructor(config: ClientConfig) {
-    throw new Error('not implemented');
+    this.config = config;
+    this.client = createClient(
+      DispatcherDefinition,
+      createChannel(
+        config.host_port
+        // FIXME: Credentials ChannelCredentials.createSsl(config.tls_config.)
+      )
+    );
+  }
+
+  async get_action_listener(options: GetActionListenerOptions) {
+    // Register the worker
+    const registration = await this.client.register({
+      tenantId: this.config.tenant_id,
+      ...options,
+    });
+
+    // Subscribe to the worker
+    const listener = this.client.listen({
+      tenantId: this.config.tenant_id,
+      workerId: registration.workerId,
+    });
+
+    return new ActionListener(this, listener, registration.workerId);
+  }
+
+  async send_action_event(in_: ActionEvent) {
+    try {
+      return this.client.sendActionEvent(in_);
+    } catch (e: any) {
+      throw new HatchetError(e.message);
+    }
   }
 }
