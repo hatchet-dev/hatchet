f72bb85c58b2d1e920fbea83d564ec186629e515
g <gabriel.ruttner@gmail.com>
2024-01-24 07:06:42 -0700

    feat: admin client

--- typescript-client/hatchet/clients/admin/admin-client.test.ts
+++ typescript-client/hatchet/clients/admin/admin-client.test.ts
@@ -1,1 +1,279 @@
-describe('AdminClient', () => {});
+import { CreateWorkflowVersionOpts, Workflow, WorkflowVersion } from '@protoc/workflows';
+import { ServerError, Status } from 'nice-grpc-common';
+import { AdminClient } from './admin-client';
+
+describe('AdminClient', () => {
+  let client: AdminClient;
+
+  it('should create a client', () => {
+    const x = new AdminClient({
+      tenant_id: 'TENANT_ID',
+      host_port: 'HOST_PORT',
+      tls_config: {
+        cert_file: 'TLS_CERT_FILE',
+        key_file: 'TLS_KEY_FILE',
+        ca_file: 'TLS_ROOT_CA_FILE',
+        server_name: 'TLS_SERVER_NAME',
+      },
+    });
+
+    expect(x).toBeDefined();
+  });
+
+  beforeEach(() => {
+    client = new AdminClient({
+      tenant_id: 'TENANT_ID',
+      host_port: 'HOST_PORT',
+      tls_config: {
+        cert_file: 'TLS_CERT_FILE',
+        key_file: 'TLS_KEY_FILE',
+        ca_file: 'TLS_ROOT_CA_FILE',
+        server_name: 'TLS_SERVER_NAME',
+      },
+    });
+  });
+
+  describe('should_put', () => {
+    let workflow: CreateWorkflowVersionOpts;
+
+    beforeAll(() => {
+      workflow = {
+        name: 'workflow1',
+        version: '',
+        description: 'description1',
+        eventTriggers: [],
+        cronTriggers: [],
+        scheduledTriggers: [],
+        jobs: [],
+      };
+    });
+
+    it('shouldPut:F and vX; if auto:F', () => {
+      const existing: any = {
+        versions: [
+          {
+            version: 'v0.0.1',
+          },
+        ],
+      };
+      const [shouldPut, version] = AdminClient.should_put(
+        {
+          ...workflow,
+          version: 'vX',
+        },
+        existing,
+        {
+          autoVersion: false,
+        }
+      );
+      expect(shouldPut).toEqual(false);
+      expect(version).toEqual('vX');
+    });
+
+    it('shouldPut:T and v0.0.1 if auto:T, no version, and no Existing', () => {
+      const existing: any = undefined;
+      const [shouldPut, version] = AdminClient.should_put(
+        {
+          ...workflow,
+          version: '',
+        },
+        existing,
+        {
+          autoVersion: true,
+        }
+      );
+      expect(shouldPut).toEqual(true);
+      expect(version).toEqual('v0.1.0');
+    });
+
+    it('shouldPut:T and bump version if auto:T, no version, and has Existing', () => {
+      const existing: any = {
+        versions: [
+          {
+            version: 'v0.0.1',
+          },
+        ],
+      };
+      const [shouldPut, version] = AdminClient.should_put(
+        {
+          ...workflow,
+          version: '',
+        },
+        existing,
+        {
+          autoVersion: true,
+        }
+      );
+
+      expect(shouldPut).toEqual(true);
+      expect(version).toEqual('v0.1.0');
+    });
+
+    it('shouldPut:T and keep existing version if auto:T, with a version, and no Existing', () => {
+      const existing: any = {
+        versions: [],
+      };
+      const [shouldPut, version] = AdminClient.should_put(
+        {
+          ...workflow,
+          version: 'vKEEP',
+        },
+        existing,
+        {
+          autoVersion: true,
+        }
+      );
+
+      expect(shouldPut).toEqual(true);
+      expect(version).toEqual('vKEEP');
+    });
+  });
+
+  describe('put_workflow', () => {
+    it('should throw an error if no version and not auto version', async () => {
+      const workflow: CreateWorkflowVersionOpts = {
+        name: 'workflow1',
+        version: '',
+        description: 'description1',
+        eventTriggers: [],
+        cronTriggers: [],
+        scheduledTriggers: [],
+        jobs: [],
+      };
+
+      expect(() => client.put_workflow(workflow, { autoVersion: false })).rejects.toThrow(
+        'PutWorkflow error: workflow version is required, or use with_auto_version'
+      );
+    });
+
+    it('should check if an existing workflow exists, if not it should put', async () => {
+      const workflow: CreateWorkflowVersionOpts = {
+        name: 'workflow1',
+        version: 'v0.0.1',
+        description: 'description1',
+        eventTriggers: [],
+        cronTriggers: [],
+        scheduledTriggers: [],
+        jobs: [],
+      };
+
+      const existingSpy = jest
+        .spyOn(client.client, 'getWorkflowByName')
+        .mockRejectedValue(new ServerError(Status.NOT_FOUND, 'not found'));
+
+      const shouldPutSpy = jest.spyOn(AdminClient, 'should_put').mockReturnValue([true, 'v0.1.0']);
+
+      const putSpy = jest.spyOn(client.client, 'putWorkflow').mockResolvedValue({
+        id: 'workflow1',
+        version: 'v0.1.0',
+        order: 1,
+        workflowId: 'workflow1',
+        jobs: [],
+        createdAt: undefined,
+        updatedAt: undefined,
+        triggers: undefined,
+      });
+
+      await client.put_workflow(workflow);
+
+      expect(existingSpy).toHaveBeenCalledWith({
+        tenantId: 'TENANT_ID',
+        name: 'workflow1',
+      });
+
+      expect(shouldPutSpy).toHaveBeenCalledWith(workflow, undefined, undefined);
+
+      expect(putSpy).toHaveBeenCalled();
+    });
+
+    it('should check if an existing workflow exists, if not it should put', async () => {
+      const workflow: CreateWorkflowVersionOpts = {
+        name: 'workflow1',
+        version: 'v0.0.1',
+        description: 'description1',
+        eventTriggers: [],
+        cronTriggers: [],
+        scheduledTriggers: [],
+        jobs: [],
+      };
+
+      const mockExisting: Workflow = {
+        id: 'workflow1',
+        name: 'workflow1',
+        tenantId: 'TENANT_ID',
+        description: 'description1',
+        versions: [
+          {
+            id: 'workflow1',
+            version: 'v0.0.1',
+            order: 1,
+            workflowId: 'workflow1',
+            jobs: [],
+            createdAt: undefined,
+            updatedAt: undefined,
+            triggers: undefined,
+          },
+        ],
+        createdAt: undefined,
+        updatedAt: undefined,
+      };
+
+      const existingSpy = jest
+        .spyOn(client.client, 'getWorkflowByName')
+        .mockResolvedValue(mockExisting);
+
+      const shouldPutSpy = jest.spyOn(AdminClient, 'should_put').mockReturnValue([false, 'v0.2.0']);
+
+      const putSpy = jest.spyOn(client.client, 'putWorkflow').mockResolvedValue({
+        id: 'workflow1',
+        version: 'v0.2.0',
+        order: 1,
+        workflowId: 'workflow1',
+        jobs: [],
+        createdAt: undefined,
+        updatedAt: undefined,
+        triggers: undefined,
+      });
+
+      await client.put_workflow(workflow, { autoVersion: false });
+
+      expect(existingSpy).toHaveBeenCalledWith({
+        tenantId: 'TENANT_ID',
+        name: 'workflow1',
+      });
+
+      expect(shouldPutSpy).toHaveBeenCalledWith(workflow, mockExisting, { autoVersion: false });
+
+      expect(putSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('schedule_workflow', () => {
+    it('should schedule a workflow', () => {
+      const res: WorkflowVersion = {
+        id: 'string',
+        version: 'v0.0.1',
+        order: 1,
+        workflowId: 'string',
+        jobs: [],
+        createdAt: undefined,
+        updatedAt: undefined,
+        triggers: undefined,
+      };
+
+      const spy = jest.spyOn(client.client, 'scheduleWorkflow').mockResolvedValue(res);
+
+      const now = new Date();
+
+      client.schedule_workflow('workflowId', {
+        schedules: [now],
+      });
+
+      expect(spy).toHaveBeenCalledWith({
+        tenantId: 'TENANT_ID',
+        workflowId: 'workflowId',
+        schedules: [now],
+      });
+    });
+  });
+});
--- typescript-client/hatchet/clients/admin/admin-client.ts
+++ typescript-client/hatchet/clients/admin/admin-client.ts
@@ -1,18 +1,100 @@
-import { Workflow } from '../../workflow';
-import { ClientConfig } from '../client/client-config';
+import { ServerError, Status, createChannel, createClient } from 'nice-grpc';
+import {
+  CreateWorkflowVersionOpts,
+  Workflow,
+  WorkflowServiceClient,
+  WorkflowServiceDefinition,
+} from '@protoc/workflows';
+import HatchetError from '@util/errors/hatchet-error';
+import { ClientConfig } from '@clients/client/client-config';
+import * as semver from '@util/semver/semver';
 
 export class AdminClient {
   config: ClientConfig;
+  client: WorkflowServiceClient;
 
   constructor(config: ClientConfig) {
     this.config = config;
+    this.client = createClient(
+      WorkflowServiceDefinition,
+      createChannel(
+        config.host_port
+        // FIXME: Credentials ChannelCredentials.createSsl(config.tls_config.)
+      )
+    );
   }
 
-  put_workflow(workflow: Workflow, options?: { autoVersion?: boolean }) {
-    throw new Error('not implemented');
+  static should_put(
+    workflow: CreateWorkflowVersionOpts,
+    existing?: Workflow,
+    options?: { autoVersion?: boolean }
+  ): [boolean, string] {
+    // TODO verify this function before merge #122
+    if (!options?.autoVersion) {
+      return [false, workflow.version];
+    }
+
+    if (workflow.version === '') {
+      return [true, 'v0.1.0'];
+    }
+
+    if (existing && existing.versions.length > 0) {
+      const newVersion = semver.bumpMinorVersion(existing.versions[0].version);
+      const shouldPut = newVersion !== workflow.version;
+      return [shouldPut, newVersion];
+    }
+
+    return [true, workflow.version];
   }
 
-  schedule_workflow(workflow: Workflow, options?: { schedules?: any[] }) {
-    throw new Error('not implemented');
+  async put_workflow(workflow: CreateWorkflowVersionOpts, options?: { autoVersion?: boolean }) {
+    if (workflow.version === '' && !options?.autoVersion) {
+      throw new HatchetError(
+        'PutWorkflow error: workflow version is required, or use with_auto_version'
+      );
+    }
+
+    let existing: Workflow | undefined;
+
+    try {
+      existing = await this.client.getWorkflowByName({
+        tenantId: this.config.tenant_id,
+        name: workflow.name,
+      });
+    } catch (e: any) {
+      if (e instanceof ServerError && e.code === Status.NOT_FOUND) {
+        existing = undefined;
+      } else {
+        throw new HatchetError(e.message);
+      }
+    }
+
+    const [shouldPut, version] = AdminClient.should_put(workflow, existing, options);
+
+    // eslint-disable-next-line no-param-reassign
+    workflow.version = version;
+
+    if (!shouldPut) return;
+
+    try {
+      await this.client.putWorkflow({
+        tenantId: this.config.tenant_id,
+        opts: workflow,
+      });
+    } catch (e: any) {
+      throw new HatchetError(e.message);
+    }
+  }
+
+  schedule_workflow(workflowId: string, options?: { schedules?: Date[] }) {
+    try {
+      this.client.scheduleWorkflow({
+        tenantId: this.config.tenant_id,
+        workflowId,
+        schedules: options?.schedules,
+      });
+    } catch (e: any) {
+      throw new HatchetError(e.message);
+    }
   }
 }
