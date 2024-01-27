90e5b7c52ec4def7a3199558c0fb1c3276def82f
g <gabriel.ruttner@gmail.com>
2024-01-27 06:51:41 -0700

    feat: init fastapi, uvicorn, poetry

---
+++ python-examples/fast-api/src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+def start():
+    """Launched with `poetry run start` at root level"""
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
