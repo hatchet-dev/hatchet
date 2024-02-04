338e79a96124b70dd00bc41cb902cda4d69cd7b1
g <gabriel.ruttner@gmail.com>
2024-02-04 17:33:36 -0500

    feat: add worker to index

--- typescript-sdk/src/index.ts
+++ typescript-sdk/src/index.ts
@@ -2,5 +2,6 @@ import { HatchetClient as Hatchet } from '@clients/hatchet-client';
 
 export * from './workflow';
 export * from './step';
+export * from './clients/worker';
 
 export default Hatchet;
