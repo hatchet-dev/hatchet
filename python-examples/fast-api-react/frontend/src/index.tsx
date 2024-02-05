f52f960f54cac4b69a2757230f059fd6ea76a77b
g <gabriel.ruttner@gmail.com>
2024-02-05 11:35:29 -0500

    feat: react-ts

--- python-examples/fast-api-react/frontend/src/index.tsx
+++ python-examples/fast-api-react/frontend/src/index.tsx
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
