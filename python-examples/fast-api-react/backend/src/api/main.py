7e05584722453a4f397c52ea59c5f4b5a2efe817
g <gabriel.ruttner@gmail.com>
2024-02-05 11:32:28 -0500

    chore: restructure example, init react

--- python-examples/fast-api-react/backend/src/api/main.py
+++ python-examples/fast-api-react/backend/src/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from hatchet_sdk import new_client
 import uvicorn
 from dotenv import load_dotenv
@@ -7,6 +8,21 @@ load_dotenv()
 
 app = FastAPI()
 hatchet = new_client()
+
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
