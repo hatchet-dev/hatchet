80b502e9abb862c418ccaa4de7c387688f0c071d
Alexander Belanger <belanger@sas.upenn.edu>
2024-02-16 12:58:30 -0500

    chore: remove ref to temporal

--- examples/deprecated/yaml/main.go
+++ examples/deprecated/yaml/main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"time"
 
+	"github.com/joho/godotenv"
+
 	"github.com/hatchet-dev/hatchet/pkg/client"
 	"github.com/hatchet-dev/hatchet/pkg/cmdutils"
 	"github.com/hatchet-dev/hatchet/pkg/worker"
-	"github.com/joho/godotenv"
 )
 
 type userCreateEvent struct {
@@ -49,8 +50,6 @@ func main() {
 		panic(err)
 	}
 
-	// Create a worker. This automatically reads in a TemporalClient from .env and workflow files from the .hatchet
-	// directory, but this can be customized with the `worker.WithTemporalClient` and `worker.WithWorkflowFiles` options.
 	worker, err := worker.NewWorker(
 		worker.WithClient(
 			client,
