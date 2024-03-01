64ea366a0870038fe3affa4f29d9e1d06dfd8ed8
steebchen <contact@luca-steeb.com>
2024-03-01 23:45:42 +0700

    zerolog changein init

--- internal/logger/stderr.go
+++ internal/logger/stderr.go
@@ -7,14 +7,19 @@ import (
 	"github.com/rs/zerolog"
 
 	"github.com/hatchet-dev/hatchet/internal/config/shared"
+
+	"time"
 )
+
+func init() {
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+}
 
 func NewDefaultLogger(service string) zerolog.Logger {
 	return NewStdErr(&shared.LoggerConfigFile{}, service)
 }
 
 func NewStdErr(cf *shared.LoggerConfigFile, service string) zerolog.Logger {
-	// zerolog.TimeFieldFormat = time.RFC3339Nano
 	lvl := zerolog.DebugLevel
 	var err error
 
