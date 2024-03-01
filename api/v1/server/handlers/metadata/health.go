4bd1b6f2ec2ad7d1212aa4adc72eb02c5f041e97
Luca Steeb <contact@luca-steeb.com>
2024-03-01 23:48:10 +0700

    feat(api): add healthcheck routes (#191)

---
+++ api/v1/server/handlers/metadata/health.go
@@ -0,0 +1,25 @@
+package metadata
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
+)
+
+func (u *MetadataService) LivenessGet(ctx echo.Context, request gen.LivenessGetRequestObject) (gen.LivenessGetResponseObject, error) {
+	return gen.LivenessGet200Response{}, nil
+}
+
+func (u *MetadataService) ReadinessGet(ctx echo.Context, request gen.ReadinessGetRequestObject) (gen.ReadinessGetResponseObject, error) {
+	if !u.config.Repository.Health().IsHealthy() {
+		return nil, fmt.Errorf("repository is not healthy")
+	}
+
+	if !u.config.TaskQueue.IsReady() {
+		return nil, fmt.Errorf("task queue is not healthy")
+	}
+
+	return gen.ReadinessGet200Response{}, nil
+}
