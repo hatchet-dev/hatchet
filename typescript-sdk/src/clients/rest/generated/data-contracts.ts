85d89886d3a185d532c1c9aef1bf4bc4ca979bcc
abelanger5 <belanger@sas.upenn.edu>
2024-03-04 19:15:56 -0800

    feat: display concurrency settings on the UI (#236)
    * feat: display concurrency settings on the UI
    
    * feat: show triggering settings as well

--- typescript-sdk/src/clients/rest/generated/data-contracts.ts
+++ typescript-sdk/src/clients/rest/generated/data-contracts.ts
@@ -335,6 +335,18 @@ export interface Workflow {
   deployment?: WorkflowDeploymentConfig;
 }
 
+export interface WorkflowConcurrency {
+  /**
+   * The maximum number of concurrent workflow runs.
+   * @format int32
+   */
+  maxRuns: number;
+  /** The strategy to use when the concurrency limit is reached. */
+  limitStrategy: 'CANCEL_IN_PROGRESS' | 'DROP_NEWEST' | 'QUEUE_NEWEST' | 'GROUP_ROUND_ROBIN';
+  /** An action which gets the concurrency group for the WorkflowRun. */
+  getConcurrencyGroup: string;
+}
+
 export interface WorkflowDeploymentConfig {
   metadata: APIResourceMeta;
   /** The repository name. */
@@ -370,6 +382,7 @@ export interface WorkflowVersion {
   order: number;
   workflowId: string;
   workflow?: Workflow;
+  concurrency?: WorkflowConcurrency;
   triggers?: WorkflowTriggers;
   jobs?: Job[];
 }
