e2ecaa813275212e57998b298b954e5e4c97ec74
badex-ai <Badamasi.link@gmail.com>
2024-03-26 18:09:27 +0100

    added the clipboardjs package for copying text

--- frontend/app/src/components/ui/copy-to-clipboard.tsx
+++ frontend/app/src/components/ui/copy-to-clipboard.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect,useRef } from 'react';
 import { Button } from './button';
 import { CopyIcon } from '@radix-ui/react-icons';
 import { CheckIcon } from '@heroicons/react/24/outline';
 import { cn } from '@/lib/utils';
+import ClipboardJS from 'clipboard'
 
 type Props = {
   text: string;
@@ -13,6 +14,19 @@ type Props = {
 const CopyToClipboard: React.FC<Props> = ({ text, className, withText }) => {
   const [successCopy, setSuccessCopy] = useState(false);
 
+  const copyRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if(copyRef.current){
+      const clipboard   =  new ClipboardJS(copyRef.current)
+   
+      return () => {
+        clipboard.destroy();
+      };
+    }
+   
+  }, [])
+ 
   return (
     <Button
       className={cn(
@@ -21,9 +35,11 @@ const CopyToClipboard: React.FC<Props> = ({ text, className, withText }) => {
           ? 'cursor-pointer flex flex-row gap-2 items-center mt-2'
           : 'w-6 h-6 p-0 cursor-pointer',
       )}
+      ref={copyRef}
+      data-clipboard-text={text}
       variant={withText ? 'default' : 'ghost'}
       onClick={() => {
-        navigator.clipboard.writeText(text);
+        // navigator.clipboard.writeText(text);
         setSuccessCopy(true);
 
         setTimeout(() => {
