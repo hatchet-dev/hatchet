a71cae4fe7e7a165b867f77669654816da75f542
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-04-09 15:46:49 -0600

    fix: badge should be span

--- frontend/app/src/components/ui/badge.tsx
+++ frontend/app/src/components/ui/badge.tsx
@@ -34,7 +34,7 @@ export interface BadgeProps
 
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <span className={cn(badgeVariants({ variant }), className)} {...props} />
   );
 }
 
