76687c56538a68865b5e5db6b8a7c96c54e99203
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-04-15 10:55:43 -0600

    feat: light theme and theme toggle

--- frontend/app/src/pages/main/workflows/$workflow/components/step-node.tsx
+++ frontend/app/src/pages/main/workflows/$workflow/components/step-node.tsx
@@ -1,23 +1,31 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Step } from '@/lib/api';
+import { cn } from '@/lib/utils';
 import { memo } from 'react';
 import { Handle, Position } from 'reactflow';
+
+export interface StepNodeProps {
+  step: Step;
+  variant: 'default' | 'input_only' | 'output_only';
+}
 
 // eslint-disable-next-line react/display-name
 export default memo(
   ({
     data,
   }: {
-    data: {
-      step: Step;
-      variant: 'default' | 'input_only' | 'output_only';
-    };
+    data: StepNodeProps;
   }) => {
     const variant = data.variant;
 
     return (
-      <Card className="p-3 cursor-pointer bg-[#020817]">
+      <Card
+        className={cn(
+          'step-run-card p-3 cursor-pointer bg-[#020817] shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200',
+        )}
+      >
+        <span className="step-run-backdrop absolute inset-[1px] rounded-full bg-background transition-colors duration-200" />
         {(variant == 'default' || variant == 'input_only') && (
           <Handle
             type="target"
@@ -26,14 +34,14 @@ export default memo(
             isConnectable={false}
           />
         )}
-        <CardContent className="p-0">
-          <div className="font-bold text-sm">{data.step.readableId}</div>
-          <Label className="cursor-pointer">
-            <span className="font-bold mr-2 text-xs">Timeout</span>
-            <span className="text-gray-500 font-medium text-xs">
-              {data.step.timeout || '300s'}
-            </span>
-          </Label>
+        <CardContent className="p-0 z-10 bg-background">
+          <div className="flex flex-row justify-between gap-2 items-center">
+            <div className="font-bold text-sm">
+              {data.step?.readableId || data.step.metadata.id}
+            </div>
+          </div>
+          <div className="flex flex-col mt-1">
+          </div>
         </CardContent>
         {(variant == 'default' || variant == 'output_only') && (
           <Handle
@@ -45,5 +53,4 @@ export default memo(
         )}
       </Card>
     );
-  },
-);
+  });
