3c42fe32b2b726f9441e354aafa102674dbac903
steebchen <contact@luca-steeb.com>
2024-04-17 16:57:40 +0400

    chore(python-sdk): extract python sdk into its own repo

--- python-sdk/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Builds python auto-generated protobuf files
-
-poetry run python -m grpc_tools.protoc --proto_path=../api-contracts/dispatcher --python_out=./hatchet_sdk --pyi_out=./hatchet_sdk --grpc_python_out=./hatchet_sdk dispatcher.proto
-poetry run python -m grpc_tools.protoc --proto_path=../api-contracts/events --python_out=./hatchet_sdk --pyi_out=./hatchet_sdk --grpc_python_out=./hatchet_sdk events.proto
-poetry run python -m grpc_tools.protoc --proto_path=../api-contracts/workflows --python_out=./hatchet_sdk --pyi_out=./hatchet_sdk --grpc_python_out=./hatchet_sdk workflows.proto
