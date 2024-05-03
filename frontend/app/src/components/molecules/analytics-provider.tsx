0ff2768f094776bb2c4e8c49ec83cb4b9158996d
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-05-03 13:27:16 -0600

    feat: respect opt out

--- frontend/app/src/components/molecules/analytics-provider.tsx
+++ frontend/app/src/components/molecules/analytics-provider.tsx
@@ -12,6 +12,8 @@ const AnalyticsProvider: React.FC<
 > = ({ user, children }) => {
   const meta = useApiMeta();
 
+  const [loaded, setLoaded] = React.useState(false);
+
   const [tenant] = useTenantContext();
 
   const config = useMemo(() => {
@@ -19,10 +21,23 @@ const AnalyticsProvider: React.FC<
   }, [meta]);
 
   useEffect(() => {
-    if (!config) {
+    if (loaded) {
       return;
     }
 
+    if (tenant && tenant.analyticsOptOut) {
+      console.log(
+        'Skipping Analytics initialization due to opt-out, we respect user privacy.',
+      );
+      return;
+    }
+
+    if (!config || !tenant) {
+      return;
+    }
+
+    console.log('Initializing Analytics, opt out in settings.');
+    setLoaded(true);
     const posthogScript = `
 !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
 posthog.init('${config.apiKey}',{
@@ -31,7 +46,7 @@ posthog.init('${config.apiKey}',{
 `;
     document.head.appendChild(document.createElement('script')).innerHTML =
       posthogScript;
-  }, [config, user]);
+  }, [config, loaded, tenant]);
 
   useEffect(() => {
     if (!config || !user) {
