07c6ea3e61e1cb2fdbd20970d71ad0da5385ef1c
Alexander Belanger <belanger@sas.upenn.edu>
2024-05-14 15:29:34 -0400

    chore: update migration for release

--- prisma/migrations/20240514192527_v0_27_0/migration.sql
+++ prisma/migrations/20240514192527_v0_27_0/migration.sql
@@ -8,3 +8,7 @@
 
 ALTER TYPE "StepRunEventReason" ADD VALUE 'TIMED_OUT';
 ALTER TYPE "StepRunEventReason" ADD VALUE 'REASSIGNED';
+ALTER TYPE "StepRunEventReason" ADD VALUE 'SLOT_RELEASED';
+
+-- AlterTable
+ALTER TABLE "StepRun" ADD COLUMN     "semaphoreReleased" BOOLEAN NOT NULL DEFAULT false;
