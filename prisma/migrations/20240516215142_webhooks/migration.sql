b7da30f6110e0008e84e71d15fa2dc46f1861580
steebchen <contact@luca-steeb.com>
2024-05-24 19:27:19 +0100

    migrations wip

--- prisma/migrations/20240516215142_webhooks/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Tenant" ADD COLUMN     "webhookSecret" TEXT;
--- prisma/migrations/20240524192649_vMIGRATIONSWIP/migrations.sql
+++ prisma/migrations/20240524192649_vMIGRATIONSWIP/migrations.sql
@@ -3,3 +3,6 @@ ALTER TABLE "Worker" ADD COLUMN     "webhook" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
 ALTER TABLE "WorkflowVersion" ADD COLUMN     "webhook" TEXT;
+
+-- AlterTable
+ALTER TABLE "Tenant" ADD COLUMN     "webhookSecret" TEXT;
