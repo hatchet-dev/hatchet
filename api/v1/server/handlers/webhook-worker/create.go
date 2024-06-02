ef04e8e4ef77ed50564c6f4994cf278851381a76
steebchen <contact@luca-steeb.com>
2024-06-02 23:44:31 +0200

    implement webhook worker create

--- api/v1/server/handlers/webhook-worker/create.go
+++ api/v1/server/handlers/webhook-worker/create.go
@@ -1,12 +1,39 @@
 package webhookworker
 
 import (
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
+	"github.com/hatchet-dev/hatchet/internal/randstr"
+	"github.com/hatchet-dev/hatchet/internal/repository"
+	"github.com/hatchet-dev/hatchet/internal/repository/prisma/db"
 )
 
 func (i *WebhookWorkersService) WebhookCreate(ctx echo.Context, request gen.WebhookCreateRequestObject) (gen.WebhookCreateResponseObject, error) {
-	// TODO implement me
-	panic("implement me")
+	tenant := ctx.Get("tenant").(*db.TenantModel)
+
+	secret, err := randstr.GenerateWebhookSecret()
+	if err != nil {
+		return nil, err
+	}
+
+	ww, err := i.config.APIRepository.WebhookWorker().CreateWebhookWorker(ctx.Request().Context(), &repository.CreateWebhookWorkerOpts{
+		TenantId: tenant.ID,
+		URL:      request.Body.Url,
+		Secret:   secret,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return gen.WebhookCreate200JSONResponse{
+		Url:    ww.URL,
+		Secret: ww.Secret,
+		Metadata: gen.APIResourceMeta{
+			Id:        uuid.MustParse(ww.ID),
+			CreatedAt: ww.CreatedAt,
+			UpdatedAt: ww.UpdatedAt,
+		},
+	}, nil
 }
