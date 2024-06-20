7ca1bb591e1d36f297463e10d8019d14361771ff
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-06-20 12:29:20 -0500

    feat: prevent non-owner from changing plan

--- api/v1/server/handlers/billing/subscription.go
+++ api/v1/server/handlers/billing/subscription.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 
+	"github.com/hatchet-dev/hatchet/api/v1/server/oas/apierrors"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
 	"github.com/hatchet-dev/hatchet/pkg/billing"
 	"github.com/hatchet-dev/hatchet/pkg/repository/prisma/db"
@@ -18,6 +19,13 @@ func (b *BillingService) SubscriptionUpsert(ctx echo.Context, req gen.Subscripti
 	}
 
 	tenant := ctx.Get("tenant").(*db.TenantModel)
+	tenantMember := ctx.Get("tenant-member").(*db.TenantMemberModel)
+
+	if tenantMember.Role != db.TenantMemberRoleOwner {
+		return gen.SubscriptionUpsert403JSONResponse(
+			apierrors.NewAPIErrors("only tenant owners can update the subscription"),
+		), nil
+	}
 
 	_, err := b.config.Billing.UpsertTenantSubscription(*tenant, billing.SubscriptionOpts{
 		Plan:   dbsqlc.TenantSubscriptionPlanCodes(*req.Body.Plan),
