92eba571e058d515503c0f2766f34fcf05cbfe8d
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-06-27 08:43:56 -0400

    nerf destructive endpoints

--- api/v1/server/handlers/api-tokens/create.go
+++ api/v1/server/handlers/api-tokens/create.go
@@ -3,28 +3,10 @@ package apitokens
 import (
 	"github.com/labstack/echo/v4"
 
+	"github.com/hatchet-dev/hatchet/api/v1/server/oas/apierrors"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
-	"github.com/hatchet-dev/hatchet/pkg/repository/prisma/db"
 )
 
 func (a *APITokenService) ApiTokenCreate(ctx echo.Context, request gen.ApiTokenCreateRequestObject) (gen.ApiTokenCreateResponseObject, error) {
-	tenant := ctx.Get("tenant").(*db.TenantModel)
-
-	// validate the request
-	if apiErrors, err := a.config.Validator.ValidateAPI(request.Body); err != nil {
-		return nil, err
-	} else if apiErrors != nil {
-		return gen.ApiTokenCreate400JSONResponse(*apiErrors), nil
-	}
-
-	token, err := a.config.Auth.JWTManager.GenerateTenantToken(ctx.Request().Context(), tenant.ID, request.Body.Name)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// This is the only time the token is sent over the API
-	return gen.ApiTokenCreate200JSONResponse{
-		Token: token.Token,
-	}, nil
+	return gen.ApiTokenCreate403JSONResponse(apierrors.NewAPIErrors("this is disabled for the demo")), nil
 }
--- api/v1/server/handlers/api-tokens/revoke.go
+++ api/v1/server/handlers/api-tokens/revoke.go
@@ -3,18 +3,10 @@ package apitokens
 import (
 	"github.com/labstack/echo/v4"
 
+	"github.com/hatchet-dev/hatchet/api/v1/server/oas/apierrors"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
-	"github.com/hatchet-dev/hatchet/pkg/repository/prisma/db"
 )
 
 func (a *APITokenService) ApiTokenUpdateRevoke(ctx echo.Context, request gen.ApiTokenUpdateRevokeRequestObject) (gen.ApiTokenUpdateRevokeResponseObject, error) {
-	apiToken := ctx.Get("api-token").(*db.APITokenModel)
-
-	err := a.config.APIRepository.APIToken().RevokeAPIToken(apiToken.ID)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return gen.ApiTokenUpdateRevoke204Response{}, nil
+	return gen.ApiTokenUpdateRevoke403JSONResponse(apierrors.NewAPIErrors("this is disabled for the demo")), nil
 }
