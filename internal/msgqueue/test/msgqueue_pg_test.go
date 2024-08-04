ea15d9afbcfa7d96ab943fcccb3de7e407669d07
steebchen <contact@luca-steeb.com>
2024-08-04 17:22:09 +0200

    fix msgqueue test

--- internal/msgqueue/test/msgqueue_pg_test.go
+++ internal/msgqueue/test/msgqueue_pg_test.go
@@ -3,6 +3,10 @@
 package test
 
 import (
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
 	"sync"
 	"testing"
 
@@ -13,18 +17,37 @@ import (
 )
 
 func TestPgQueueEnsure(t *testing.T) {
-	// read in the local config
-	configLoader := loader.NewConfigLoader("../../../generated/")
+	_, b, _, _ := runtime.Caller(0)
+	testPath := filepath.Dir(b)
+	dir := path.Join(testPath, "../../..")
+
+	_ = os.Setenv("DATABASE_URL", "postgresql://hatchet:hatchet@127.0.0.1:5431/hatchet")
+	_ = os.Setenv("SERVER_TLS_CERT_FILE", path.Join(dir, "hack/dev/certs/cluster.pem"))
+	_ = os.Setenv("SERVER_TLS_KEY_FILE", path.Join(dir, "hack/dev/certs/cluster.key"))
+	_ = os.Setenv("SERVER_TLS_ROOT_CA_FILE", path.Join(dir, "hack/dev/certs/ca.cert"))
+	_ = os.Setenv("SERVER_PORT", "8080")
+	_ = os.Setenv("SERVER_URL", "http://localhost:8080")
+	_ = os.Setenv("SERVER_AUTH_COOKIE_SECRETS", "something something")
+	_ = os.Setenv("SERVER_AUTH_COOKIE_DOMAIN", "app.dev.hatchet-tools.com")
+	_ = os.Setenv("SERVER_AUTH_COOKIE_INSECURE", "false")
+	_ = os.Setenv("SERVER_AUTH_SET_EMAIL_VERIFIED", "true")
+	_ = os.Setenv("SERVER_ENCRYPTION_MASTER_KEYSET_FILE", path.Join(dir, "hack/dev/encryption-keys/master.key"))
+	_ = os.Setenv("SERVER_ENCRYPTION_JWT_PRIVATE_KEYSET_FILE", path.Join(dir, "hack/dev/encryption-keys/private_ec256.key"))
+	_ = os.Setenv("SERVER_ENCRYPTION_JWT_PUBLIC_KEYSET_FILE", path.Join(dir, "hack/dev/encryption-keys/public_ec256.key"))
+
+	_ = os.Setenv("SERVER_SECURITY_CHECK_ENABLED", "false")
+
+	cf := loader.NewConfigLoader(path.Join(dir, "./generated/"))
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 
-	cleanup, _, err := configLoader.LoadServerConfig("", func(scf *server.ServerConfigFile) {
+	cleanup, _, err := cf.LoadServerConfig("", func(scf *server.ServerConfigFile) {
 		assert.Equal(t, "postgres", scf.MessageQueue.Kind)
 		wg.Done()
 	})
 	if err != nil {
-		t.Errorf("error loading server config: %v", err)
+		t.Fatalf("error loading server config: %v", err)
 	}
 	defer cleanup() // nolint:errcheck
 	wg.Wait()
