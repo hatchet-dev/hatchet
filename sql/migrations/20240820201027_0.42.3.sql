366cbe4c9d239fa8e0fb9226bb876b0ca1b4844a
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-08-20 16:11:44 -0400

    chore: migration version

--- sql/migrations/20240820201027_0.42.3.sql
+++ sql/migrations/20240820201027_0.42.3.sql
@@ -3,9 +3,9 @@ CREATE TYPE "WorkerType" AS ENUM ('WEBHOOK', 'MANAGED', 'SELFHOSTED');
 -- Create index "Action_actionId_tenantId_key" to table: "Action"
 CREATE UNIQUE INDEX "Action_actionId_tenantId_key" ON "Action" ("actionId", "tenantId");
 -- Drop index "QueueItem_isQueued_priority_tenantId_queue_id_idx_2" from table: "QueueItem"
-DROP INDEX IF EXISTS "QueueItem_isQueued_priority_tenantId_queue_id_idx_2";
+DROP INDEX "QueueItem_isQueued_priority_tenantId_queue_id_idx_2";
 -- Modify "QueueItem" table
-ALTER TABLE "QueueItem" DROP CONSTRAINT IF EXISTS "QueueItem_priority_check";
+ALTER TABLE "QueueItem" DROP CONSTRAINT "QueueItem_priority_check";
 -- Create index "QueueItem_isQueued_priority_tenantId_queue_id_idx" to table: "QueueItem"
 CREATE INDEX "QueueItem_isQueued_priority_tenantId_queue_id_idx" ON "QueueItem" ("isQueued", "priority", "tenantId", "queue", "id");
 -- Modify "Worker" table
