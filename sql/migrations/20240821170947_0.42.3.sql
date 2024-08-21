4f69c0dfacb4798d0a702149bad0a5bba38e71b2
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-08-21 13:10:04 -0400

    chore: fix migration

--- sql/migrations/20240821170947_0.42.3.sql
+++ sql/migrations/20240821170947_0.42.3.sql
@@ -2,12 +2,6 @@
 CREATE TYPE "WorkerType" AS ENUM ('WEBHOOK', 'MANAGED', 'SELFHOSTED');
 -- Create index "Action_actionId_tenantId_key" to table: "Action"
 CREATE UNIQUE INDEX "Action_actionId_tenantId_key" ON "Action" ("actionId", "tenantId");
--- Drop index "QueueItem_isQueued_priority_tenantId_queue_id_idx_2" from table: "QueueItem"
-DROP INDEX "QueueItem_isQueued_priority_tenantId_queue_id_idx_2";
--- Modify "QueueItem" table
-ALTER TABLE "QueueItem" DROP CONSTRAINT "QueueItem_priority_check";
--- Create index "QueueItem_isQueued_priority_tenantId_queue_id_idx" to table: "QueueItem"
-CREATE INDEX "QueueItem_isQueued_priority_tenantId_queue_id_idx" ON "QueueItem" ("isQueued", "priority", "tenantId", "queue", "id");
 -- Modify "Worker" table
 ALTER TABLE "Worker" ADD COLUMN "type" "WorkerType" NOT NULL DEFAULT 'SELFHOSTED', ADD COLUMN "webhookId" uuid NULL, ADD CONSTRAINT "Worker_webhookId_fkey" FOREIGN KEY ("webhookId") REFERENCES "WebhookWorker" ("id") ON UPDATE CASCADE ON DELETE SET NULL;
 -- Create index "Worker_webhookId_key" to table: "Worker"
