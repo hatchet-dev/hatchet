61d291f4bd593e76162c56366366ee14cbe82de5
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-08-23 07:35:15 -0400

    fix: sort order

--- frontend/app/src/components/cloud/logging/logs.tsx
+++ frontend/app/src/components/cloud/logging/logs.tsx
@@ -121,7 +121,9 @@ const LoggingComponent: React.FC<LogProps> = ({
           },
         ];
 
-  const sortedLogs = [...showLogs].reverse();
+  const sortedLogs = [...showLogs].sort(
+    (a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime(),
+  );
 
   return (
     <div
