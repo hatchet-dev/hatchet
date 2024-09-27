18000d026ec777387e4cb33521d04dd3562859eb
Alexander Belanger <alexander@hatchet.run>
2024-09-27 10:28:11 -0400

    fix: always use retention on queues, optional data/worker

--- pkg/config/loader/loader.go
+++ pkg/config/loader/loader.go
@@ -441,6 +441,7 @@ func GetServerConfigFromConfigfile(dc *database.Config, cf *server.ServerConfigF
 		AdditionalOAuthConfigs: additionalOAuthConfigs,
 		AdditionalLoggers:      cf.AdditionalLoggers,
 		EnableDataRetention:    cf.EnableDataRetention,
+		EnableWorkerRetention:  cf.EnableWorkerRetention,
 	}, nil
 }
 
