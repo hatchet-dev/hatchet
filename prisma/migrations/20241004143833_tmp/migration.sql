6ede82ae25c5e0fc56186e6283514215b60be19f
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-10-15 13:33:42 -0400

    chore: migrations and generation

--- prisma/migrations/20241004143833_tmp/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "WorkerSDKS" AS ENUM ('UNKNOWN', 'GO', 'PYTHON', 'TYPESCRIPT');
-
--- AlterTable
-ALTER TABLE "Worker" ADD COLUMN     "language" "WorkerSDKS",
-ADD COLUMN     "languageVersion" TEXT,
-ADD COLUMN     "os" TEXT,
-ADD COLUMN     "runtimeExtra" TEXT,
-ADD COLUMN     "sdkVersion" TEXT;
--- sql/migrations/20241014194326_v0.50.0.sql
+++ sql/migrations/20241014194326_v0.50.0.sql
@@ -8,3 +8,8 @@ CREATE TABLE "Lease" ("id" bigserial NOT NULL, "expiresAt" timestamp(3) NULL, "t
 CREATE UNIQUE INDEX "Lease_tenantId_kind_resourceId_key" ON "Lease" ("tenantId", "kind", "resourceId");
 -- Add value to enum type: "StepRunEventReason"
 ALTER TYPE "StepRunEventReason" ADD VALUE 'ACKNOWLEDGED';
+
+-- Create enum type "WorkerSDKS"
+CREATE TYPE "WorkerSDKS" AS ENUM ('UNKNOWN', 'GO', 'PYTHON', 'TYPESCRIPT');
+-- Modify "Worker" table
+ALTER TABLE "Worker" ADD COLUMN "language" "WorkerSDKS" NULL, ADD COLUMN "languageVersion" text NULL, ADD COLUMN "os" text NULL, ADD COLUMN "runtimeExtra" text NULL, ADD COLUMN "sdkVersion" text NULL;
