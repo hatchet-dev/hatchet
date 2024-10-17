7b701ed2094d3e72edc48b81f998ff23491fdcc6
abelanger5 <belanger@sas.upenn.edu>
2024-10-17 15:47:15 -0400

    fix: proper deletion of tenants from the scheduling pool (#974)
    * fix: proper deletion of tenants from the scheduling pool
    
    * adds some assignment spans
    
    * feat: caching for rankings
    
    * remove cache

--- pkg/scheduling/v2/scheduler.go
+++ pkg/scheduling/v2/scheduler.go
@@ -3,6 +3,7 @@ package v2
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -296,8 +297,14 @@ func (s *Scheduler) replenish(ctx context.Context, mustReplenish bool) error {
 	s.actionsMu.Lock()
 	defer s.actionsMu.Unlock()
 
+	// (we don't need cryptographically secure randomness)
+	randSource := rand.New(rand.NewSource(time.Now().UnixNano())) // nolint: gosec
+
 	// first pass: write all actions with new slots to the scheduler
 	for actionId, newSlots := range actionsToNewSlots {
+		// randomly sort the slots
+		randSource.Shuffle(len(newSlots), func(i, j int) { newSlots[i], newSlots[j] = newSlots[j], newSlots[i] })
+
 		if _, ok := s.actions[actionId]; !ok {
 			s.actions[actionId] = &action{
 				slots:                      newSlots,
@@ -371,6 +378,15 @@ type assignSingleResult struct {
 	rateLimitResult *rateLimitResult
 }
 
+type tryAssignTiming struct {
+	checkRateLimits     time.Duration
+	getSlots            time.Duration
+	rankSlots           time.Duration
+	pickSlot            time.Duration
+	assignedCountMuLock time.Duration
+	unackedMuLock       time.Duration
+}
+
 // tryAssignSingleton attempts to assign a singleton step to a worker.
 func (s *Scheduler) tryAssignSingleton(
 	ctx context.Context,
@@ -383,10 +399,15 @@ func (s *Scheduler) tryAssignSingleton(
 	labels []*dbsqlc.GetDesiredLabelsRow,
 	rls map[string]int32,
 ) (
-	res assignSingleResult, err error,
+	res assignSingleResult, timing tryAssignTiming, err error,
 ) {
+	ctx, span := telemetry.NewSpan(ctx, "try-assign-singleton")
+	defer span.End()
+
+	checkpoint := time.Now()
+
 	if !qi.ActionId.Valid {
-		return res, fmt.Errorf("queue item does not have a valid action id")
+		return res, timing, fmt.Errorf("queue item does not have a valid action id")
 	}
 
 	actionId := qi.ActionId.String
@@ -400,12 +421,15 @@ func (s *Scheduler) tryAssignSingleton(
 
 		if !rlResult.succeeded {
 			res.rateLimitResult = &rlResult
-			return res, nil
+			return res, timing, nil
 		}
 
 		rateLimitAck = rlResult.ack
 		rateLimitNack = rlResult.nack
 	}
+
+	timing.checkRateLimits = time.Since(checkpoint)
+	checkpoint = time.Now()
 
 	// pick a worker to assign the slot to
 	var assignedSlot *slot
@@ -414,7 +438,7 @@ func (s *Scheduler) tryAssignSingleton(
 	if _, ok := s.actions[actionId]; !ok {
 		s.actionsMu.RUnlock()
 		res.noSlots = true
-		return res, nil
+		return res, timing, nil
 	}
 
 	candidateSlots := s.actions[actionId].slots
@@ -426,7 +450,15 @@ func (s *Scheduler) tryAssignSingleton(
 
 	s.actionsMu.RUnlock()
 
-	candidateSlots = getRankedSlots(qi, labels, candidateSlots)
+	timing.getSlots = time.Since(checkpoint)
+	checkpoint = time.Now()
+
+	if qi.Sticky.Valid || len(labels) > 0 {
+		candidateSlots = getRankedSlots(qi, labels, candidateSlots)
+	}
+
+	timing.rankSlots = time.Since(checkpoint)
+	checkpoint = time.Now()
 
 	for _, slot := range candidateSlots {
 		if !slot.active() {
@@ -441,9 +473,12 @@ func (s *Scheduler) tryAssignSingleton(
 		break
 	}
 
+	timing.pickSlot = time.Since(checkpoint)
+	checkpoint = time.Now()
+
 	if assignedSlot == nil {
 		res.noSlots = true
-		return res, nil
+		return res, timing, nil
 	}
 
 	s.assignedCountMu.Lock()
@@ -451,14 +486,19 @@ func (s *Scheduler) tryAssignSingleton(
 	res.ackId = s.assignedCount
 	s.assignedCountMu.Unlock()
 
+	timing.assignedCountMuLock = time.Since(checkpoint)
+	checkpoint = time.Now()
+
 	s.unackedMu.Lock()
 	s.unackedSlots[res.ackId] = assignedSlot
 	s.unackedMu.Unlock()
 
+	timing.unackedMuLock = time.Since(checkpoint)
+
 	res.workerId = sqlchelpers.UUIDFromStr(assignedSlot.getWorkerId())
 	res.succeeded = true
 
-	return res, nil
+	return res, timing, nil
 }
 
 type AssignedQueueItem struct {
@@ -490,6 +530,8 @@ func (s *Scheduler) tryAssign(
 	stepIdsToLabels map[string][]*dbsqlc.GetDesiredLabelsRow,
 	stepRunIdsToRateLimits map[string]map[string]int32,
 ) <-chan *assignResults {
+	ctx, span := telemetry.NewSpan(ctx, "try-assign")
+
 	// split into groups based on action ids, and process each action id in parallel
 	actionIdToQueueItems := make(map[string][]*dbsqlc.QueueItem)
 
@@ -516,6 +558,7 @@ func (s *Scheduler) tryAssign(
 
 			go func(actionId string, qis []*dbsqlc.QueueItem) {
 				defer wg.Done()
+				start := time.Now()
 				assigned := make([]*AssignedQueueItem, 0, len(qis))
 				errored := make([]*erroredQueueItem, 0, len(qis))
 				unassigned := make([]*dbsqlc.QueueItem, 0, len(qis))
@@ -550,10 +593,28 @@ func (s *Scheduler) tryAssign(
 						}
 					}
 
-					singleRes, err := s.tryAssignSingleton(ctx, qi, ringOffset, labels, rls)
+					singleRes, timing, err := s.tryAssignSingleton(ctx, qi, ringOffset, labels, rls)
 
 					if err != nil {
 						s.l.Error().Err(err).Msg("error assigning queue item")
+					}
+
+					if sinceStart := time.Since(start); sinceStart > 100*time.Millisecond {
+						s.l.Warn().Dur(
+							"check_rate_limits", timing.checkRateLimits,
+						).Dur(
+							"get_slots", timing.getSlots,
+						).Dur(
+							"rank_slots", timing.rankSlots,
+						).Dur(
+							"pick_slot", timing.pickSlot,
+						).Dur(
+							"assigned_count_mu_lock", timing.assignedCountMuLock,
+						).Dur(
+							"unacked_mu_lock", timing.unackedMuLock,
+						).Msgf(
+							"assigning queue item took longer than 100ms (%v)", sinceStart.String(),
+						)
 					}
 
 					if !singleRes.succeeded {
@@ -592,6 +653,7 @@ func (s *Scheduler) tryAssign(
 		}
 
 		wg.Wait()
+		span.End()
 		close(resultsCh)
 	}()
 
