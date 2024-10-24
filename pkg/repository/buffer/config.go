9381ca5722a0fee692620cd09990b1d04d46a619
Sean Reilly <sean@hatchet.run>
2024-10-24 11:07:46 -0700

    add a serial write for step run events

--- pkg/repository/buffer/config.go
+++ pkg/repository/buffer/config.go
@@ -7,4 +7,7 @@ type ConfigFileBuffer struct {
 
 	// FlushItemsThreshold is the number of items to hold in memory until flushing to the database
 	FlushItemsThreshold int `mapstructure:"flushItemsThreshold" json:"flushItemsThreshold,omitempty" default:"100"`
+
+	// SerialBuffer is a flag to determine if the buffer should be serial or bulk
+	SerialBuffer bool `mapstructure:"serialBuffer" json:"serialBuffer,omitempty" default:"false"`
 }
