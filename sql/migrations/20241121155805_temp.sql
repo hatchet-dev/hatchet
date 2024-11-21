b16435f433cb6be2bbb84a75a292516f3ec62bf6
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-11-21 15:26:54 -0500

    fix: migrations

--- sql/migrations/20241121155805_temp.sql
+++
@@ -1,4 +0,0 @@
--- Create enum type "WorkflowTriggerCronRefMethods"
-CREATE TYPE "WorkflowTriggerCronRefMethods" AS ENUM ('DEFAULT', 'API');
--- Modify "WorkflowTriggerCronRef" table
-ALTER TABLE "WorkflowTriggerCronRef" ADD COLUMN "method" "WorkflowTriggerCronRefMethods" NOT NULL DEFAULT 'DEFAULT';
