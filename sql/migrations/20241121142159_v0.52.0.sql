d076e4fa7acfee6105d6d51859af2dd3a6bf9648
Alexander Belanger <alexander@hatchet.run>
2024-11-27 14:20:40 -0500

    fix: make cron migration more performant

--- sql/migrations/20241121142159_v0.52.0.sql
+++ sql/migrations/20241121142159_v0.52.0.sql
@@ -12,7 +12,19 @@ CREATE TYPE "WorkflowTriggerScheduledRefMethods" AS ENUM ('DEFAULT', 'API');
 ALTER TABLE "WorkflowTriggerCronRef" ADD COLUMN "name" text NULL, ADD COLUMN "id" uuid NOT NULL, ADD COLUMN "method" "WorkflowTriggerCronRefMethods" NOT NULL DEFAULT 'DEFAULT', ADD CONSTRAINT "WorkflowTriggerCronRef_parentId_cron_name_key" UNIQUE ("parentId", "cron", "name");
 -- Modify "WorkflowTriggerScheduledRef" table
 ALTER TABLE "WorkflowTriggerScheduledRef" ADD COLUMN "method" "WorkflowTriggerScheduledRefMethods" NOT NULL DEFAULT 'DEFAULT';
+
 -- Modify "WorkflowRunTriggeredBy" table
-ALTER TABLE "WorkflowRunTriggeredBy" DROP CONSTRAINT "WorkflowRunTriggeredBy_cronParentId_cronSchedule_fkey", ADD COLUMN "cronName" text NULL, ADD CONSTRAINT "WorkflowRunTriggeredBy_cronParentId_cronSchedule_cronName_fkey" FOREIGN KEY ("cronParentId", "cronSchedule", "cronName") REFERENCES "WorkflowTriggerCronRef" ("parentId", "cron", "name") ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE "WorkflowRunTriggeredBy" 
+DROP CONSTRAINT "WorkflowRunTriggeredBy_cronParentId_cronSchedule_fkey",
+ADD COLUMN "cronName" text NULL;
+
+ALTER TABLE "WorkflowRunTriggeredBy" 
+ADD CONSTRAINT "WorkflowRunTriggeredBy_cronParentId_cronSchedule_cronName_fkey" 
+FOREIGN KEY ("cronParentId", "cronSchedule", "cronName") 
+REFERENCES "WorkflowTriggerCronRef" ("parentId", "cron", "name") 
+ON UPDATE CASCADE 
+ON DELETE SET NULL
+NOT VALID;
+
 -- Drop index "WorkflowTriggerCronRef_parentId_cron_key" from table: "WorkflowTriggerCronRef"
 DROP INDEX "WorkflowTriggerCronRef_parentId_cron_key";
