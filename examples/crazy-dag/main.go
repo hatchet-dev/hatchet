3852d0356c7df7b06ac8dfde8766da4061915cec
Sean Reilly <sean@hatchet.run>
2024-12-05 14:56:25 -0800

    crazy-dag with e2e

---
+++ examples/crazy-dag/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"github.com/hatchet-dev/hatchet/pkg/client"
+	"github.com/hatchet-dev/hatchet/pkg/cmdutils"
+	"github.com/hatchet-dev/hatchet/pkg/worker"
+)
+
+type userCreateEvent struct {
+	Username string            `json:"username"`
+	UserID   string            `json:"user_id"`
+	Data     map[string]string `json:"data"`
+}
+
+type stepOutput struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	os.Setenv("HATCHET_CLIENT_NAMESPACE", randomNamespace())
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		<-cmdutils.InterruptChan()
+		cancel()
+	}()
+
+	results := make(chan *stepOutput, 50)
+
+	if err := run(ctx, results); err != nil {
+		panic(err)
+	}
+}
+
+func run(ctx context.Context, results chan<- *stepOutput) error {
+	c, err := client.New()
+
+	if err != nil {
+		return fmt.Errorf("error creating client: %w", err)
+	}
+
+	w, err := worker.NewWorker(
+		worker.WithClient(
+			c,
+		),
+		worker.WithMaxRuns(500),
+	)
+	if err != nil {
+		return fmt.Errorf("error creating worker: %w", err)
+	}
+
+	testSvc := w.NewService("test")
+
+	stepNames := make([]string, 40) // assuming 4 steps per layer * 10 layers
+	for i := range stepNames {
+		stepNames[i] = generateRandomName()
+	}
+
+	steps := make([]*worker.WorkflowStep, len(stepNames))
+
+	for i, name := range stepNames {
+		steps[i] = worker.Fn(func(ctx worker.HatchetContext) (result *stepOutput, err error) {
+			time.Sleep(generateRandomSleep())
+			output := stepOutput{
+				Message: "Completed step " + name,
+			}
+
+			results <- &output
+
+			return &output, nil
+		}).SetName(name)
+
+		if i >= 4 {
+			// setting dependencies from previous layer (4 steps back)
+			steps[i].AddParents(stepNames[i-4])
+		}
+	}
+
+	err = testSvc.On(
+		worker.Events("crazy-dag"),
+		&worker.WorkflowJob{
+			Name:        "crazy-dag",
+			Description: "This runs after an update to the user model with random step dependencies.",
+			Steps:       steps,
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	interruptCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	cleanup, err := w.Start()
+	if err != nil {
+		return fmt.Errorf("error starting worker: %w", err)
+	}
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			testEvent := userCreateEvent{
+				Username: "echo-test",
+				UserID:   "1234",
+				Data: map[string]string{
+					"test": "test",
+				},
+			}
+
+			log.Printf("pushing event crazy-dag")
+
+			// push an event
+			err = c.Event().Push(
+				context.Background(),
+				"crazy-dag",
+				testEvent,
+			)
+
+			if err != nil {
+				log.Printf("error pushing event: %s", err.Error())
+			}
+
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+
+	<-interruptCtx.Done()
+	return cleanup()
+
+}
+
+func randomNamespace() string {
+	return fmt.Sprintf("namespace-%s", generateRandomName())
+}
+
+func generateRandomName() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	nameLength := 20 // random length between 50 and 150
+	b := make([]byte, nameLength)
+
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func generateRandomSleep() time.Duration {
+	return time.Duration(10+rand.Intn(30)) * time.Millisecond
+}
---
+++ examples/crazy-dag/main_e2e_test.go
@@ -0,0 +1,69 @@
+//go:build e2e
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/hatchet-dev/hatchet/internal/testutils"
+)
+
+func TestCrazyDAG(t *testing.T) {
+	os.Setenv("HATCHET_CLIENT_NAMESPACE", randomNamespace())
+
+	testutils.Prepare(t)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	results := make(chan *stepOutput, 50)
+
+	go func() {
+		err := run(ctx, results)
+
+		if err != nil {
+			t.Fatalf("/run() error = %v", err)
+		}
+	}()
+
+	var count int
+outer:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("ctx.Done()")
+			break outer
+
+		case res := <-results:
+			fmt.Println(res)
+			count++
+			fmt.Println("count is now ", count)
+			if count == 90 {
+				// 90 is the number of steps in the DAG
+				break outer
+			}
+
+		// timeout is longer because of how long it takes things to start up
+		case <-time.After(120 * time.Second):
+			t.Fatalf("timeout waiting for DAG to complete finished %d of %d steps", count, 90)
+		}
+	}
+
+	// assert.Equal(t, []string{
+	// 	"step-one",
+	// 	"step-two",
+	// }, items)
+
+	// if err := cleanup(); err != nil {
+	// 	t.Fatalf("cleanup() error = %v", err)
+	// }
+	if count != 90 {
+		t.Fatalf("expected 90 steps to complete, got %d", count)
+	}
+
+	fmt.Println("TestCrazyDAG done")
+}
