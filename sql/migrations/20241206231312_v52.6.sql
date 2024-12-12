f2331bc6a83022fe198efa2a14e9fb768804fb97
Sean Reilly <sean@hatchet.run>
2024-12-12 10:04:30 -0800

    merge migrations and rehash

--- sql/migrations/20241206231312_v52.6.sql
+++
@@ -1,4 +0,0 @@
--- atlas:txmode none
-
-CREATE INDEX CONCURRENTLY "Worker_tenantId_lastHeartbeatAt_idx" ON "Worker" ("tenantId", "lastHeartbeatAt");
-
--- sql/migrations/20241209235857_v52.7.sql
+++
@@ -1,2 +0,0 @@
--- Modify "WorkerAssignEvent" table
-ALTER TABLE "WorkerAssignEvent" DROP CONSTRAINT "WorkerAssignEvent_workerId_fkey", ALTER COLUMN "workerId" DROP NOT NULL, ADD CONSTRAINT "WorkerAssignEvent_workerId_fkey" FOREIGN KEY ("workerId") REFERENCES "Worker" ("id") ON UPDATE CASCADE ON DELETE SET NULL NOT VALID;
