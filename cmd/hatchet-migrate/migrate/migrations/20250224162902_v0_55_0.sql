bbdc062b2bda7918680a2c95b999b8bfe98f8541
Alexander Belanger <alexander@hatchet.run>
2025-02-24 16:37:20 -0500

    make default tenant engine version V0

--- cmd/hatchet-migrate/migrate/migrations/20250224162902_v0_55_0.sql
+++ cmd/hatchet-migrate/migrate/migrations/20250224162902_v0_55_0.sql
@@ -3,7 +3,7 @@
 -- Create enum type "TenantMajorEngineVersion"
 CREATE TYPE "TenantMajorEngineVersion" AS ENUM ('V0', 'V1');
 -- Modify "Tenant" table
-ALTER TABLE "Tenant" ADD COLUMN "version" "TenantMajorEngineVersion" NOT NULL DEFAULT 'V1';
+ALTER TABLE "Tenant" ADD COLUMN "version" "TenantMajorEngineVersion" NOT NULL DEFAULT 'V0';
 
 ALTER TYPE "LeaseKind" ADD VALUE 'CONCURRENCY_STRATEGY';
 -- +goose StatementEnd
