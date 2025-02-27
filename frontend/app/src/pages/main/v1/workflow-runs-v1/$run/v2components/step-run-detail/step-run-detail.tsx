820ea2577bd37a4b35271faa06e20bdebade568a
mrkaye97 <mrkaye97@gmail.com>
2025-02-27 11:55:51 -0500

    fix: rename

--- frontend/app/src/pages/main/v1/workflow-runs-v1/$run/v2components/step-run-detail/step-run-detail.tsx
+++ frontend/app/src/pages/main/v1/workflow-runs-v1/$run/v2components/step-run-detail/step-run-detail.tsx
@@ -21,7 +21,7 @@ import RelativeDate from '@/components/v1/molecules/relative-date';
 import { formatDuration } from '@/lib/utils';
 import { V1StepRunOutput } from './step-run-output';
 import { CodeHighlighter } from '@/components/v1/ui/code-highlighter';
-import { CancelTaskRunButton } from '@/pages/main/v1/task-runs-v1/cancel-button';
+import { CancelTaskRunButton } from '@/pages/main/v1/task-runs-v1/cancellation';
 
 export enum TabOption {
   Output = 'output',
