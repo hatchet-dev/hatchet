94351383bfc80be1701fca01900b9f3643b81249
mrkaye97 <mrkaye97@gmail.com>
2025-03-04 11:35:34 -0500

    fix: more redirects

--- frontend/app/src/pages/main/v1/managed-workers/create/index.tsx
+++ frontend/app/src/pages/main/v1/managed-workers/create/index.tsx
@@ -37,7 +37,7 @@ export default function CreateWorker() {
       return res.data;
     },
     onSuccess: (data) => {
-      navigate(`/managed-workers/${data.metadata.id}`);
+      navigate(`/v1/managed-workers/${data.metadata.id}`);
     },
     onError: handleApiError,
   });
