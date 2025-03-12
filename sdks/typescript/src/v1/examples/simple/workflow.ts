91ef5e264119fdfc0efb98c3d4052f06474de9ca
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-03-12 09:30:18 -0400

    examples

--- sdks/typescript/src/v1/examples/simple/workflow.ts
+++ sdks/typescript/src/v1/examples/simple/workflow.ts
@@ -4,35 +4,15 @@ type SimpleInput = {
   Message: string;
 };
 
-type SimpleOutput = {
-  step2: {
-    Original: string;
-    Transformed: string;
-  };
-};
-
-export const simple = hatchet.workflow<SimpleInput, SimpleOutput>({
+export const simple = hatchet.workflow<SimpleInput>({
   name: 'simple',
 });
 
-const step1 = simple.task({
-  name: 'step1',
+simple.task({
+  name: 'to-lower',
   fn: (input) => {
     return {
       TransformedMessage: input.Message.toLowerCase(),
-    };
-  },
-});
-
-simple.task({
-  name: 'step2',
-  parents: [step1],
-  timeout: '15s',
-  fn: async (input, ctx) => {
-    const step1Res = await ctx.parentData(step1);
-    return {
-      Original: input.Message,
-      Transformed: step1Res.TransformedMessage,
     };
   },
 });
