96aec3f14b9ca0ecf701a0d2bcbe1b6c9f7e451b
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-03-21 09:15:57 -0400

    feat: simplified client

--- pkg/v1/client.go
+++ pkg/v1/client.go
@@ -1,63 +1,24 @@
 package v1
 
 import (
-	"github.com/hatchet-dev/hatchet/pkg/client"
 	v0Client "github.com/hatchet-dev/hatchet/pkg/client"
 	v0Config "github.com/hatchet-dev/hatchet/pkg/config/client"
-	"github.com/hatchet-dev/hatchet/pkg/config/shared"
 )
 
 type HatchetClient interface {
-	V0() client.Client
+	V0() v0Client.Client
 }
 
-type v1ClientImpl struct {
-	v0 *client.Client
-}
-
-// Config represents client configuration
-type Config struct {
-	TenantId           string
-	Token              string
-	HostPort           string
-	Namespace          string
-	NoGrpcRetry        bool
-	CloudRegisterID    string
-	RawRunnableActions []string
-	AutoscalingTarget  string
-	TLS                *TLSConfig
-}
-
-type TLSConfig struct {
-	Base          *shared.TLSConfigFile
-	TLSServerName string
+type v1HatchetClientImpl struct {
+	v0 *v0Client.Client
 }
 
 func NewHatchetClient(config ...Config) (HatchetClient, error) {
-
 	cf := &v0Config.ClientConfigFile{}
 
 	if len(config) > 0 {
 		opts := config[0]
-		cf := &v0Config.ClientConfigFile{}
-
-		// Apply provided config to the internal configuration
-		// Zero values won't override server defaults
-		cf.TenantId = opts.TenantId
-		cf.Token = opts.Token
-		cf.HostPort = opts.HostPort
-		cf.Namespace = opts.Namespace
-		cf.NoGrpcRetry = opts.NoGrpcRetry
-		cf.CloudRegisterID = &opts.CloudRegisterID
-		cf.RawRunnableActions = opts.RawRunnableActions
-		cf.AutoscalingTarget = opts.AutoscalingTarget
-
-		if opts.TLS != nil {
-			cf.TLS = v0Config.ClientTLSConfigFile{
-				Base:          *opts.TLS.Base,
-				TLSServerName: opts.TLS.TLSServerName,
-			}
-		}
+		cf = mapConfigToCF(opts)
 	}
 
 	client, err := v0Client.NewFromConfigFile(cf)
@@ -65,11 +26,11 @@ func NewHatchetClient(config ...Config) (HatchetClient, error) {
 		return nil, err
 	}
 
-	return &v1ClientImpl{
+	return &v1HatchetClientImpl{
 		v0: &client,
 	}, nil
 }
 
-func (c *v1ClientImpl) V0() client.Client {
+func (c *v1HatchetClientImpl) V0() v0Client.Client {
 	return *c.v0
 }
---
+++ pkg/v1/config.go
@@ -0,0 +1,48 @@
+package v1
+
+import (
+	v0Config "github.com/hatchet-dev/hatchet/pkg/config/client"
+	"github.com/hatchet-dev/hatchet/pkg/config/shared"
+)
+
+// TODO logger settings
+type Config struct {
+	TenantId           string
+	Token              string
+	HostPort           string
+	Namespace          string
+	NoGrpcRetry        bool
+	CloudRegisterID    string
+	RawRunnableActions []string
+	AutoscalingTarget  string
+	TLS                *TLSConfig
+}
+
+type TLSConfig struct {
+	Base          *shared.TLSConfigFile
+	TLSServerName string
+}
+
+func mapConfigToCF(opts Config) *v0Config.ClientConfigFile {
+	cf := &v0Config.ClientConfigFile{}
+
+	// Apply provided config to the internal configuration
+	// Zero values won't override server defaults
+	cf.TenantId = opts.TenantId
+	cf.Token = opts.Token
+	cf.HostPort = opts.HostPort
+	cf.Namespace = opts.Namespace
+	cf.NoGrpcRetry = opts.NoGrpcRetry
+	cf.CloudRegisterID = &opts.CloudRegisterID
+	cf.RawRunnableActions = opts.RawRunnableActions
+	cf.AutoscalingTarget = opts.AutoscalingTarget
+
+	if opts.TLS != nil {
+		cf.TLS = v0Config.ClientTLSConfigFile{
+			Base:          *opts.TLS.Base,
+			TLSServerName: opts.TLS.TLSServerName,
+		}
+	}
+
+	return cf
+}
