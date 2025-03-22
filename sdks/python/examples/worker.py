4d0534aaf62e1fa1e470a2cbb13b2ea7834a07cd
mrkaye97 <mrkaye97@gmail.com>
2025-03-22 13:40:58 -0400

    fix: lint

--- sdks/python/examples/worker.py
+++ sdks/python/examples/worker.py
@@ -6,7 +6,7 @@ from examples.concurrency_limit_rr.worker import concurrency_limit_rr_workflow
 from examples.dag.worker import dag_workflow
 from examples.dedupe.worker import dedupe_child_wf, dedupe_parent_wf
 from examples.fanout.worker import child_wf, parent_wf
-from examples.fanout_sync.worker import sync_fanout_parent, sync_fanout_child
+from examples.fanout_sync.worker import sync_fanout_child, sync_fanout_parent
 from examples.logger.workflow import logging_workflow
 from examples.on_failure.worker import on_failure_wf, on_failure_wf_with_details
 from examples.opentelemetry_instrumentation.worker import otel_workflow
