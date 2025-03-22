401f3d51a51328fd238989f20dbf22538be5016a
mrkaye97 <mrkaye97@gmail.com>
2025-03-22 16:31:03 -0400

    debug: rest of the wfs

--- sdks/python/examples/worker.py
+++ sdks/python/examples/worker.py
@@ -8,12 +8,12 @@ from examples.concurrency_limit_rr.worker import concurrency_limit_rr_workflow
 from examples.dag.worker import dag_workflow
 from examples.dedupe.worker import dedupe_child_wf, dedupe_parent_wf
 from examples.fanout.worker import child_wf, parent_wf
-# from examples.fanout_sync.worker import sync_fanout_child, sync_fanout_parent
-# from examples.logger.workflow import logging_workflow
-# from examples.on_failure.worker import on_failure_wf, on_failure_wf_with_details
-# from examples.rate_limit.worker import rate_limit_workflow
-# from examples.timeout.worker import refresh_timeout_wf, timeout_wf
-# from examples.waits.worker import dag_waiting_workflow
+from examples.fanout_sync.worker import sync_fanout_child, sync_fanout_parent
+from examples.logger.workflow import logging_workflow
+from examples.on_failure.worker import on_failure_wf, on_failure_wf_with_details
+from examples.rate_limit.worker import rate_limit_workflow
+from examples.timeout.worker import refresh_timeout_wf, timeout_wf
+from examples.waits.worker import dag_waiting_workflow
 from hatchet_sdk import Hatchet
 
 hatchet = Hatchet(debug=True)
@@ -40,16 +40,16 @@ def main() -> None:
             dedupe_parent_wf,
             child_wf,
             parent_wf,
-            # on_failure_wf,
-            # on_failure_wf_with_details,
-            # logging_workflow,
-            # rate_limit_workflow,
-            # timeout_wf,
-            # refresh_timeout_wf,
-            # dag_waiting_workflow,
-            # wf,
-            # sync_fanout_parent,
-            # sync_fanout_child,
+            on_failure_wf,
+            on_failure_wf_with_details,
+            logging_workflow,
+            rate_limit_workflow,
+            timeout_wf,
+            refresh_timeout_wf,
+            dag_waiting_workflow,
+            wf,
+            sync_fanout_parent,
+            sync_fanout_child,
         ],
     )
 
