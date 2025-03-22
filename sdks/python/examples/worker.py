c4759672b24c4bdfe9c8df80c04962899b535bf2
mrkaye97 <mrkaye97@gmail.com>
2025-03-22 16:28:37 -0400

    debug: more workflows

--- sdks/python/examples/worker.py
+++ sdks/python/examples/worker.py
@@ -2,12 +2,12 @@ import os
 
 from examples.affinity_workers.worker import affinity_worker_workflow
 from examples.bulk_fanout.worker import bulk_child_wf, bulk_parent_wf
-# from examples.cancellation.worker import wf
-# from examples.concurrency_limit.worker import concurrency_limit_workflow
-# from examples.concurrency_limit_rr.worker import concurrency_limit_rr_workflow
-# from examples.dag.worker import dag_workflow
-# from examples.dedupe.worker import dedupe_child_wf, dedupe_parent_wf
-# from examples.fanout.worker import child_wf, parent_wf
+from examples.cancellation.worker import wf
+from examples.concurrency_limit.worker import concurrency_limit_workflow
+from examples.concurrency_limit_rr.worker import concurrency_limit_rr_workflow
+from examples.dag.worker import dag_workflow
+from examples.dedupe.worker import dedupe_child_wf, dedupe_parent_wf
+from examples.fanout.worker import child_wf, parent_wf
 # from examples.fanout_sync.worker import sync_fanout_child, sync_fanout_parent
 # from examples.logger.workflow import logging_workflow
 # from examples.on_failure.worker import on_failure_wf, on_failure_wf_with_details
@@ -33,13 +33,13 @@ def main() -> None:
             affinity_worker_workflow,
             bulk_child_wf,
             bulk_parent_wf,
-            # concurrency_limit_workflow,
-            # concurrency_limit_rr_workflow,
-            # dag_workflow,
-            # dedupe_child_wf,
-            # dedupe_parent_wf,
-            # child_wf,
-            # parent_wf,
+            concurrency_limit_workflow,
+            concurrency_limit_rr_workflow,
+            dag_workflow,
+            dedupe_child_wf,
+            dedupe_parent_wf,
+            child_wf,
+            parent_wf,
             # on_failure_wf,
             # on_failure_wf_with_details,
             # logging_workflow,
