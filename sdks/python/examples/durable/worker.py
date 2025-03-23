9e3b655dfc21040da6787009c1e73fcc3f50c3d0
mrkaye97 <mrkaye97@gmail.com>
2025-03-23 19:07:05 -0400

    fix: example

--- sdks/python/examples/durable/worker.py
+++ sdks/python/examples/durable/worker.py
@@ -26,7 +26,7 @@ def ephemeral_task_2(input: EmptyModel, ctx: Context) -> None:
 
 
 def main() -> None:
-    worker = hatchet.worker("durable-worker", workflows=[durable_workflow])
+    worker = hatchet.worker("durable-worker", workflows=[durable_workflow, ephemeral_workflow])
     worker.start()
 
 
