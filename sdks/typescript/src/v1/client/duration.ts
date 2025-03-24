c345ed028becbe8ca0d6947ef6dbded972ece50f
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-03-24 13:41:29 -0700

    oops days aren't supported

--- sdks/typescript/src/v1/client/duration.ts
+++ sdks/typescript/src/v1/client/duration.ts
@@ -2,28 +2,14 @@
 type SecondsDuration = `${number}s`;
 type MinutesDuration = `${number}m`;
 type HoursDuration = `${number}h`;
-type DaysDuration = `${number}d`;
 
 // Combined durations
-type TwoUnitDurations =
-  | `${number}d${number}h`
-  | `${number}d${number}m`
-  | `${number}d${number}s`
-  | `${number}h${number}m`
-  | `${number}h${number}s`
-  | `${number}m${number}s`;
-type ThreeUnitDurations =
-  | `${number}d${number}h${number}m`
-  | `${number}d${number}h${number}s`
-  | `${number}d${number}m${number}s`
-  | `${number}h${number}m${number}s`;
-type FourUnitDuration = `${number}d${number}h${number}m${number}s`;
+type TwoUnitDurations = `${number}h${number}m` | `${number}h${number}s` | `${number}m${number}s`;
+type ThreeUnitDurations = `${number}h${number}m${number}s`;
 
 export type Duration =
   | SecondsDuration
   | MinutesDuration
   | HoursDuration
-  | DaysDuration
   | TwoUnitDurations
-  | ThreeUnitDurations
-  | FourUnitDuration;
+  | ThreeUnitDurations;
