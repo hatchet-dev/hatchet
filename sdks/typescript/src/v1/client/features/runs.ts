5344041579bedea54e92027b47ab479017b6c8d0
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-03-24 07:14:04 -0400

    feat: init runs client

--- sdks/typescript/src/v1/client/features/runs.ts
+++ sdks/typescript/src/v1/client/features/runs.ts
@@ -1,3 +1,4 @@
+import WorkflowRunRef from '@hatchet/util/workflow-run-ref';
 import { HatchetClient } from '../client';
 
 /**
@@ -5,8 +6,45 @@ import { HatchetClient } from '../client';
  */
 export class RunsClient {
   api: HatchetClient['api'];
+  tenantId: string;
 
   constructor(client: HatchetClient) {
     this.api = client.api;
+    this.tenantId = client.tenantId;
+  }
+
+  async get<T = any>(run: string | WorkflowRunRef<T>) {
+    const runId = typeof run === 'string' ? run : await run.getWorkflowRunId();
+
+    const { data } = await this.api.workflowRunGet(this.tenantId, runId);
+    return data;
+  }
+
+  async getDetails<T = any>(run: string | WorkflowRunRef<T>) {
+    const runId = typeof run === 'string' ? run : await run.getWorkflowRunId();
+
+    const { data } = await this.api.workflowRunGetShape(this.tenantId, runId);
+    return data;
+  }
+
+  async list(opts?: Parameters<typeof this.api.workflowRunList>[1]) {
+    // TODO workflow id on opts is a uuid
+
+    const { data } = await this.api.workflowRunList(this.tenantId, opts);
+    return data;
+  }
+
+  async replay(opts: Parameters<typeof this.api.v1TaskReplay>[1]) {
+    // TODO is v1 check
+    // TODO   workflowIds?: string[]; on opts.filters
+    const { data } = await this.api.v1TaskReplay(this.tenantId, opts);
+    return data;
+  }
+
+  async cancel(opts: Parameters<typeof this.api.v1TaskCancel>[1]) {
+    // TODO is v1 check
+    // TODO   workflowIds?: string[]; on opts.filters
+    const { data } = await this.api.v1TaskCancel(this.tenantId, opts);
+    return data;
   }
 }
