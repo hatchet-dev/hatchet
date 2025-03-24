55a37dc6ca6c4d2693275d6d2ade9c20719582be
mrkaye97 <mrkaye97@gmail.com>
2025-03-23 21:52:53 -0400

    feat: start working on more docs

--- sdks/typescript/src/examples/fanout-worker.ts
+++ sdks/typescript/src/examples/fanout-worker.ts
@@ -1,5 +1,6 @@
 import Hatchet from '../sdk';
-import { Workflow } from '../workflow';
+import sleep from '@hatchet/util/sleep';
+
 
 const hatchet = Hatchet.init();
 
@@ -13,82 +14,97 @@ type Output = {
   };
 };
 
-const parentWorkflow: Workflow = {
-  id: 'parent-workflow',
+export const parentWorkflow = hatchet.workflow<Input, Output>({
+  name: 'simple',
   description: 'simple example for spawning child workflows',
-  on: {
-    event: 'fanout:create',
+});
+
+const firstTask = parentWorkflow.task({
+  name: 'first-task',
+  fn: async () => {
+    await sleep(2000);
+    return {
+      Completed: true,
+    };
   },
-  steps: [
-    {
-      name: 'parent-spawn',
-      timeout: '70s',
-      run: async (ctx) => {
-        const promises = Array.from({ length: 3 }, (_, i) =>
-          ctx
-            .spawnWorkflow<Input, Output>(
-              'child-workflow',
-              { input: `child-input-${i}` },
-              { additionalMetadata: { childKey: 'childValue' } }
-            )
-            .result()
-            .then((result) => {
-              ctx.log('spawned workflow result:');
-              return result;
-            })
-        );
+});
 
-        const results = await Promise.all(promises);
-        console.log('spawned workflow results:', results);
-        console.log('number of spawned workflows:', results.length);
-        return { spawned: 'x' };
-      },
-    },
-  ],
-};
+// const parentWorkflow: Workflow = {
+//   id: 'parent-workflow',
+//   description: 'simple example for spawning child workflows',
+//   on: {
+//     event: 'fanout:create',
+//   },
+//   steps: [
+//     {
+//       name: 'parent-spawn',
+//       timeout: '70s',
+//       run: async (ctx) => {
+//         const promises = Array.from({ length: 3 }, (_, i) =>
+//           ctx
+//             .spawnWorkflow<Input, Output>(
+//               'child-workflow',
+//               { input: `child-input-${i}` },
+//               { additionalMetadata: { childKey: 'childValue' } }
+//             )
+//             .result()
+//             .then((result) => {
+//               ctx.log('spawned workflow result:');
+//               return result;
+//             })
+//         );
 
-const childWorkflow: Workflow = {
-  id: 'child-workflow',
-  description: 'simple example for spawning child workflows',
-  on: {
-    event: 'child:create',
-  },
-  steps: [
-    {
-      name: 'child-work',
-      run: async (ctx) => {
-        const { input } = ctx.workflowInput();
-        // throw new Error('child error');
-        return { 'child-output': 'sm' };
-      },
-    },
-    {
-      name: 'child-work2',
-      run: async (ctx) => {
-        const { input } = ctx.workflowInput();
-        // Perform CPU-bound work
-        // throw new Error('child error');
-        console.log('child workflow input:', input);
-        // Generate a large amount of garbage data
+//         const results = await Promise.all(promises);
+//         console.log('spawned workflow results:', results);
+//         console.log('number of spawned workflows:', results.length);
+//         return { spawned: 'x' };
+//       },
+//     },
+//   ],
+// };
 
-        const garbageData = Array.from({ length: 1e6 / 3.5 }, (_, i) => `garbage-${i}`).join(',');
-        console.log('Generated garbage data:', `${garbageData.slice(0, 100)}...`); // Print a snippet of the garbage data
-        return { 'child-output': garbageData };
-      },
-    },
-    {
-      name: 'child-work3',
-      parents: ['child-work'],
-      run: async (ctx) => {
-        const { input } = ctx.workflowInput();
-        // throw new Error('child error');
-        const garbageData = Array.from({ length: 1e6 / 3.5 }, (_, i) => `garbage-${i}`).join(',');
-        console.log('Generated garbage data:', `${garbageData.slice(0, 100)}...`); // Print a snippet of the garbage data
-        return { 'child-output': garbageData };
-      },
-    },
-  ],
-};
+// const childWorkflow: Workflow = {
+//   id: 'child-workflow',
+//   description: 'simple example for spawning child workflows',
+//   on: {
+//     event: 'child:create',
+//   },
+//   steps: [
+//     {
+//       name: 'child-work',
+//       run: async (ctx) => {
+//         const { input } = ctx.workflowInput();
+//         // throw new Error('child error');
+//         return { 'child-output': 'sm' };
+//       },
+//     },
+//     {
+//       name: 'child-work2',
+//       run: async (ctx) => {
+//         const { input } = ctx.workflowInput();
+//         // Perform CPU-bound work
+//         // throw new Error('child error');
+//         console.log('child workflow input:', input);
+//         // Generate a large amount of garbage data
+
+//         const garbageData = Array.from({ length: 1e6 / 3.5 }, (_, i) => `garbage-${i}`).join(',');
+//         console.log('Generated garbage data:', `${garbageData.slice(0, 100)}...`); // Print a snippet of the garbage data
+//         return { 'child-output': garbageData };
+//       },
+//     },
+//     {
+//       name: 'child-work3',
+//       parents: ['child-work'],
+//       run: async (ctx) => {
+//         const { input } = ctx.workflowInput();
+//         // throw new Error('child error');
+//         const garbageData = Array.from({ length: 1e6 / 3.5 }, (_, i) => `garbage-${i}`).join(',');
+//         console.log('Generated garbage data:', `${garbageData.slice(0, 100)}...`); // Print a snippet of the garbage data
+//         return { 'child-output': garbageData };
+//       },
+//     },
+//   ],
+// };
 
 async function main() {
   const worker = await hatchet.worker('fanout-worker', { maxRuns: 1000 });
