bb34c290f1eb0563d5a7468b8c9dc31440eb508d
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-03-25 15:14:44 -0700

    examples

--- sdks/typescript/src/v1/examples/retries/workflow.ts
+++ sdks/typescript/src/v1/examples/retries/workflow.ts
@@ -5,6 +5,41 @@ import { hatchet } from '../hatchet-client';
 export const retries = hatchet.task({
   name: 'retries',
   retries: 3,
+  fn: async (_, ctx) => {
+    throw new Error('intentional failure');
+  },
+});
+// !!
+
+// ❓ Step Retries with Count
+export const retriesWithCount = hatchet.task({
+  name: 'retriesWithCount',
+  retries: 3,
+  fn: async (_, ctx) => {
+    // ❓ Get the current retry count
+    const retryCount = ctx.retryCount();
+
+    console.log(`Retry count: ${retryCount}`);
+
+    if (retryCount < 2) {
+      throw new Error('intentional failure');
+    }
+
+    return {
+      message: 'success',
+    };
+  },
+});
+// !!
+
+// ❓ Step Retries with Backoff
+export const withBackoff = hatchet.task({
+  name: 'withBackoff',
+  retries: 3,
+  backoff: {
+    factor: 2,
+    maxSeconds: 10,
+  },
   fn: async () => {
     throw new Error('intentional failure');
   },
---
+++ sdks/typescript/src/v1/examples/sticky/workflow.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import { StickyStrategy } from '@hatchet/protoc/workflows';
+import { hatchet } from '../hatchet-client';
+import { child } from '../child_workflows/workflow';
+
+// ❓ Sticky Task
+export const sticky = hatchet.task({
+  name: 'sticky',
+  retries: 3,
+  sticky: StickyStrategy.SOFT,
+  fn: async (_, ctx) => {
+    // specify a child workflow to run on the same worker
+    const result = await ctx.runChild(
+      child,
+      {
+        N: 1,
+      },
+      { sticky: true }
+    );
+
+    return {
+      result,
+    };
+  },
+});
+// !!
