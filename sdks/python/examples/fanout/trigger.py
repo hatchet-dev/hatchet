7152f3b31c76179df6bdce82511a72311d4868a4
mrkaye97 <mrkaye97@gmail.com>
2025-03-26 15:49:56 -0400

    fix: example

--- sdks/python/examples/fanout/trigger.py
+++ sdks/python/examples/fanout/trigger.py
@@ -8,7 +8,7 @@ hatchet = Hatchet()
 
 
 async def main() -> None:
-    await parent_wf.run(
+    await parent_wf.aio_run(
         ParentInput(n=2),
         options=TriggerWorkflowOptions(additional_metadata={"hello": "moon"}),
     )
