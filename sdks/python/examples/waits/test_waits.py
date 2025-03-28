d0d9a0717719dee2e9e991ef17d5ff88da60ef29
mrkaye97 <mrkaye97@gmail.com>
2025-03-28 14:39:49 -0400

    feat: add test for waits

---
+++ sdks/python/examples/waits/test_waits.py
@@ -0,0 +1,44 @@
+import asyncio
+
+import pytest
+
+from examples.waits.worker import task_condition_workflow
+from hatchet_sdk import Hatchet
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_waits(hatchet: Hatchet) -> None:
+
+    ref = task_condition_workflow.run_no_wait()
+
+    await asyncio.sleep(5)
+
+    hatchet.event.push("skip_on_event:skip", {})
+    hatchet.event.push("wait_for_event:start", {})
+
+    result = await ref.aio_result()
+
+    assert result["skip_on_event"] == {"skipped": True}
+
+    first_random_number = result["start"]["random_number"]
+    wait_for_event_random_number = result["wait_for_event"]["random_number"]
+    wait_for_sleep_random_number = result["wait_for_sleep"]["random_number"]
+
+    left_branch = result["left_branch"]
+    right_branch = result["right_branch"]
+
+    assert left_branch.get("skipped") is True or right_branch.get("skipped") is True
+
+    branch_random_number = left_branch.get("random_number") or right_branch.get(
+        "random_number"
+    )
+
+    result_sum = result["sum"]["sum"]
+
+    assert (
+        result_sum
+        == first_random_number
+        + wait_for_event_random_number
+        + wait_for_sleep_random_number
+        + branch_random_number
+    )
