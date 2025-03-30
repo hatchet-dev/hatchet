9d79701eba5b8b66c7974faa0f585f8956d17537
mrkaye97 <mrkaye97@gmail.com>
2025-03-30 15:48:23 -0400

    fix: rm dead code

--- sdks/python/hatchet_sdk/utils/aio_utils.py
+++ sdks/python/hatchet_sdk/utils/aio_utils.py
@@ -8,23 +8,6 @@ Y = TypeVar("Y")
 S = TypeVar("S")
 
 
-def get_active_event_loop() -> asyncio.AbstractEventLoop | None:
-    """
-    Get the active event loop.
-
-    Returns:
-        asyncio.AbstractEventLoop: The active event loop, or None if there is no active
-        event loop in the current thread.
-    """
-    try:
-        return asyncio.get_event_loop()
-    except RuntimeError as e:
-        if str(e).startswith("There is no current event loop in thread"):
-            return None
-        else:
-            raise e
-
-
 def _run_async_function_do_not_use_directly(
     async_func: Callable[P, Coroutine[Y, S, R]],
     *args: P.args,
