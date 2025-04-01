046fe94f5b538a3e626f1b8ed151ac0bf9e7d454
mrkaye97 <mrkaye97@gmail.com>
2025-04-01 18:09:10 -0400

    python: cruft

--- sdks/python/examples/cancellation/test_cancellation.py
+++ sdks/python/examples/cancellation/test_cancellation.py
@@ -1,11 +1,19 @@
+import asyncio
+
 import pytest
 
-from examples.cancellation.worker import wf
+from examples.cancellation.worker import cancellation_workflow
 from hatchet_sdk import Hatchet
+from hatchet_sdk.clients.rest.models.v1_task_status import V1TaskStatus
 
-
-# requires scope module or higher for shared event loop
 @pytest.mark.asyncio()
-async def test_run(hatchet: Hatchet) -> None:
-    with pytest.raises(Exception, match="(Task exceeded timeout|TIMED_OUT)"):
-        await wf.aio_run()
+async def test_cancellation(hatchet: Hatchet) -> None:
+    ref = await cancellation_workflow.aio_run_no_wait()
+
+    """Sleep for a long time since we only need cancellation to happen _eventually_"""
+    await asyncio.sleep(10)
+
+    run = await hatchet.runs.aio_get(ref.workflow_run_id)
+
+    assert run.run.status == V1TaskStatus.CANCELLED
+    assert not run.run.output
