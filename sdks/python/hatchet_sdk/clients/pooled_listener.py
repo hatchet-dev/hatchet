fde3ca9917422d25956759b019beaf8f78e81e66
mrkaye97 <mrkaye97@gmail.com>
2025-04-02 12:32:35 -0400

    feat: initial generic impl

--- sdks/python/hatchet_sdk/clients/pooled_listener.py
+++ sdks/python/hatchet_sdk/clients/pooled_listener.py
@@ -1,40 +1,73 @@
 import asyncio
-import json
+from abc import ABC, abstractmethod
 from collections.abc import AsyncIterator
-from typing import Any, cast, Generic, TypeVar, Literal
+from typing import Any, Callable, Generic, Literal, TypeVar, cast
 
 import grpc
 import grpc.aio
 from grpc._cython import cygrpc  # type: ignore[attr-defined]
 
-from hatchet_sdk.clients.event_ts import ThreadSafeEvent, read_with_interrupt
 from hatchet_sdk.config import ClientConfig
 from hatchet_sdk.connection import new_conn
-from hatchet_sdk.contracts.dispatcher_pb2 import (
-    SubscribeToWorkflowRunsRequest,
-    WorkflowRunEvent,
-)
 from hatchet_sdk.contracts.dispatcher_pb2_grpc import DispatcherStub
 from hatchet_sdk.logger import logger
 from hatchet_sdk.metadata import get_metadata
 
-DEFAULT_WORKFLOW_LISTENER_RETRY_INTERVAL = 3  # seconds
-DEFAULT_WORKFLOW_LISTENER_RETRY_COUNT = 5
-DEFAULT_WORKFLOW_LISTENER_INTERRUPT_INTERVAL = 1800  # 30 minutes
+DEFAULT_LISTENER_RETRY_INTERVAL = 3  # seconds
+DEFAULT_LISTENER_RETRY_COUNT = 5
+DEFAULT_LISTENER_INTERRUPT_INTERVAL = 1800  # 30 minutes
 
-DEDUPE_MESSAGE = "DUPLICATE_WORKFLOW_RUN"
-
+R = TypeVar("R")
 T = TypeVar("T")
 
 SentinelValue = Literal["STOP"]
 SENTINEL_VALUE: SentinelValue = "STOP"
+
+
+class ThreadSafeEvent(asyncio.Event):
+    """
+    ThreadSafeEvent is a subclass of asyncio.Event that allows for thread-safe setting and clearing of the event.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        if self._loop is None:  # type: ignore[has-type]
+            self._loop = asyncio.get_event_loop()
+
+    def set(self) -> None:
+        if not self._loop.is_closed():
+            self._loop.call_soon_threadsafe(super().set)
+
+    def clear(self) -> None:
+        self._loop.call_soon_threadsafe(super().clear)
+
+
+TRequest = TypeVar("TRequest")
+TResponse = TypeVar("TResponse")
+
+
+async def read_with_interrupt(
+    listener: grpc.aio.UnaryStreamCall[TRequest, TResponse],
+    interrupt: ThreadSafeEvent,
+    key_generator: Callable[[TResponse], str],
+) -> tuple[TResponse, str]:
+    try:
+        result = cast(TResponse, await listener.read())
+
+        if result is cygrpc.EOF:
+            raise ValueError("Unexpected EOF")
+
+        return result, key_generator(result)
+    finally:
+        interrupt.set()
+
 
 class Subscription(Generic[T]):
     def __init__(self, id: int) -> None:
         self.id = id
         self.queue: asyncio.Queue[T | SentinelValue] = asyncio.Queue()
 
-    async def __aiter__(self) -> "Subscription":
+    async def __aiter__(self) -> "Subscription[T]":
         return self
 
     async def __anext__(self) -> T | SentinelValue:
@@ -52,37 +85,28 @@ class Subscription(Generic[T]):
         await self.queue.put(item)
 
     async def close(self) -> None:
-        await self.queue.put(SentinelValue)
+        await self.queue.put("STOP")
 
 
-class PooledWorkflowRunListener:
+class PooledListener(Generic[R, T], ABC):
     def __init__(self, config: ClientConfig):
         self.token = config.token
         self.config = config
 
-        # list of all active subscriptions, mapping from a subscription id to a workflow run id
-        self.subscriptions_to_workflows: dict[int, str] = {}
-
-        # list of workflow run ids mapped to an array of subscription ids
-        self.workflows_to_subscriptions: dict[str, list[int]] = {}
+        self.from_subscriptions: dict[int, str] = {}
+        self.to_subscriptions: dict[str, list[int]] = {}
 
         self.subscription_counter: int = 0
         self.subscription_counter_lock: asyncio.Lock = asyncio.Lock()
 
-        self.requests: asyncio.Queue[SubscribeToWorkflowRunsRequest | int] = (
-            asyncio.Queue()
-        )
+        self.requests: asyncio.Queue[R | int] = asyncio.Queue()
 
-        self.listener: (
-            grpc.aio.UnaryStreamCall[SubscribeToWorkflowRunsRequest, WorkflowRunEvent]
-            | None
-        ) = None
+        self.listener: grpc.aio.UnaryStreamCall[R, T] | None = None
         self.listener_task: asyncio.Task[None] | None = None
 
         self.curr_requester: int = 0
 
-        # events have keys of the format workflow_run_id + subscription_id
-        self.events: dict[int, Subscription] = {}
+        self.events: dict[int, Subscription[T]] = {}
 
         self.interrupter: asyncio.Task[None] | None = None
 
@@ -94,10 +118,14 @@ class PooledWorkflowRunListener:
         """
         _interrupter runs in a separate thread and interrupts the listener according to a configurable duration.
         """
-        await asyncio.sleep(DEFAULT_WORKFLOW_LISTENER_INTERRUPT_INTERVAL)
+        await asyncio.sleep(DEFAULT_LISTENER_INTERRUPT_INTERVAL)
 
         if self.interrupt is not None:
             self.interrupt.set()
+
+    @abstractmethod
+    def generate_key(self, response: T) -> str:
+        pass
 
     async def _init_producer(self) -> None:
         try:
@@ -106,7 +134,7 @@ class PooledWorkflowRunListener:
                     try:
                         self.listener = await self._retry_subscribe()
 
-                        logger.debug("Workflow run listener connected.")
+                        logger.debug("Listener connected.")
 
                         # spawn an interrupter task
                         if self.interrupter is not None and not self.interrupter.done():
@@ -120,44 +148,40 @@ class PooledWorkflowRunListener:
                                 continue
 
                             t = asyncio.create_task(
-                                read_with_interrupt(self.listener, self.interrupt)
+                                read_with_interrupt(
+                                    self.listener, self.interrupt, self.generate_key
+                                )
                             )
                             await self.interrupt.wait()
 
                             if not t.done():
-                                # print a warning
                                 logger.warning(
-                                    "Interrupted read_with_interrupt task of workflow run listener"
+                                    "Interrupted read_with_interrupt task of listener"
                                 )
 
                                 t.cancel()
                                 self.listener.cancel()
 
-                                await asyncio.sleep(
-                                    DEFAULT_WORKFLOW_LISTENER_RETRY_INTERVAL
-                                )
+                                await asyncio.sleep(DEFAULT_LISTENER_RETRY_INTERVAL)
                                 break
 
-                            workflow_event: WorkflowRunEvent = t.result()
+                            event, key = t.result()
 
-                            if workflow_event is cygrpc.EOF:
+                            if event is cygrpc.EOF:
                                 break
 
-                            # get a list of subscriptions for this workflow
-                            subscriptions = self.workflows_to_subscriptions.get(
-                                workflow_event.workflowRunId, []
-                            )
+                            subscriptions = self.to_subscriptions.get(key, [])
 
                             for subscription_id in subscriptions:
-                                await self.events[subscription_id].put(workflow_event)
+                                await self.events[subscription_id].put(event)
 
                     except grpc.RpcError as e:
-                        logger.debug(f"grpc error in workflow run listener: {e}")
-                        await asyncio.sleep(DEFAULT_WORKFLOW_LISTENER_RETRY_INTERVAL)
+                        logger.debug(f"grpc error in listener: {e}")
+                        await asyncio.sleep(DEFAULT_LISTENER_RETRY_INTERVAL)
                         continue
 
         except Exception as e:
-            logger.error(f"Error in workflow run listener: {e}")
+            logger.error(f"Error in listener: {e}")
 
             self.listener = None
 
@@ -167,16 +191,17 @@ class PooledWorkflowRunListener:
 
             raise e
 
-    async def _request(self) -> AsyncIterator[SubscribeToWorkflowRunsRequest]:
+    @abstractmethod
+    def create_request_body(self, item: str) -> R:
+        pass
+
+    async def _request(self) -> AsyncIterator[R]:
         self.curr_requester = self.curr_requester + 1
 
-        # replay all existing subscriptions
-        workflow_run_set = set(self.subscriptions_to_workflows.values())
+        to_subscribe_to = set(self.from_subscriptions.values())
 
-        for workflow_run_id in workflow_run_set:
-            yield SubscribeToWorkflowRunsRequest(
-                workflowRunId=workflow_run_id,
-            )
+        for item in to_subscribe_to:
+            yield self.create_request_body(item)
 
         while True:
             request = await self.requests.get()
@@ -193,40 +218,32 @@ class PooledWorkflowRunListener:
             self.requests.task_done()
 
     def cleanup_subscription(self, subscription_id: int) -> None:
-        workflow_run_id = self.subscriptions_to_workflows[subscription_id]
+        id = self.from_subscriptions[subscription_id]
 
-        if workflow_run_id in self.workflows_to_subscriptions:
-            self.workflows_to_subscriptions[workflow_run_id].remove(subscription_id)
+        if id in self.to_subscriptions:
+            self.to_subscriptions[id].remove(subscription_id)
 
-        del self.subscriptions_to_workflows[subscription_id]
+        del self.from_subscriptions[subscription_id]
         del self.events[subscription_id]
 
-    async def subscribe(self, workflow_run_id: str) -> WorkflowRunEvent:
+    async def subscribe(self, id: str) -> T:
         subscription_id: int | None = None
 
         try:
-            # create a new subscription id, place a mutex on the counter
-            await self.subscription_counter_lock.acquire()
-            self.subscription_counter += 1
-            subscription_id = self.subscription_counter
-            self.subscription_counter_lock.release()
+            async with self.subscription_counter_lock:
+                self.subscription_counter += 1
+                subscription_id = self.subscription_counter
 
-            self.subscriptions_to_workflows[subscription_id] = workflow_run_id
+            self.from_subscriptions[subscription_id] = id
 
-            if workflow_run_id not in self.workflows_to_subscriptions:
-                self.workflows_to_subscriptions[workflow_run_id] = [subscription_id]
+            if id not in self.to_subscriptions:
+                self.to_subscriptions[id] = [subscription_id]
             else:
-                self.workflows_to_subscriptions[workflow_run_id].append(subscription_id)
+                self.to_subscriptions[id].append(subscription_id)
 
-            self.events[subscription_id] = Subscription(
-                subscription_id, workflow_run_id
-            )
+            self.events[subscription_id] = Subscription(subscription_id)
 
-            await self.requests.put(
-                SubscribeToWorkflowRunsRequest(
-                    workflowRunId=workflow_run_id,
-                )
-            )
+            await self.requests.put(self.create_request_body(id))
 
             if not self.listener_task or self.listener_task.done():
                 self.listener_task = asyncio.create_task(self._init_producer())
@@ -238,54 +255,42 @@ class PooledWorkflowRunListener:
             if subscription_id:
                 self.cleanup_subscription(subscription_id)
 
-    async def aio_result(self, workflow_run_id: str) -> dict[str, Any]:
-        from hatchet_sdk.clients.admin import DedupeViolationErr
+    @abstractmethod
+    async def aio_result(self, id: str) -> dict[str, Any]:
+        pass
 
-        event = await self.subscribe(workflow_run_id)
-        errors = [result.error for result in event.results if result.error]
-
-        if errors:
-            if DEDUPE_MESSAGE in errors[0]:
-                raise DedupeViolationErr(errors[0])
-            else:
-                raise Exception(f"Workflow Errors: {errors}")
-
-        return {
-            result.stepReadableId: json.loads(result.output)
-            for result in event.results
-            if result.output
-        }
+    @abstractmethod
+    async def create_subscription(
+        self, request: AsyncIterator[R], metadata: tuple[tuple[str, str]]
+    ) -> grpc.aio.UnaryStreamCall[R, T]:
+        pass
 
     async def _retry_subscribe(
         self,
-    ) -> grpc.aio.UnaryStreamCall[SubscribeToWorkflowRunsRequest, WorkflowRunEvent]:
+    ) -> grpc.aio.UnaryStreamCall[R, T]:
         retries = 0
         if self.client is None:
             conn = new_conn(self.config, True)
             self.client = DispatcherStub(conn)
 
-        while retries < DEFAULT_WORKFLOW_LISTENER_RETRY_COUNT:
+        while retries < DEFAULT_LISTENER_RETRY_COUNT:
             try:
                 if retries > 0:
-                    await asyncio.sleep(DEFAULT_WORKFLOW_LISTENER_RETRY_INTERVAL)
+                    await asyncio.sleep(DEFAULT_LISTENER_RETRY_INTERVAL)
 
                 # signal previous async iterator to stop
                 if self.curr_requester != 0:
                     self.requests.put_nowait(self.curr_requester)
 
-                return cast(
-                    grpc.aio.UnaryStreamCall[
-                        SubscribeToWorkflowRunsRequest, WorkflowRunEvent
-                    ],
-                    self.client.SubscribeToWorkflowRuns(
-                        self._request(),  # type: ignore[arg-type]
-                        metadata=get_metadata(self.token),
-                    ),
+                return await self.create_subscription(
+                    self._request(),
+                    metadata=get_metadata(self.token),
                 )
+
             except grpc.RpcError as e:
                 if e.code() == grpc.StatusCode.UNAVAILABLE:
                     retries = retries + 1
                 else:
                     raise ValueError(f"gRPC error: {e}")
 
-        raise ValueError("Failed to connect to workflow run listener")
+        raise ValueError("Failed to connect to listener")
