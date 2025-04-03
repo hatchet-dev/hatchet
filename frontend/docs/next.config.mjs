258e64a50bfe81c88923c8fa34682cc20fe5723a
mrkaye97 <mrkaye97@gmail.com>
2025-04-03 10:26:35 -0400

    fix: add a bunch of redirects

--- frontend/docs/next.config.mjs
+++ frontend/docs/next.config.mjs
@@ -40,6 +40,106 @@ const nextConfig = {
         destination: "https://app.posthog.com/:path*",
         permanent: false,
       },
+      {
+        source: "/home/basics/overview",
+        destination: "/home/setup",
+        permanent: false,
+      },
+      {
+        source: "/home/basics/(steps|workflows)",
+        destination: "/home/your-first-task",
+        permanent: false,
+      },
+      {
+        source: "/home/basics/environments",
+        destination: "/home/environments",
+        permanent: false,
+      },
+      {
+        source: "/home/features/concurrency/*",
+        destination: "/home/concurrency",
+        permanent: false,
+      },
+      {
+        source: "/home/features/durable-execution",
+        destination: "/home/durable-execution",
+        permanent: false,
+      },
+      {
+        source: "/home/features/retries/*",
+        destination: "/home/retry-policies",
+        permanent: false,
+      },
+      {
+        source: "/home/features/errors-and-logging",
+        destination: "/home/logging",
+        permanent: false,
+      },
+      {
+        source: "/home/features/on-failure-step",
+        destination: "/home/on-failure-tasks",
+        permanent: false,
+      },
+      {
+        source: "/home/features/triggering-runs/event-trigger",
+        destination: "/home/run-on-event",
+        permanent: false,
+      },
+      {
+        source: "/home/features/triggering-runs/cron-trigger",
+        destination: "/home/cron-runs",
+        permanent: false,
+      },
+      {
+        source: "/home/features/triggering-runs/schedule-trigger",
+        destination: "/home/scheduled-runs",
+        permanent: false,
+      },
+      {
+        source: "/home/features/rate-limits",
+        destination: "/home/rate-limits",
+        permanent: false,
+      },
+      {
+        source: "/home/features/worker-assignment/overview",
+        destination: "/home/sticky-assignment",
+        permanent: false,
+      },
+      {
+        source: "/home/features/worker-assignment/(overview|sticky-assignment)",
+        destination: "/home/sticky-assignment",
+        permanent: false,
+      },
+      {
+        source: "/home/features/worker-assignment/worker-affinity",
+        destination: "/home/worker-affinity",
+        permanent: false,
+      },
+      {
+        source: "/home/features/additional-metadata",
+        destination: "/home/additional-metadata",
+        permanent: false,
+      },
+      {
+        source: "/home/features/advanced/manual-slot-release",
+        destination: "/home/manual-slot-release",
+        permanent: false,
+      },
+      {
+        source: "/home/features/opentelemetry",
+        destination: "/home/opentelemetry",
+        permanent: false,
+      },
+      {
+        source: "/home/features/cancellation",
+        destination: "/home/cancellation",
+        permanent: false,
+      },
+      {
+        source: "/home/features/child-workflows",
+        destination: "/home/child-spawning",
+        permanent: false,
+      },
     ];
   },
 }
