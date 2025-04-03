d4056cf6c28354b3698532299ade6658ccf499ce
mrkaye97 <mrkaye97@gmail.com>
2025-04-03 19:18:27 -0400

    feat: tests

--- sdks/python/tests/test_client.py
+++ sdks/python/tests/test_client.py
@@ -1,3 +1,6 @@
+import os
+from unittest import mock
+
 from hatchet_sdk.config import ClientConfig
 
 
@@ -6,10 +9,27 @@ def test_client_initialization_from_defaults() -> None:
 
 
 def test_client_host_port_overrides() -> None:
-    host_port = "localhost:8080"
+    host_port = "foo:8080"
     with_host_port = ClientConfig(host_port=host_port)
+
     assert with_host_port.host_port == host_port
-    assert with_host_port.server_url == host_port
+    assert with_host_port.server_url == ClientConfig().server_url
 
     assert ClientConfig().host_port != host_port
     assert ClientConfig().server_url != host_port
+
+
+def test_client_host_port_override_when_env_var() -> None:
+    with mock.patch.dict(os.environ, {"HATCHET_CLIENT_HOST_PORT": "foo:8080"}):
+        config = ClientConfig()
+
+    assert config.host_port == "foo:8080"
+    assert config.server_url == ClientConfig().server_url
+
+
+def test_client_server_url_override_when_env_var() -> None:
+    with mock.patch.dict(os.environ, {"HATCHET_CLIENT_SERVER_URL": "foobaz:8080"}):
+        config = ClientConfig()
+
+    assert config.server_url == "foobaz:8080"
+    assert config.host_port == ClientConfig().host_port
