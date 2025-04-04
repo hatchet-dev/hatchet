4f1e26c37fce39271ee559104d9937f43017fbd9
mrkaye97 <mrkaye97@gmail.com>
2025-04-04 11:48:34 -0400

    feat: e2e test for priority

--- sdks/python/examples/priority/worker.py
+++ sdks/python/examples/priority/worker.py
@@ -1,20 +1,31 @@
 import time
 
-from hatchet_sdk import Context, EmptyModel, Hatchet
+from hatchet_sdk import (
+    ConcurrencyExpression,
+    ConcurrencyLimitStrategy,
+    Context,
+    EmptyModel,
+    Hatchet,
+)
 
 hatchet = Hatchet(debug=True)
 
 priority_workflow = hatchet.workflow(
-    name="PriorityWorkflow", default_priority=2
+    name="PriorityWorkflow",
+    default_priority=2,
+    concurrency=ConcurrencyExpression(
+        max_runs=1,
+        expression="'true'",
+        limit_strategy=ConcurrencyLimitStrategy.GROUP_ROUND_ROBIN,
+    ),
 )
 
-SLEEP_TIME = 3
+SLEEP_TIME = 1
 
 
 @priority_workflow.task()
 def priority_task(input: EmptyModel, ctx: Context) -> None:
     time.sleep(SLEEP_TIME)
-
 
 
 def main() -> None:
