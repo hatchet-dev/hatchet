69ba9f59a650aa95dbf724ac81033a90bba7eba6
mrkaye97 <mrkaye97@gmail.com>
2025-04-04 11:05:40 -0400

    feat: I think it works?

--- sdks/python/examples/priority/worker.py
+++ sdks/python/examples/priority/worker.py
@@ -4,35 +4,24 @@ from hatchet_sdk import Context, EmptyModel, Hatchet
 
 hatchet = Hatchet(debug=True)
 
-high_priority_workflow = hatchet.workflow(
-    name="HighPriorityWorkflow", default_priority=3
+priority_workflow = hatchet.workflow(
+    name="PriorityWorkflow", default_priority=2
 )
-low_priority_workflow = hatchet.workflow(name="LowPriorityWorkflow", default_priority=1)
-control_workflow = hatchet.workflow(name="ControlWorkflow", default_priority=1)
 
 SLEEP_TIME = 3
 
 
-@high_priority_workflow.task()
-def high_prio_task(input: EmptyModel, ctx: Context) -> None:
+@priority_workflow.task()
+def priority_task(input: EmptyModel, ctx: Context) -> None:
     time.sleep(SLEEP_TIME)
 
-
-@low_priority_workflow.task()
-def low_prio_task(input: EmptyModel, ctx: Context) -> None:
-    time.sleep(SLEEP_TIME)
-
-
-@control_workflow.task()
-def control_task(input: EmptyModel, ctx: Context) -> None:
-    time.sleep(SLEEP_TIME)
 
 
 def main() -> None:
     worker = hatchet.worker(
         "priority-worker",
         slots=1,
-        workflows=[low_priority_workflow, high_priority_workflow, control_workflow],
+        workflows=[priority_workflow],
     )
 
     worker.start()
