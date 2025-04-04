9f040a38dc94074b158032bbbc1531187f1dce19
mrkaye97 <mrkaye97@gmail.com>
2025-04-04 11:48:34 -0400

    feat: e2e test for priority

--- sdks/python/examples/priority/trigger.py
+++ sdks/python/examples/priority/trigger.py
@@ -3,7 +3,23 @@ from hatchet_sdk import TriggerWorkflowOptions
 
 priority_workflow.run_no_wait()
 
-low_prio = priority_workflow.run_no_wait(options=TriggerWorkflowOptions(priority=1, additional_metadata={"priority": "low", "key": 1}))
-low_prio = priority_workflow.run_no_wait(options=TriggerWorkflowOptions(priority=1, additional_metadata={"priority": "low", "key": 2}))
-high_prio = priority_workflow.run_no_wait(options=TriggerWorkflowOptions(priority=3, additional_metadata={"priority": "high", "key": 1}))
-high_prio = priority_workflow.run_no_wait(options=TriggerWorkflowOptions(priority=3, additional_metadata={"priority": "high", "key": 2}))
+low_prio = priority_workflow.run_no_wait(
+    options=TriggerWorkflowOptions(
+        priority=1, additional_metadata={"priority": "low", "key": 1}
+    )
+)
+low_prio = priority_workflow.run_no_wait(
+    options=TriggerWorkflowOptions(
+        priority=1, additional_metadata={"priority": "low", "key": 2}
+    )
+)
+high_prio = priority_workflow.run_no_wait(
+    options=TriggerWorkflowOptions(
+        priority=3, additional_metadata={"priority": "high", "key": 1}
+    )
+)
+high_prio = priority_workflow.run_no_wait(
+    options=TriggerWorkflowOptions(
+        priority=3, additional_metadata={"priority": "high", "key": 2}
+    )
+)
