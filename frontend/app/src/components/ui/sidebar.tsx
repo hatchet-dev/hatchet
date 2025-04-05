2ed895583ab68d87a7cd512b47bf3c75a8f7f7df
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-05 14:13:19 -0400

    responsive sb

--- frontend/app/src/components/ui/sidebar.tsx
+++ frontend/app/src/components/ui/sidebar.tsx
@@ -210,7 +210,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] !bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
             style={
               {
                 '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
