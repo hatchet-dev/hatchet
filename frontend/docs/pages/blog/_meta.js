4bee2f53ac75ed9fc8e237d0ad4a3e4835d5ce02
mrkaye97 <mrkaye97@gmail.com>
2025-04-15 16:10:51 -0400

    fix: tweaks

--- frontend/docs/pages/blog/_meta.js
+++ frontend/docs/pages/blog/_meta.js
@@ -6,7 +6,8 @@ export default {
     "title": "Why we moved off Prisma"
   },
   "problems-with-celery": {
-    "title": "The problems with Celery"
+    "title": "The problems with Celery",
+    "display": "hidden",
   },
   "multi-tenant-queues": {
     "title": "An unfair advantage: multi-tenant queues in Postgres"
