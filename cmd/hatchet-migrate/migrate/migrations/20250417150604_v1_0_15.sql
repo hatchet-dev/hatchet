5dc431f5ef03cc213a07becb094e6ebfa3e1a248
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-17 15:29:16 -0400

    fix: indexes

--- cmd/hatchet-migrate/migrate/migrations/20250417150604_v1_0_15.sql
+++ cmd/hatchet-migrate/migrate/migrations/20250417150604_v1_0_15.sql
@@ -2,10 +2,10 @@
 -- +goose NO TRANSACTION
 
 -- For v1_runs_olap
-CREATE INDEX IF NOT EXISTS idx_v1_runs_olap_external_id ON v1_runs_olap(parent_task_external_id);
+CREATE INDEX IF NOT EXISTS idx_v1_runs_olap_external_id ON v1_runs_olap(parent_task_external_id) WHERE parent_task_external_id IS NOT NULL;
 
 -- For v1_statuses_olap
-CREATE INDEX IF NOT EXISTS idx_v1_statuses_olap_query_optim ON v1_statuses_olap(tenant_id, inserted_at, workflow_id, readable_status);
+CREATE INDEX IF NOT EXISTS idx_v1_statuses_olap_query_optim ON v1_statuses_olap(tenant_id, inserted_at, workflow_id);
 
 -- +goose Down
 -- +goose NO TRANSACTION
