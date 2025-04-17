0b50b6296efb6f91d78ca927a53ca35b00a7688b
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-17 12:02:52 -0400

    remove statemtn

--- cmd/hatchet-migrate/migrate/migrations/20250417150604_v1_0_15.sql
+++ cmd/hatchet-migrate/migrate/migrations/20250417150604_v1_0_15.sql
@@ -1,20 +1,17 @@
 -- +goose Up
 -- +goose NO TRANSACTION
--- +goose StatementBegin
 
 -- For v1_runs_olap
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_v1_runs_olap_external_id ON v1_runs_olap(parent_task_external_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_v1_runs_olap_workflow_tenant ON v1_runs_olap(tenant_id, workflow_id);
+CREATE INDEX IF NOT EXISTS idx_v1_runs_olap_external_id ON v1_runs_olap(parent_task_external_id);
+CREATE INDEX IF NOT EXISTS idx_v1_runs_olap_workflow_tenant ON v1_runs_olap(tenant_id, workflow_id);
 
 -- For v1_statuses_olap
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_v1_statuses_olap_tenant_workflow_inserted ON v1_statuses_olap(tenant_id, workflow_id, inserted_at);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_v1_statuses_olap_external_id_status ON v1_statuses_olap(readable_status);
+CREATE INDEX IF NOT EXISTS idx_v1_statuses_olap_tenant_workflow_inserted ON v1_statuses_olap(tenant_id, workflow_id, inserted_at);
+CREATE INDEX IF NOT EXISTS idx_v1_statuses_olap_external_id_status ON v1_statuses_olap(readable_status);
 
--- +goose StatementEnd
 
 -- +goose Down
 -- +goose NO TRANSACTION
--- +goose StatementBegin
 
 -- For v1_runs_olap
 DROP INDEX IF EXISTS idx_v1_runs_olap_external_id;
@@ -24,4 +21,3 @@ DROP INDEX IF EXISTS idx_v1_runs_olap_workflow_tenant;
 DROP INDEX IF EXISTS idx_v1_statuses_olap_tenant_workflow_inserted;
 DROP INDEX IF EXISTS idx_v1_statuses_olap_external_id_status;
 
--- +goose StatementEnd
