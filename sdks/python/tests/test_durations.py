25d242c0ae1e2cd221fa04e6fdf8aab59b46588e
mrkaye97 <mrkaye97@gmail.com>
2025-04-18 14:12:30 -0400

    fix: linter

--- sdks/python/tests/test_durations.py
+++ sdks/python/tests/test_durations.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from hatchet_sdk.utils.timedelta_to_expression import timedelta_to_expr
 
 
-def test_timedelta_to_expr():
+def test_timedelta_to_expr() -> None:
     assert timedelta_to_expr(timedelta(seconds=3600)) == "1h"
     assert timedelta_to_expr(timedelta(seconds=60)) == "1m"
     assert timedelta_to_expr(timedelta(seconds=1)) == "1s"
