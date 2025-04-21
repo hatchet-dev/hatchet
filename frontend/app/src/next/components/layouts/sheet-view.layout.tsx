55632df2d1f3696e977fb7704561aeb2892c001e
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-21 14:39:59 -0400

    fix nits

--- frontend/app/src/next/components/layouts/sheet-view.layout.tsx
+++ frontend/app/src/next/components/layouts/sheet-view.layout.tsx
@@ -7,7 +7,7 @@ interface SheetViewLayoutProps {
 
 export function SheetViewLayout({ children, sheet }: SheetViewLayoutProps) {
   return (
-    <div className="flex h-full w-full flex-row gap-4">
+    <div className="flex h-full w-full flex-row">
       <div className="overflow-y-auto flex-grow p-4 md:p-8 lg:p-12">
         {children}
       </div>
