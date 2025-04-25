a780041ca52af53df6deb8c900f85075bc9aeddd
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-25 11:54:56 -0400

    get snips into app

---
+++ frontend/app/src/next/components/ui/code/snippet.tsx
@@ -0,0 +1,37 @@
+import { CodeBlock } from './code-block';
+import { snippets } from '@/next/lib/docs/snips';
+
+interface GithubSnippetProps {
+  src: string;
+  highlightLines?: number[];
+  showLineNumbers?: boolean;
+  title?: string;
+}
+
+// This is a server component that will be rendered at build time
+export const Snippet = ({ src, ...props }: GithubSnippetProps) => {
+  if (!src) {
+    throw new Error('src is required');
+  }
+
+  const [, filePath] = src.split(':');
+
+  // Get the snippet content from the snippets object
+  const snippet = snippets[filePath];
+  if (!snippet) {
+    throw new Error(`Snippet content not found: ${filePath}`);
+  }
+
+  return (
+    <>
+      <CodeBlock
+        value={snippet.content}
+        language={snippet.language}
+        //   highlightLines={snippet.highlights}
+        {...props}
+        title={props.title || snippet.source}
+        link={`https://github.com/hatchet-dev/hatchet/blob/main/${snippet.source}`}
+      />
+    </>
+  );
+};
--- frontend/app/src/next/lib/docs/index.ts
+++ frontend/app/src/next/lib/docs/index.ts
@@ -1,4 +1,6 @@
 // Re-export all meta data from generated directory
 import * as docs from './generated';
+import { snippets } from './snips';
 
 export default docs;
+export { snippets };
