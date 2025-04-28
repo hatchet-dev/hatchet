f6f069b7a1a75d8c136291100ee9eeac2b50b8b8
mrkaye97 <mrkaye97@gmail.com>
2025-04-28 16:32:07 -0400

    fix: inner join

--- pkg/repository/v1/sqlcv1/olap.sql
+++ pkg/repository/v1/sqlcv1/olap.sql
@@ -1292,5 +1292,5 @@ SELECT
     i.event_id,
     i.event_inserted_at
 FROM inputs i
-LEFT JOIN v1_lookup_table_olap lt ON lt.external_id = i.run_external_id
+JOIN v1_lookup_table_olap lt ON lt.external_id = i.run_external_id
 RETURNING *;
