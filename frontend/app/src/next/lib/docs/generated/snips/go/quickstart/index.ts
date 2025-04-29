5613e4c22c5fb741c8a75b6b9e72d5d1b962cd7b
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-29 07:59:20 -0400

    dont test examples

--- frontend/app/src/next/lib/docs/generated/snips/go/quickstart/index.ts
+++ frontend/app/src/next/lib/docs/generated/snips/go/quickstart/index.ts
@@ -1,11 +1,7 @@
-import readme from './README';
-import gitignore from './gitignore';
 import * as cmd from './cmd';
 import * as hatchet_client from './hatchet_client';
 import * as workflows from './workflows';
 
-export { readme }
-export { gitignore }
 export { cmd };
 export { hatchet_client };
 export { workflows };
