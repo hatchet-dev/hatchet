5538c723d1a3d2c922e39aa41b4c9e151c766008
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-04-29 09:22:45 -0400

    dont break examples

--- examples/go/workflows/timeouts.go
+++ examples/go/workflows/timeouts.go
@@ -18,6 +18,7 @@ type TimeoutResult struct {
 
 func Timeout(hatchet v1.HatchetClient) workflow.WorkflowDeclaration[TimeoutInput, TimeoutResult] {
 
+	// > Execution Timeout
 	// Create a task with a timeout of 3 seconds that tries to sleep for 10 seconds
 	timeout := factory.NewTask(
 		create.StandaloneTask{
@@ -30,7 +31,40 @@ func Timeout(hatchet v1.HatchetClient) workflow.WorkflowDeclaration[TimeoutInput
 			// Check if the context was cancelled due to timeout
 			select {
 			case <-ctx.Done():
-				return nil, errors.New("Task timed out")
+				return nil, errors.New("TASK TIMED OUT")
+			default:
+				// Continue execution
+			}
+
+			return &TimeoutResult{
+				Completed: true,
+			}, nil
+		},
+		hatchet,
+	)
+
+	return timeout
+}
+
+func RefreshTimeout(hatchet v1.HatchetClient) workflow.WorkflowDeclaration[TimeoutInput, TimeoutResult] {
+
+	// > Refresh Timeout
+	timeout := factory.NewTask(
+		create.StandaloneTask{
+			Name:             "timeout-task",
+			ExecutionTimeout: 3 * time.Second, // Task will timeout after 3 seconds
+		}, func(ctx worker.HatchetContext, input TimeoutInput) (*TimeoutResult, error) {
+
+			// Refresh the timeout by 10 seconds (new timeout will be 13 seconds)
+			ctx.RefreshTimeout("10s")
+
+			// Sleep for 10 seconds
+			time.Sleep(10 * time.Second)
+
+			// Check if the context was cancelled due to timeout
+			select {
+			case <-ctx.Done():
+				return nil, errors.New("TASK TIMED OUT")
 			default:
 				// Continue execution
 			}
