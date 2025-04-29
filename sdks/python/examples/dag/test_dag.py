08121381bba0a3d55d4a3b3641b80bf266ed2ac4
mrkaye97 <mrkaye97@gmail.com>
2025-04-29 12:03:04 -0400

    debug: dummy change

--- sdks/python/examples/dag/test_dag.py
+++ sdks/python/examples/dag/test_dag.py
@@ -10,5 +10,6 @@ async def test_run(hatchet: Hatchet) -> None:
 
     one = result["step1"]["random_number"]
     two = result["step2"]["random_number"]
+
     assert result["step3"]["sum"] == one + two
     assert result["step4"]["step4"] == "step4"
