0bdc9b830b21cea011006e9181cc743206b174d4
mrkaye97 <mrkaye97@gmail.com>
2025-04-30 12:05:10 -0400

    debug: more retries

--- sdks/python/examples/priority/test_priority.py
+++ sdks/python/examples/priority/test_priority.py
@@ -227,7 +227,7 @@ async def crons(
 ) -> AsyncGenerator[tuple[str, str, int], None]:
     test_run_id = str(uuid4())
     choices: list[Priority] = ["low", "medium", "high"]
-    n = 25
+    n = 30
 
     crons = await asyncio.gather(
         *[
