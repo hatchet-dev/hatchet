8b81e78a31ace8c636c5498c4a75e1bd2ff7a54f
Alexander Belanger <alexander@hatchet.run>
2025-04-30 09:01:30 -0400

    fix: initialize otel in api runner

--- cmd/hatchet-api/api/run.go
+++ cmd/hatchet-api/api/run.go
@@ -2,10 +2,39 @@ package api
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/hatchet-dev/hatchet/api/v1/server/run"
+	"github.com/hatchet-dev/hatchet/internal/telemetry"
 	"github.com/hatchet-dev/hatchet/pkg/config/loader"
 )
+
+func init() {
+	svcName := os.Getenv("SERVER_OTEL_SERVICE_NAME")
+	collectorURL := os.Getenv("SERVER_OTEL_COLLECTOR_URL")
+	insecure := os.Getenv("SERVER_OTEL_INSECURE")
+	traceIDRatio := os.Getenv("SERVER_OTEL_TRACE_ID_RATIO")
+
+	var insecureBool bool
+
+	if insecureStr := strings.ToLower(strings.TrimSpace(insecure)); insecureStr == "t" || insecureStr == "true" {
+		insecureBool = true
+	}
+
+	// we do this to we get the tracer set globally, which is needed by some of the otel
+	// integrations for the database before start
+	_, err := telemetry.InitTracer(&telemetry.TracerOpts{
+		ServiceName:  svcName,
+		CollectorURL: collectorURL,
+		TraceIdRatio: traceIDRatio,
+		Insecure:     insecureBool,
+	})
+
+	if err != nil {
+		panic(fmt.Errorf("could not initialize tracer: %w", err))
+	}
+}
 
 func Start(cf *loader.ConfigLoader, interruptCh <-chan interface{}, version string) error {
 	// init the repository
