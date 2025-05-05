c10bf2ee553e177d15bd30d4bec6b798958bd151
mrkaye97 <mrkaye97@gmail.com>
2025-05-05 18:02:39 -0400

    fix: required param

--- api/v1/server/oas/transformers/v1/events.go
+++ api/v1/server/oas/transformers/v1/events.go
@@ -26,14 +26,14 @@ func ToV1EventList(events []*sqlcv1.ListEventsRow) gen.V1EventList {
 				UpdatedAt: row.EventGeneratedAt.Time,
 				Id:        strconv.FormatInt(row.EventID, 10),
 			},
-			WorkflowRunSummary: &gen.V1EventWorkflowRunSummary{
+			WorkflowRunSummary: gen.V1EventWorkflowRunSummary{
 				Cancelled: row.CancelledCount.Int64,
 				Succeeded: row.CompletedCount.Int64,
 				Queued:    row.QueuedCount.Int64,
 				Failed:    row.FailedCount.Int64,
 				Running:   row.RunningCount.Int64,
 			},
-			ExternalId: &externalId,
+			ExternalId: externalId,
 		}
 	}
 
