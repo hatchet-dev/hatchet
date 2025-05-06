f256c09af9247ec5ee631ce11508174b2564ccf5
mrkaye97 <mrkaye97@gmail.com>
2025-05-06 12:00:45 -0400

    chore: gen

--- pkg/repository/v1/sqlcv1/models.go
+++ pkg/repository/v1/sqlcv1/models.go
@@ -2482,7 +2482,7 @@ type V1DurableSleep struct {
 type V1EventToRunOlap struct {
 	RunID         int64              `json:"run_id"`
 	RunInsertedAt pgtype.Timestamptz `json:"run_inserted_at"`
-	EventID       int64              `json:"event_id"`
+	EventID       pgtype.UUID        `json:"event_id"`
 	EventSeenAt   pgtype.Timestamptz `json:"event_seen_at"`
 }
 
