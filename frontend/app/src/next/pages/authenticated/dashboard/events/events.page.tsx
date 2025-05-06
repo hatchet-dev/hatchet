b07decfe7049c3a2f769af96a0b7b66c32239947
mrkaye97 <mrkaye97@gmail.com>
2025-05-06 16:45:24 -0400

    tweak: table

--- frontend/app/src/next/pages/authenticated/dashboard/events/events.page.tsx
+++ frontend/app/src/next/pages/authenticated/dashboard/events/events.page.tsx
@@ -82,33 +82,9 @@ function EventsContent() {
 export const columns = (): ColumnDef<V1Event>[] => {
   return [
     {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && 'indeterminate')
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-          className="translate-y-[2px]"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-          className="translate-y-[2px]"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
       accessorKey: 'EventId',
       header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="ID" />
+        <DataTableColumnHeader column={column} title="ID" className="pl-4" />
       ),
       cell: ({ row }) => (
         <div className="w-full">
