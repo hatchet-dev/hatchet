4c4ce6430fc6561c31dce936910084228a3296b3
mrkaye97 <mrkaye97@gmail.com>
2025-05-06 11:50:21 -0400

    fix: uuid pk

--- cmd/hatchet-migrate/migrate/migrations/20250428175026_v1_0_17.sql
+++ cmd/hatchet-migrate/migrate/migrations/20250428175026_v1_0_17.sql
@@ -2,27 +2,24 @@
 -- +goose StatementBegin
 CREATE TABLE v1_events_olap (
     tenant_id UUID NOT NULL,
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    inserted_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    generated_at TIMESTAMPTZ NOT NULL,
-    external_id UUID NOT NULL,
+    id UUID NOT NULL,
+    seen_at TIMESTAMPTZ NOT NULL,
     key TEXT NOT NULL,
     payload JSONB NOT NULL,
     additional_metadata JSONB,
 
-    PRIMARY KEY (tenant_id, id, inserted_at)
-) PARTITION BY RANGE(inserted_at);
-
-CREATE INDEX v1_events_olap_external_id_idx ON v1_events_olap (tenant_id, external_id);
+    PRIMARY KEY (tenant_id, id, seen_at)
+) PARTITION BY RANGE(seen_at);
 
 CREATE TABLE v1_event_to_run_olap (
     run_id BIGINT NOT NULL,
     run_inserted_at TIMESTAMPTZ NOT NULL,
     event_id BIGINT NOT NULL,
-    event_inserted_at TIMESTAMPTZ NOT NULL,
+    event_seen_at TIMESTAMPTZ NOT NULL,
 
-    PRIMARY KEY (event_id, event_inserted_at, run_id, run_inserted_at)
-) PARTITION BY RANGE(event_inserted_at);
+    PRIMARY KEY (event_id, event_seen_at, run_id, run_inserted_at)
+) PARTITION BY RANGE(event_seen_at);
+
 
 SELECT create_v1_range_partition('v1_events_olap', DATE 'today');
 SELECT create_v1_range_partition('v1_event_to_run_olap', DATE 'today');
