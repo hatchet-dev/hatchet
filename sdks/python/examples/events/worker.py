83adcd9ae63d66411f77f3cd07c5837e054f15da
mrkaye97 <mrkaye97@gmail.com>
2025-05-08 18:23:47 -0400

    feat: e2e test

--- sdks/python/examples/events/worker.py
+++ sdks/python/examples/events/worker.py
@@ -1,14 +1,26 @@
-from hatchet_sdk import Context, EmptyModel, Hatchet
+from pydantic import BaseModel
+
+from hatchet_sdk import Context, Hatchet
+
+
+class EventWorkflowInput(BaseModel):
+    should_skip: bool
+
 
 hatchet = Hatchet()
 
 # > Event trigger
-event_workflow = hatchet.workflow(name="EventWorkflow", on_events=["user:create"])
+event_workflow = hatchet.workflow(
+    name="EventWorkflow",
+    on_events=["user:create"],
+    event_filter_expression="input.should_skip == true",
+    input_validator=EventWorkflowInput,
+)
 # !!
 
 
 @event_workflow.task()
-def task(input: EmptyModel, ctx: Context) -> None:
+def task(input: EventWorkflowInput, ctx: Context) -> None:
     print("event received")
 
 
