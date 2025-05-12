ab595b4303e9854b8d6e660903c2596ef46ab817
mrkaye97 <mrkaye97@gmail.com>
2025-05-12 16:39:35 -0400

    chore: gen again

--- frontend/app/src/next/lib/docs/generated/snips/python/events/worker.ts
+++ frontend/app/src/next/lib/docs/generated/snips/python/events/worker.ts
@@ -3,7 +3,7 @@ import { Snippet } from '@/next/lib/docs/generated/snips/types';
 const snippet: Snippet = {
   language: 'python',
   content:
-    "from hatchet_sdk import Context, EmptyModel, Hatchet\n\nhatchet = Hatchet()\n\n# > Event trigger\nevent_workflow = hatchet.workflow(name='EventWorkflow', on_events=['user:create'])\n\n\n@event_workflow.task()\ndef task(input: EmptyModel, ctx: Context) -> None:\n    print('event received')\n\ndef main() -> None:\n    worker = hatchet.worker(name='EventWorker', workflows=[event_workflow])\n\n    worker.start()\n\nif __name__ == '__main__':\n    main()",
+    "from hatchet_sdk import Context, EmptyModel, Hatchet\n\nhatchet = Hatchet()\n\n# > Event trigger\nevent_workflow = hatchet.workflow(name='EventWorkflow', on_events=['user:create'])\n\n\n@event_workflow.task()\ndef task(input: EmptyModel, ctx: Context) -> None:\n    print('event received')\n\n\ndef main() -> None:\n    worker = hatchet.worker(name='EventWorker', workflows=[event_workflow])\n\n    worker.start()\n\n\nif __name__ == '__main__':\n    main()\n",
   source: 'out/python/events/worker.py',
   blocks: {
     event_trigger: {
