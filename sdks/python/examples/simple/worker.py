a48dc34822b02b080cead9b0b733d5a3751d5387
mrkaye97 <mrkaye97@gmail.com>
2025-05-13 20:26:08 -0400

    Revert "fix: rm _aio_output helper"
    This reverts commit ff51126c4379ed2a69226be0785f983bd27b399c.

--- sdks/python/examples/simple/worker.py
+++ sdks/python/examples/simple/worker.py
@@ -6,14 +6,8 @@ hatchet = Hatchet(debug=True)
 
 
 @hatchet.task(name="SimpleWorkflow")
-async def step1(input: EmptyModel, ctx: Context) -> dict[str, str]:
+def step1(input: EmptyModel, ctx: Context) -> None:
     print("executed step1")
-    return {
-        "step1": "step1",
-    }
-
-
-x = step1.run()
 
 
 def main() -> None:
