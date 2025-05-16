74aae469f005cc76e6d51cafa9d6b4a4b6848713
mrkaye97 <mrkaye97@gmail.com>
2025-05-16 14:36:12 -0400

    chore: gen

--- frontend/app/src/next/lib/docs/generated/snips/python/simple/trigger.ts
+++ frontend/app/src/next/lib/docs/generated/snips/python/simple/trigger.ts
@@ -2,8 +2,7 @@ import { Snippet } from '@/next/lib/docs/generated/snips/types';
 
 const snippet: Snippet = {
   language: 'python',
-  content:
-    "from examples.simple.worker import hatchet\nfrom hatchet_sdk.clients.admin import CreateFilterRequest\nfrom hatchet_sdk.clients.events import PushEventOptions\n\nhatchet._client.admin.put_filter(\n    CreateFilterRequest(\n        workflow_id='1563ea96-3d4a-4b62-ae68-94bb11ea3bb1',\n        expression='input.shouldSkipThis == true',\n        resource_hint='foobar',\n        payload={'test': 'test'},\n    )\n)\n\nhatchet.event.push(\n    'workflow-filters:test:1',\n    {'shouldSkipThis': True},\n    PushEventOptions(\n        resource_hint='foobar',\n        additional_metadata={\n            'shouldSkipThis': True,\n        },\n    ),\n)\n\nhatchet.event.push(\n    'workflow-filters:test:1',\n    {'shouldSkipThis': False},\n    PushEventOptions(\n        resource_hint='foobar',\n        additional_metadata={\n            'shouldSkipThis': False,\n        },\n    ),\n)\n",
+  content: 'from examples.simple.worker import simple\n\nsimple.run()\n',
   source: 'out/python/simple/trigger.py',
   blocks: {},
   highlights: {},
--- frontend/docs/lib/generated/snips/python/events/event.ts
+++ frontend/docs/lib/generated/snips/python/events/event.ts
@@ -2,14 +2,9 @@ import { Snippet } from '@/lib/generated/snips/types';
 
 const snippet: Snippet = {
   'language': 'python',
-  'content': 'from hatchet_sdk import Hatchet\n\nhatchet = Hatchet()\n\n# > Event trigger\nhatchet.event.push(\'user:create\', {})\n',
+  'content': 'from examples.events.worker import EVENT_KEY, event_workflow, hatchet\nfrom hatchet_sdk.clients.events import PushEventOptions\n\nSCOPE = \'foobar\'\nhatchet.filters.create(\n    workflow_id=event_workflow.id,\n    expression=\'input.should_skip == false\',\n    scope=SCOPE,\n    payload={\'test\': \'test\'},\n)\n\nhatchet.event.push(\n    EVENT_KEY,\n    {\'should_skip\': True},\n    PushEventOptions(\n        ## If no scope is provided, all events pushed will trigger workflows\n        ## (i.e. the filter will not apply)\n        scope=SCOPE,\n        additional_metadata={\n            \'should_skip\': True,\n        },\n    ),\n)\n\nhatchet.event.push(\n    EVENT_KEY,\n    {\'should_skip\': False},\n    PushEventOptions(\n        ## If no scope is provided, all events pushed will trigger workflows\n        ## (i.e. the filter will not apply)\n        scope=SCOPE,\n        additional_metadata={\n            \'should_skip\': False,\n        },\n    ),\n)\n',
   'source': 'out/python/events/event.py',
-  'blocks': {
-    'event_trigger': {
-      'start': 6,
-      'stop': 6
-    }
-  },
+  'blocks': {},
   'highlights': {}
 };  // Then replace double quotes with single quotes
 
