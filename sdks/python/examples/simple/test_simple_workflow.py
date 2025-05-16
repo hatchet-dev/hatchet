0174ceb87f587994892004463c1bd94f3b3cc819
mrkaye97 <mrkaye97@gmail.com>
2025-05-16 08:37:09 -0400

    fix: refs

--- sdks/python/examples/simple/test_simple_workflow.py
+++ sdks/python/examples/simple/test_simple_workflow.py
@@ -1,33 +1,35 @@
 import pytest
 
-from examples.simple.worker import step1
+from examples.simple.worker import simple
 
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_simple_workflow_running_options() -> None:
-    x1 = step1.run()
-    x2 = await step1.aio_run()
+    x1 = simple.run()
+    x2 = await simple.aio_run()
 
-    x3 = step1.run_many([step1.create_bulk_run_item()])[0]
-    x4 = (await step1.aio_run_many([step1.create_bulk_run_item()]))[0]
+    x3 = simple.run_many([simple.create_bulk_run_item()])[0]
+    x4 = (await simple.aio_run_many([simple.create_bulk_run_item()]))[0]
 
-    x5 = step1.run_no_wait().result()
-    x6 = (await step1.aio_run_no_wait()).result()
-    x7 = [x.result() for x in step1.run_many_no_wait([step1.create_bulk_run_item()])][0]
+    x5 = simple.run_no_wait().result()
+    x6 = (await simple.aio_run_no_wait()).result()
+    x7 = [x.result() for x in simple.run_many_no_wait([simple.create_bulk_run_item()])][
+        0
+    ]
     x8 = [
         x.result()
-        for x in await step1.aio_run_many_no_wait([step1.create_bulk_run_item()])
+        for x in await simple.aio_run_many_no_wait([simple.create_bulk_run_item()])
     ][0]
 
-    x9 = await step1.run_no_wait().aio_result()
-    x10 = await (await step1.aio_run_no_wait()).aio_result()
+    x9 = await simple.run_no_wait().aio_result()
+    x10 = await (await simple.aio_run_no_wait()).aio_result()
     x11 = [
         await x.aio_result()
-        for x in step1.run_many_no_wait([step1.create_bulk_run_item()])
+        for x in simple.run_many_no_wait([simple.create_bulk_run_item()])
     ][0]
     x12 = [
         await x.aio_result()
-        for x in await step1.aio_run_many_no_wait([step1.create_bulk_run_item()])
+        for x in await simple.aio_run_many_no_wait([simple.create_bulk_run_item()])
     ][0]
 
     assert all(
--- sdks/python/examples/simple/worker.py
+++ sdks/python/examples/simple/worker.py
@@ -5,13 +5,13 @@ from hatchet_sdk import Context, EmptyModel, Hatchet
 hatchet = Hatchet(debug=True)
 
 
-@hatchet.task(name="SimpleWorkflow")
-def step1(input: EmptyModel, ctx: Context) -> dict[str, str]:
+@hatchet.task()
+def simple(input: EmptyModel, ctx: Context) -> dict[str, str]:
     return {"result": "Hello, world!"}
 
 
 def main() -> None:
-    worker = hatchet.worker("test-worker", workflows=[step1])
+    worker = hatchet.worker("test-worker", workflows=[simple])
     worker.start()
 
 
