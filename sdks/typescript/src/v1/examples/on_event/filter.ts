d748b5b15d239d6a60108f3a1bca5fb24d295b4f
mrkaye97 <mrkaye97@gmail.com>
2025-05-19 17:38:03 -0400

    fix: lint

--- sdks/typescript/src/v1/examples/on_event/filter.ts
+++ sdks/typescript/src/v1/examples/on_event/filter.ts
@@ -1,41 +1,41 @@
-import { hatchet } from "../hatchet-client";
-import { lower, SIMPLE_EVENT } from "./workflow";
+import { hatchet } from '../hatchet-client';
+import { lower, SIMPLE_EVENT } from './workflow';
 
 // > Create a filter
 hatchet.filters.create({
-    workflowId: lower.id,
-    expression: "input.ShouldSkip == false",
-    scope: "foobarbaz",
-    payload: {
-        main_character: "Anna",
-        supporting_character: "Stiva",
-        location: "Moscow",
-    },
-})
+  workflowId: lower.id,
+  expression: 'input.ShouldSkip == false',
+  scope: 'foobarbaz',
+  payload: {
+    main_character: 'Anna',
+    supporting_character: 'Stiva',
+    location: 'Moscow',
+  },
+});
 // !!
 
 // > Skip a run
 hatchet.events.push(
-    SIMPLE_EVENT,
-    {
-        "Message": "hello",
-        "ShouldSkip": true,
-    },
-    {
-        scope: "foobarbaz",
-    }
-)
+  SIMPLE_EVENT,
+  {
+    Message: 'hello',
+    ShouldSkip: true,
+  },
+  {
+    scope: 'foobarbaz',
+  }
+);
 // !!
 
 // > Trigger a run
 hatchet.events.push(
-    SIMPLE_EVENT,
-    {
-        "Message": "hello",
-        "ShouldSkip": false,
-    },
-    {
-        scope: "foobarbaz",
-    }
-)
+  SIMPLE_EVENT,
+  {
+    Message: 'hello',
+    ShouldSkip: false,
+  },
+  {
+    scope: 'foobarbaz',
+  }
+);
 // !!
