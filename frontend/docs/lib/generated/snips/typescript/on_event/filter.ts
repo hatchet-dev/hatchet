638f457e80f5eab074660eb99532d481e69c9667
mrkaye97 <mrkaye97@gmail.com>
2025-05-19 17:38:19 -0400

    fix: gen

--- frontend/docs/lib/generated/snips/typescript/on_event/filter.ts
+++ frontend/docs/lib/generated/snips/typescript/on_event/filter.ts
@@ -2,7 +2,7 @@ import { Snippet } from '@/lib/generated/snips/types';
 
 const snippet: Snippet = {
   'language': 'typescript ',
-  'content': 'import { hatchet } from \'../hatchet-client\';\nimport { lower, SIMPLE_EVENT } from \'./workflow\';\n\n// > Create a filter\nhatchet.filters.create({\n    workflowId: lower.id,\n    expression: \'input.ShouldSkip == false\',\n    scope: \'foobarbaz\',\n    payload: {\n        main_character: \'Anna\',\n        supporting_character: \'Stiva\',\n        location: \'Moscow\',\n    },\n})\n\n// > Skip a run\nhatchet.events.push(\n    SIMPLE_EVENT,\n    {\n        \'Message\': \'hello\',\n        \'ShouldSkip\': true,\n    },\n    {\n        scope: \'foobarbaz\',\n    }\n)\n\n// > Trigger a run\nhatchet.events.push(\n    SIMPLE_EVENT,\n    {\n        \'Message\': \'hello\',\n        \'ShouldSkip\': false,\n    },\n    {\n        scope: \'foobarbaz\',\n    }\n)',
+  'content': 'import { hatchet } from \'../hatchet-client\';\nimport { lower, SIMPLE_EVENT } from \'./workflow\';\n\n// > Create a filter\nhatchet.filters.create({\n  workflowId: lower.id,\n  expression: \'input.ShouldSkip == false\',\n  scope: \'foobarbaz\',\n  payload: {\n    main_character: \'Anna\',\n    supporting_character: \'Stiva\',\n    location: \'Moscow\',\n  },\n});\n\n// > Skip a run\nhatchet.events.push(\n  SIMPLE_EVENT,\n  {\n    Message: \'hello\',\n    ShouldSkip: true,\n  },\n  {\n    scope: \'foobarbaz\',\n  }\n);\n\n// > Trigger a run\nhatchet.events.push(\n  SIMPLE_EVENT,\n  {\n    Message: \'hello\',\n    ShouldSkip: false,\n  },\n  {\n    scope: \'foobarbaz\',\n  }\n);\n',
   'source': 'out/typescript/on_event/filter.ts',
   'blocks': {
     'create_a_filter': {
