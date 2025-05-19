2244a48c95fab192f52036fb926f4acbf00a6830
mrkaye97 <mrkaye97@gmail.com>
2025-05-19 15:50:43 -0400

    chore: gen yet again

--- frontend/docs/lib/generated/snips/go/run/event.ts
+++ frontend/docs/lib/generated/snips/go/run/event.ts
@@ -2,12 +2,12 @@ import { Snippet } from '@/lib/generated/snips/types';
 
 const snippet: Snippet = {
   'language': 'go',
-  'content': 'package main\n\nimport (\n\t\'context\'\n\n\tv1_workflows \'github.com/hatchet-dev/hatchet/examples/go/workflows\'\n\tv1 \'github.com/hatchet-dev/hatchet/pkg/v1\'\n\t\'github.com/joho/godotenv\'\n)\n\nfunc event() {\n\terr := godotenv.Load()\n\tif err != nil {\n\t\tpanic(err)\n\t}\n\n\thatchet, err := v1.NewHatchetClient()\n\n\tif err != nil {\n\t\tpanic(err)\n\t}\n\t// > Pushing an Event\n\terr = hatchet.Events().Push(\n\t\tcontext.Background(),\n\t\t\'simple-event:create\',\n\t\tv1_workflows.SimpleInput{\n\t\t\tMessage: \'Hello, World!\',\n\t\t},\n\t\tnil,\n\t\tnil,\n\t)\n\n\tif err != nil {\n\t\tpanic(err)\n\t}\n}\n',
+  'content': 'package main\n\nimport (\n\t\'context\'\n\n\tv1_workflows \'github.com/hatchet-dev/hatchet/examples/go/workflows\'\n\tv1 \'github.com/hatchet-dev/hatchet/pkg/v1\'\n\t\'github.com/joho/godotenv\'\n)\n\nfunc event() {\n\terr := godotenv.Load()\n\tif err != nil {\n\t\tpanic(err)\n\t}\n\n\thatchet, err := v1.NewHatchetClient()\n\n\tif err != nil {\n\t\tpanic(err)\n\t}\n\t// > Pushing an Event\n\terr = hatchet.Events().Push(\n\t\tcontext.Background(),\n\t\t\'simple-event:create\',\n\t\tv1_workflows.SimpleInput{\n\t\t\tMessage: \'Hello, World!\',\n\t\t},\n\t)\n\n\tif err != nil {\n\t\tpanic(err)\n\t}\n}\n',
   'source': 'out/go/run/event.go',
   'blocks': {
     'pushing_an_event': {
       'start': 23,
-      'stop': 31
+      'stop': 29
     }
   },
   'highlights': {}
