80a4757a5d2be276f46bfdc7ce9aed7f12692e0f
Gabe Ruttner <gabriel.ruttner@gmail.com>
2025-05-19 11:37:49 -0700

    ident from ref (#1751)

--- frontend/app/src/next/main.tsx
+++ frontend/app/src/next/main.tsx
@@ -23,6 +23,14 @@ if (import.meta.env.VITE_SENTRY_DSN) {
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
+
+  // Store referral code from URL if present
+  // See AnalyticsProvider for more details
+  const urlParams = new URLSearchParams(window.location.search);
+  const ref = urlParams.get('ref');
+  if (ref) {
+    localStorage.setItem('ref', ref);
+  }
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
