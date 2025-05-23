9edfbc90e250bbcebe3391e3289911e4a08b8606
mrkaye97 <mrkaye97@gmail.com>
2025-05-23 17:48:17 -0400

    chore: gen

--- frontend/docs/lib/generated/snips/python/blocked_async/blocking_example_trigger.ts
+++ frontend/docs/lib/generated/snips/python/blocked_async/blocking_example_trigger.ts
@@ -2,12 +2,12 @@ import { Snippet } from '@/lib/generated/snips/types';
 
 const snippet: Snippet = {
   "language": "python",
-  "content": "# > Trigger\nfrom examples.blocked_async.blocking_example_worker import non_blocking_async, non_blocking_sync, blocking\nimport time\n\nnon_blocking_sync.run_no_wait()\nnon_blocking_async.run_no_wait()\n\ntime.sleep(1)\n\nblocking.run_no_wait()\n\ntime.sleep(1)\n\nnon_blocking_sync.run_no_wait()\n",
+  "content": "# > Trigger\nimport time\n\nfrom examples.blocked_async.blocking_example_worker import (\n    blocking,\n    non_blocking_async,\n    non_blocking_sync,\n)\n\nnon_blocking_sync.run_no_wait()\nnon_blocking_async.run_no_wait()\n\ntime.sleep(1)\n\nblocking.run_no_wait()\n\ntime.sleep(1)\n\nnon_blocking_sync.run_no_wait()\n\n",
   "source": "out/python/blocked_async/blocking_example_trigger.py",
   "blocks": {
     "trigger": {
       "start": 2,
-      "stop": 15
+      "stop": 20
     }
   },
   "highlights": {}
