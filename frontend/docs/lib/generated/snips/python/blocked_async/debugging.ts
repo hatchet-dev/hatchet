99ca2d826669abe893b20754df6ef9e56fc7b573
mrkaye97 <mrkaye97@gmail.com>
2025-05-23 17:48:17 -0400

    chore: gen

--- frontend/docs/lib/generated/snips/python/blocked_async/debugging.ts
+++ frontend/docs/lib/generated/snips/python/blocked_async/debugging.ts
@@ -2,20 +2,20 @@ import { Snippet } from '@/lib/generated/snips/types';
 
 const snippet: Snippet = {
   "language": "python",
-  "content": "# > Functions\nimport asyncio\nimport time\n\nsleep_time = 3\n\nasync def blocking() -> None:\n    for i in range(sleep_time):\n        print(\"Blocking\", i)\n        time.sleep(1)\n\nasync def non_blocking(task_id: str = \"Non-blocking\") -> None:\n    for i in range(sleep_time):\n        print(task_id, i)\n        await asyncio.sleep(1)\n\n# > Blocked\nasync def blocked() -> None:\n    loop = asyncio.get_event_loop()\n\n    await asyncio.gather(*[\n        loop.create_task(blocking()),\n        loop.create_task(non_blocking()),\n    ])\n\n# > Unblocked\nasync def working() -> None:\n    loop = asyncio.get_event_loop()\n\n    await asyncio.gather(*[\n        loop.create_task(non_blocking(\"A\")),\n        loop.create_task(non_blocking(\"B\")),\n    ])\n\n\nif __name__ == \"__main__\":\n    asyncio.run(blocked())\n    asyncio.run(working())\n",
+  "content": "# > Functions\nimport asyncio\nimport time\n\nsleep_time = 3\n\n\nasync def blocking() -> None:\n    for i in range(sleep_time):\n        print(\"Blocking\", i)\n        time.sleep(1)\n\n\nasync def non_blocking(task_id: str = \"Non-blocking\") -> None:\n    for i in range(sleep_time):\n        print(task_id, i)\n        await asyncio.sleep(1)\n\n\n\n\n# > Blocked\nasync def blocked() -> None:\n    loop = asyncio.get_event_loop()\n\n    await asyncio.gather(\n        *[\n            loop.create_task(blocking()),\n            loop.create_task(non_blocking()),\n        ]\n    )\n\n\n\n\n# > Unblocked\nasync def working() -> None:\n    loop = asyncio.get_event_loop()\n\n    await asyncio.gather(\n        *[\n            loop.create_task(non_blocking(\"A\")),\n            loop.create_task(non_blocking(\"B\")),\n        ]\n    )\n\n\n\n\nif __name__ == \"__main__\":\n    asyncio.run(blocked())\n    asyncio.run(working())\n",
   "source": "out/python/blocked_async/debugging.py",
   "blocks": {
     "functions": {
       "start": 2,
-      "stop": 15
+      "stop": 19
     },
     "blocked": {
-      "start": 18,
-      "stop": 24
+      "start": 23,
+      "stop": 33
     },
     "unblocked": {
-      "start": 27,
-      "stop": 33
+      "start": 37,
+      "stop": 47
     }
   },
   "highlights": {}
