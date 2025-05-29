2a790d4dc31dab8dedd26d6fc63e965f75a8fac9
mrkaye97 <mrkaye97@gmail.com>
2025-05-29 12:12:49 -0400

    feat: example cleanup

--- sdks/python/examples/opentelemetry_instrumentation/langfuse/trigger.py
+++ sdks/python/examples/opentelemetry_instrumentation/langfuse/trigger.py
@@ -1,17 +1,23 @@
 import asyncio
 
-from langfuse import get_client  # type: ignore[import-untyped]
+from opentelemetry.trace import StatusCode
 
+from examples.opentelemetry_instrumentation.langfuse.client import trace_provider
 from examples.opentelemetry_instrumentation.langfuse.worker import langfuse_task
 
-langfuse = get_client()
+tracer = trace_provider.get_tracer(__name__)
 
 
 async def main() -> None:
-    with langfuse.start_as_current_span(name="trigger") as span:
+    with tracer.start_as_current_span(name="trigger") as span:
         result = await langfuse_task.aio_run()
+        location = result.get("location")
 
-        span._otel_span.set_attributes(result)
+        if not location:
+            span.set_status(StatusCode.ERROR)
+            return
+
+        span.set_attribute("location", location)
 
 
 if __name__ == "__main__":
