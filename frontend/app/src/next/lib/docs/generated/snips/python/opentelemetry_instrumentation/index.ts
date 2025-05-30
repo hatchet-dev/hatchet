493566a307272e3de17b43d8145fe9d358709e9e
mrkaye97 <mrkaye97@gmail.com>
2025-05-30 11:02:56 -0400

    fix: rm langfuse for now

--- frontend/app/src/next/lib/docs/generated/snips/python/opentelemetry_instrumentation/index.ts
+++ frontend/app/src/next/lib/docs/generated/snips/python/opentelemetry_instrumentation/index.ts
@@ -2,10 +2,8 @@ import client from './client';
 import tracer from './tracer';
 import triggers from './triggers';
 import worker from './worker';
-import * as langfuse from './langfuse';
 
 export { client };
 export { tracer };
 export { triggers };
 export { worker };
-export { langfuse };
