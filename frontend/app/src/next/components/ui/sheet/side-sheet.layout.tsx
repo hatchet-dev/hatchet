df1d03b965618b099ac6bb0e08ee2289ab28ca14
mrkaye97 <mrkaye97@gmail.com>
2025-05-29 22:12:27 -0400

    fix: side panel

---
+++ frontend/app/src/next/components/ui/sheet/side-sheet.layout.tsx
@@ -0,0 +1,159 @@
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '../sheet';
+import { useSideSheet } from '@/next/hooks/use-side-sheet';
+import { useIsMobile } from '@/next/hooks/use-mobile';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import {
+  RunDetailSheet,
+  RunDetailSheetSerializableProps,
+} from '@/next/pages/authenticated/dashboard/runs/detail-sheet/run-detail-sheet';
+import { useMemo, useEffect } from 'react';
+import { cn } from '@/lib/utils';
+import { Button } from '../button';
+import { useDocs } from '@/next/hooks/use-docs-sheet';
+import { useSidePanel } from '@/next/hooks/use-side-panel';
+
+interface SideSheetProps {
+  variant?: 'overlay' | 'push';
+  onClose: () => void;
+  isOpen: boolean;
+  props: SidePanelProps;
+}
+
+interface SideSheetContent {
+  component: React.ReactNode;
+  title: React.ReactNode;
+  actions?: React.ReactNode;
+}
+
+type SidePanelProps =
+  | {
+      type: 'docs';
+      url: string;
+      title: string;
+    }
+  | {
+      type: 'task-detail';
+      props: RunDetailSheetSerializableProps;
+    };
+
+export function SideSheetComponent({
+  variant = 'push',
+  onClose,
+}: SideSheetProps) {
+  const isMobile = useIsMobile();
+  const { sheet } = useSideSheet();
+  const { close: closeDocsSheet } = useDocs();
+
+  const isOpen = useMemo(() => !!sheet.openProps, [sheet.openProps]);
+
+  useEffect(() => {
+    if (isOpen) {
+      closeDocsSheet();
+    }
+  }, [isOpen]);
+
+  const content = useMemo<SideSheetContent | undefined>(() => {
+    if (sheet.openProps?.type === 'task-detail') {
+      return {
+        component: <RunDetailSheet {...sheet.openProps.props} />,
+        title: 'Run Detail',
+      };
+    }
+
+    return undefined;
+  }, [sheet, isOpen, onClose]);
+
+  if (variant === 'push' && !isMobile) {
+    return (
+      <div>
+        {isOpen && content && (
+          <div className="flex flex-col h-screen">
+            <div
+              className={cn(
+                'flex flex-row w-full justify-between items-center border-b bg-background',
+                isMobile ? 'h-16 px-4' : 'h-12 px-4',
+              )}
+            >
+              <h2 className="text-lg font-semibold truncate pr-2">
+                {content.title}
+              </h2>
+              <div className="flex items-center gap-2">
+                {content.actions}
+                <Button
+                  variant="ghost"
+                  onClick={onClose}
+                  className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 flex-shrink-0"
+                >
+                  <Cross2Icon className="h-4 w-4" />
+                  <span className="sr-only">Close</span>
+                </Button>
+              </div>
+            </div>
+
+            {content.component}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <Sheet open={isOpen} onOpenChange={(open) => !open && onClose()}>
+      <SheetContent
+        side="right"
+        className="w-[min(500px,90vw)] sm:w-[min(800px,90vw)]"
+      >
+        {content && (
+          <>
+            <SheetHeader>
+              <div className="flex justify-between items-center">
+                <SheetTitle className="truncate">{content.title}</SheetTitle>
+                <div className="flex items-center gap-2">{content.actions}</div>
+              </div>
+            </SheetHeader>
+            <div className="flex-1 overflow-y-auto mt-4">
+              {content.component}
+            </div>
+          </>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export function SidePanel() {
+  const { content: maybeContent, isOpen, close } = useSidePanel();
+
+  if (!maybeContent) {
+    return null;
+  }
+
+  return (
+    isOpen && (
+      <div className="flex flex-col h-screen">
+        <div
+          className={
+            'flex flex-row w-full justify-between items-center border-b bg-background h-16 px-4 md:px-12'
+          }
+        >
+          <h2 className="text-lg font-semibold truncate pr-2">
+            {maybeContent.title}
+          </h2>
+          <div className="flex items-center gap-2">
+            {!maybeContent.isDocs && maybeContent.actions}
+            <Button
+              variant="ghost"
+              onClick={close}
+              className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 flex-shrink-0"
+            >
+              <Cross2Icon className="h-4 w-4" />
+              <span className="sr-only">Close</span>
+            </Button>
+          </div>
+        </div>
+
+        {maybeContent.component}
+      </div>
+    )
+  );
+}
