6e1bb37d05c8be8dc1416c33654ee0f2d4aeb65c
mrkaye97 <mrkaye97@gmail.com>
2025-06-06 18:33:58 -0400

    fix: pr feedback

--- api/v1/server/handlers/v1/workflow-runs/get-status.go
+++ api/v1/server/handlers/v1/workflow-runs/get-status.go
@@ -3,19 +3,14 @@ package workflowruns
 import (
 	"github.com/labstack/echo/v4"
 
-	"github.com/hatchet-dev/hatchet/api/v1/server/oas/apierrors"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
 	v1 "github.com/hatchet-dev/hatchet/pkg/repository/v1"
 )
 
 func (t *V1WorkflowRunsService) V1WorkflowRunGetStatus(ctx echo.Context, request gen.V1WorkflowRunGetStatusRequestObject) (gen.V1WorkflowRunGetStatusResponseObject, error) {
-	rawWorkflowRun := ctx.Get("v1-workflow-run").(*v1.V1WorkflowRunPopulator)
-
-	if rawWorkflowRun == nil {
-		return gen.V1WorkflowRunGetStatus404JSONResponse(apierrors.NewAPIErrors("could not find the workflow run provided")), nil
-	}
+	maybeWorkflowRun := ctx.Get("v1-workflow-run").(*v1.V1WorkflowRunPopulator)
 
 	return gen.V1WorkflowRunGetStatus200JSONResponse(
-		gen.V1TaskStatus(rawWorkflowRun.WorkflowRun.ReadableStatus),
+		gen.V1TaskStatus(maybeWorkflowRun.WorkflowRun.ReadableStatus),
 	), nil
 }
