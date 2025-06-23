fc6675639ece4c97129f4bd895a12d03a7285d9a
mrkaye97 <mrkaye97@gmail.com>
2025-06-23 16:53:29 -0400

    fix: make test faster

--- sdks/python/examples/streaming/worker.py
+++ sdks/python/examples/streaming/worker.py
@@ -29,7 +29,7 @@ async def stream_task(input: EmptyModel, ctx: Context) -> None:
 
     for chunk in chunks:
         ctx.put_stream(chunk)
-        await asyncio.sleep(0.20)
+        await asyncio.sleep(0.05)
 
 
 def main() -> None:
