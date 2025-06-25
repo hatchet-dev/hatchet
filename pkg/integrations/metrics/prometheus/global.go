27bae528aa15622abbcd136fa5597377d1b5c8d1
Mohammed Nafees <hello@mnafees.me>
2025-06-25 20:58:30 +0200

    move scheduler metrics to prom tenant specific file

--- pkg/integrations/metrics/prometheus/global.go
+++ pkg/integrations/metrics/prometheus/global.go
@@ -8,18 +8,13 @@ import (
 type GlobalHatchetMetric string
 
 const (
-	QueueInvocationsTotal       GlobalHatchetMetric = "hatchet_queue_invocations_total"
-	CreatedTasksTotal           GlobalHatchetMetric = "hatchet_created_tasks_total"
-	RetriedTasksTotal           GlobalHatchetMetric = "hatchet_retried_tasks_total"
-	SucceededTasksTotal         GlobalHatchetMetric = "hatchet_succeeded_tasks_total"
-	FailedTasksTotal            GlobalHatchetMetric = "hatchet_failed_tasks_total"
-	SkippedTasksTotal           GlobalHatchetMetric = "hatchet_skipped_tasks_total"
-	CancelledTasksTotal         GlobalHatchetMetric = "hatchet_cancelled_tasks_total"
-	AssignedTasksTotal          GlobalHatchetMetric = "hatchet_assigned_tasks_total"
-	SchedulingTimedOutTotal     GlobalHatchetMetric = "hatchet_scheduling_timed_out_total"
-	RateLimitedTotal            GlobalHatchetMetric = "hatchet_rate_limited_total"
-	QueuedToAssignedTotal       GlobalHatchetMetric = "hatchet_queued_to_assigned_total"
-	QueuedToAssignedTimeSeconds GlobalHatchetMetric = "hatchet_queued_to_assigned_seconds"
+	QueueInvocationsTotal GlobalHatchetMetric = "hatchet_queue_invocations_total"
+	CreatedTasksTotal     GlobalHatchetMetric = "hatchet_created_tasks_total"
+	RetriedTasksTotal     GlobalHatchetMetric = "hatchet_retried_tasks_total"
+	SucceededTasksTotal   GlobalHatchetMetric = "hatchet_succeeded_tasks_total"
+	FailedTasksTotal      GlobalHatchetMetric = "hatchet_failed_tasks_total"
+	SkippedTasksTotal     GlobalHatchetMetric = "hatchet_skipped_tasks_total"
+	CancelledTasksTotal   GlobalHatchetMetric = "hatchet_cancelled_tasks_total"
 )
 
 var (
@@ -56,31 +51,5 @@ var (
 	CancelledTasks = promauto.NewCounter(prometheus.CounterOpts{
 		Name: string(CancelledTasksTotal),
 		Help: "The total number of tasks cancelled",
-	})
-
-	AssignedTasks = promauto.NewCounter(prometheus.CounterOpts{
-		Name: string(AssignedTasksTotal),
-		Help: "The total number of tasks assigned to a worker",
-	})
-
-	SchedulingTimedOut = promauto.NewCounter(prometheus.CounterOpts{
-		Name: string(SchedulingTimedOutTotal),
-		Help: "The total number of tasks that timed out while waiting to be scheduled",
-	})
-
-	RateLimited = promauto.NewCounter(prometheus.CounterOpts{
-		Name: string(RateLimitedTotal),
-		Help: "The total number of tasks that were rate limited",
-	})
-
-	QueuedToAssigned = promauto.NewCounter(prometheus.CounterOpts{
-		Name: string(QueuedToAssignedTotal),
-		Help: "The total number of unique tasks that were queued and later got assigned to a worker",
-	})
-
-	QueuedToAssignedTimeBuckets = promauto.NewHistogram(prometheus.HistogramOpts{
-		Name:    string(QueuedToAssignedTimeSeconds),
-		Help:    "Buckets of time in seconds spent in the queue before being assigned to a worker",
-		Buckets: []float64{0.01, 0.02, 0.05, 0.1, 0.5, 1, 2, 5, 15},
 	})
 )
--- pkg/integrations/metrics/prometheus/tenant.go
+++ pkg/integrations/metrics/prometheus/tenant.go
@@ -10,11 +10,21 @@ import (
 type TenantHatchetMetric string
 
 const (
-	TenantWorkflowDuration TenantHatchetMetric = "hatchet_workflow_duration_milliseconds"
+	TenantWorkflowDuration            TenantHatchetMetric = "hatchet_workflow_duration_milliseconds"
+	TenantAssignedTasks               TenantHatchetMetric = "hatchet_assigned_tasks"
+	TenantSchedulingTimedOut          TenantHatchetMetric = "hatchet_scheduling_timed_out"
+	TenantRateLimited                 TenantHatchetMetric = "hatchet_rate_limited"
+	TenantQueuedToAssigned            TenantHatchetMetric = "hatchet_queued_to_assigned"
+	TenantQueuedToAssignedTimeBuckets TenantHatchetMetric = "hatchet_queued_to_assigned_time_seconds"
 )
 
 type TenantMetric struct {
-	WorkflowDuration *prometheus.HistogramVec
+	WorkflowDuration            *prometheus.HistogramVec
+	AssignedTasks               *prometheus.CounterVec
+	SchedulingTimedOut          *prometheus.CounterVec
+	RateLimited                 *prometheus.CounterVec
+	QueuedToAssigned            *prometheus.CounterVec
+	QueuedToAssignedTimeBuckets *prometheus.HistogramVec
 }
 
 var tenantMetricsMap sync.Map
@@ -58,8 +68,39 @@ func WithTenant(tenantId string) *TenantMetric {
 			},
 		}, []string{"tenant_id", "workflow_name", "status"})
 
+		assignedTasks := promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: string(TenantAssignedTasks),
+			Help: "The total number of tasks assigned to a worker",
+		}, []string{"tenant_id"})
+
+		schedulingTimedOut := promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: string(TenantSchedulingTimedOut),
+			Help: "The total number of tasks that timed out while waiting to be scheduled",
+		}, []string{"tenant_id"})
+
+		rateLimited := promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: string(TenantRateLimited),
+			Help: "The total number of tasks that were rate limited",
+		}, []string{"tenant_id"})
+
+		queuedToAssigned := promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: string(TenantQueuedToAssigned),
+			Help: "The total number of unique tasks that were queued and later got assigned to a worker",
+		}, []string{"tenant_id"})
+
+		queuedToAssignedTimeBuckets := promauto.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    string(TenantQueuedToAssignedTimeBuckets),
+			Help:    "Buckets of time in seconds spent in the queue before being assigned to a worker",
+			Buckets: []float64{0.01, 0.02, 0.05, 0.1, 0.5, 1, 2, 5, 15},
+		}, []string{"tenant_id"})
+
 		tenantMetric = &TenantMetric{
-			WorkflowDuration: workflowDuration,
+			WorkflowDuration:            workflowDuration,
+			AssignedTasks:               assignedTasks,
+			SchedulingTimedOut:          schedulingTimedOut,
+			RateLimited:                 rateLimited,
+			QueuedToAssigned:            queuedToAssigned,
+			QueuedToAssignedTimeBuckets: queuedToAssignedTimeBuckets,
 		}
 
 		tenantMetricsMap.Store(tenantId, tenantMetric)
