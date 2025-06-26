784d514071a7369f6f03547def553f7f3564154a
mrkaye97 <mrkaye97@gmail.com>
2025-06-26 12:28:21 -0400

    chore: gen

--- frontend/app/src/next/lib/docs/generated/snips/python/streaming/index.ts
+++ frontend/app/src/next/lib/docs/generated/snips/python/streaming/index.ts
@@ -3,7 +3,7 @@ import fastapi_proxy from './fastapi_proxy';
 import test_streaming from './test_streaming';
 import worker from './worker';
 
-export { async_stream }
-export { fastapi_proxy }
-export { test_streaming }
-export { worker }
+export { async_stream };
+export { fastapi_proxy };
+export { test_streaming };
+export { worker };
--- frontend/app/src/next/lib/docs/generated/snips/typescript/streaming/run.ts
+++ frontend/app/src/next/lib/docs/generated/snips/typescript/streaming/run.ts
@@ -1,16 +1,17 @@
 import { Snippet } from '@/next/lib/docs/generated/snips/types';
 
 const snippet: Snippet = {
-  "language": "typescript ",
-  "content": "import { RunEventType } from '@hatchet-dev/typescript-sdk-dev/typescript-sdk/clients/listeners/run-listener/child-listener-client';\nimport { streamingTask } from './workflow';\n\nasync function main() {\n  // > Consume\n  const ref = await streamingTask.runNoWait({});\n\n  const stream = await ref.stream();\n\n  for await (const event of stream) {\n    if (event.type === RunEventType.STEP_RUN_EVENT_TYPE_STREAM) {\n      process.stdout.write(event.payload);\n    }\n  }\n}\n\nif (require.main === module) {\n  main()\n    .catch(console.error)\n    .finally(() => {\n      process.exit(0);\n    });\n}\n",
-  "source": "out/typescript/streaming/run.ts",
-  "blocks": {
-    "consume": {
-      "start": 6,
-      "stop": 14
-    }
+  language: 'typescript ',
+  content:
+    "import { RunEventType } from '@hatchet-dev/typescript-sdk-dev/typescript-sdk/clients/listeners/run-listener/child-listener-client';\nimport { streamingTask } from './workflow';\n\nasync function main() {\n  // > Consume\n  const ref = await streamingTask.runNoWait({});\n\n  const stream = await ref.stream();\n\n  for await (const event of stream) {\n    if (event.type === RunEventType.STEP_RUN_EVENT_TYPE_STREAM) {\n      process.stdout.write(event.payload);\n    }\n  }\n}\n\nif (require.main === module) {\n  main()\n    .catch(console.error)\n    .finally(() => {\n      process.exit(0);\n    });\n}\n",
+  source: 'out/typescript/streaming/run.ts',
+  blocks: {
+    consume: {
+      start: 6,
+      stop: 14,
+    },
   },
-  "highlights": {}
+  highlights: {},
 };
 
 export default snippet;
