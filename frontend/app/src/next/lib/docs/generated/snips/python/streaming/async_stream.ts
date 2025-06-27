209cd32ee8ff9ed3a94c2c196a7f4a6abb7645e2
mrkaye97 <mrkaye97@gmail.com>
2025-06-27 12:30:10 -0400

    chore: gen

--- frontend/app/src/next/lib/docs/generated/snips/python/streaming/async_stream.ts
+++ frontend/app/src/next/lib/docs/generated/snips/python/streaming/async_stream.ts
@@ -3,12 +3,12 @@ import { Snippet } from '@/next/lib/docs/generated/snips/types';
 const snippet: Snippet = {
   language: 'python',
   content:
-    'import asyncio\n\nfrom examples.streaming.worker import stream_task\nfrom hatchet_sdk.clients.listeners.run_event_listener import StepRunEventType\n\n\nasync def main() -> None:\n    # > Consume\n    ref = await stream_task.aio_run_no_wait()\n\n    async for chunk in ref.stream():\n        if chunk.type == StepRunEventType.STEP_RUN_EVENT_TYPE_STREAM:\n            print(chunk.payload, flush=True, end="")\n\n\nif __name__ == "__main__":\n    asyncio.run(main())\n',
+    'import asyncio\n\nfrom examples.streaming.worker import hatchet, stream_task\nfrom hatchet_sdk.clients.listeners.run_event_listener import StepRunEventType\n\n\nasync def main() -> None:\n    # > Consume\n    ref = await stream_task.aio_run_no_wait()\n\n    async for chunk in hatchet.subscribe_to_stream(ref.workflow_run_id):\n        print(chunk, flush=True, end="")\n\n\nif __name__ == "__main__":\n    asyncio.run(main())\n',
   source: 'out/python/streaming/async_stream.py',
   blocks: {
     consume: {
       start: 9,
-      stop: 13,
+      stop: 12,
     },
   },
   highlights: {},
