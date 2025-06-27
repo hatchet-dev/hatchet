73fcb8eeac374fa0e644f411e4254d08e95bf36f
mrkaye97 <mrkaye97@gmail.com>
2025-06-27 12:26:19 -0400

    feat: add stream sub on top level

--- sdks/python/examples/streaming/fastapi_proxy.py
+++ sdks/python/examples/streaming/fastapi_proxy.py
@@ -4,23 +4,20 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 
 from examples.streaming.worker import stream_task
-from hatchet_sdk import RunEventListener, StepRunEventType
+from hatchet_sdk import Hatchet
 
 # > FastAPI Proxy
+hatchet = Hatchet()
 app = FastAPI()
-
-
-async def generate_stream(stream: RunEventListener) -> AsyncGenerator[str, None]:
-    async for chunk in stream:
-        if chunk.type == StepRunEventType.STEP_RUN_EVENT_TYPE_STREAM:
-            yield chunk.payload
 
 
 @app.get("/stream")
 async def stream() -> StreamingResponse:
     ref = await stream_task.aio_run_no_wait()
 
-    return StreamingResponse(generate_stream(ref.stream()), media_type="text/plain")
+    return StreamingResponse(
+        hatchet.subscribe_to_stream(ref.workflow_run_id), media_type="text/plain"
+    )
 
 
 # !!
