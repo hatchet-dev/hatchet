9b39ac92e4473dc86d2376247b137df3bdb3b77f
Matt Kaye <mrkaye97@gmail.com>
2025-07-10 11:50:55 -0400

    Feat: Improve metrics feature client (#1976)
    * feat: add optional replacement to remove null unicode char
    
    * feat: rework metrics client
    
    * feat: rm unused
    
    * chore: changelog
    
    * chore: gen
    
    * feat: add tenant prom metrics scraper
    
    * feat: docs
    
    * chore: changelog, naming
    
    * fix: lint
    
    * fix: type
    
    * fix: pass replacement through

--- sdks/python/hatchet_sdk/features/metrics.py
+++ sdks/python/hatchet_sdk/features/metrics.py
@@ -1,20 +1,21 @@
 import asyncio
+from datetime import datetime, timedelta, timezone
+from typing import Any
 
+from pydantic import BaseModel
+
+from hatchet_sdk.clients.rest.api.task_api import TaskApi
 from hatchet_sdk.clients.rest.api.tenant_api import TenantApi
-from hatchet_sdk.clients.rest.api.workflow_api import WorkflowApi
 from hatchet_sdk.clients.rest.api_client import ApiClient
-from hatchet_sdk.clients.rest.models.tenant_queue_metrics import TenantQueueMetrics
-from hatchet_sdk.clients.rest.models.tenant_step_run_queue_metrics import (
-    TenantStepRunQueueMetrics,
-)
-from hatchet_sdk.clients.rest.models.workflow_metrics import WorkflowMetrics
-from hatchet_sdk.clients.rest.models.workflow_run_status import WorkflowRunStatus
-from hatchet_sdk.clients.v1.api_client import (
-    BaseRestClient,
-    maybe_additional_metadata_to_kv,
-    retry,
-)
-from hatchet_sdk.utils.typing import JSONSerializableMapping
+from hatchet_sdk.clients.v1.api_client import BaseRestClient, retry
+
+
+class TaskMetrics(BaseModel):
+    cancelled: int
+    completed: int
+    failed: int
+    queued: int
+    running: int
 
 
 class MetricsClient(BaseRestClient):
@@ -22,108 +23,126 @@ class MetricsClient(BaseRestClient):
     The metrics client is a client for reading metrics out of Hatchet's metrics API.
     """
 
-    def _wa(self, client: ApiClient) -> WorkflowApi:
-        return WorkflowApi(client)
+    def _taskapi(self, client: ApiClient) -> TaskApi:
+        return TaskApi(client)
 
     def _ta(self, client: ApiClient) -> TenantApi:
         return TenantApi(client)
 
     @retry
-    def get_workflow_metrics(
-        self,
-        workflow_id: str,
-        status: WorkflowRunStatus | None = None,
-        group_key: str | None = None,
-    ) -> WorkflowMetrics:
-        """
-        Retrieve workflow metrics for a given workflow ID.
-
-        :param workflow_id: The ID of the workflow to retrieve metrics for.
-        :param status: The status of the workflow run to filter by.
-        :param group_key: The key to group the metrics by.
-
-        :return: Workflow metrics for the specified workflow ID.
-        """
-        with self.client() as client:
-            return self._wa(client).workflow_get_metrics(
-                workflow=workflow_id, status=status, group_key=group_key
-            )
-
-    async def aio_get_workflow_metrics(
-        self,
-        workflow_id: str,
-        status: WorkflowRunStatus | None = None,
-        group_key: str | None = None,
-    ) -> WorkflowMetrics:
-        """
-        Retrieve workflow metrics for a given workflow ID.
-
-        :param workflow_id: The ID of the workflow to retrieve metrics for.
-        :param status: The status of the workflow run to filter by.
-        :param group_key: The key to group the metrics by.
-
-        :return: Workflow metrics for the specified workflow ID.
-        """
-        return await asyncio.to_thread(
-            self.get_workflow_metrics, workflow_id, status, group_key
-        )
-
-    @retry
     def get_queue_metrics(
         self,
-        workflow_ids: list[str] | None = None,
-        additional_metadata: JSONSerializableMapping | None = None,
-    ) -> TenantQueueMetrics:
+    ) -> dict[str, Any]:
         """
-        Retrieve queue metrics for a set of workflow ids and additional metadata.
+        Retrieve the current queue metrics for the tenant.
 
-        :param workflow_ids: A list of workflow IDs to retrieve metrics for.
-        :param additional_metadata: Additional metadata to filter the metrics by.
-
-        :return: Workflow metrics for the specified workflow IDs.
+        :return: The current queue metrics
         """
         with self.client() as client:
-            return self._wa(client).tenant_get_queue_metrics(
-                tenant=self.client_config.tenant_id,
-                workflows=workflow_ids,
-                additional_metadata=maybe_additional_metadata_to_kv(
-                    additional_metadata
-                ),
-            )
+            return (
+                self._ta(client)
+                .tenant_get_step_run_queue_metrics(
+                    tenant=self.client_config.tenant_id,
+                )
+                .queues
+            ) or {}
 
     async def aio_get_queue_metrics(
         self,
-        workflow_ids: list[str] | None = None,
-        additional_metadata: JSONSerializableMapping | None = None,
-    ) -> TenantQueueMetrics:
+    ) -> dict[str, Any]:
         """
-        Retrieve queue metrics for a set of workflow ids and additional metadata.
+        Retrieve the current queue metrics for the tenant.
 
-        :param workflow_ids: A list of workflow IDs to retrieve metrics for.
-        :param additional_metadata: Additional metadata to filter the metrics by.
-
-        :return: Workflow metrics for the specified workflow IDs.
+        :return: The current queue metrics
         """
-        return await asyncio.to_thread(
-            self.get_queue_metrics, workflow_ids, additional_metadata
-        )
+
+        return await asyncio.to_thread(self.get_queue_metrics)
 
     @retry
-    def get_task_metrics(self) -> TenantStepRunQueueMetrics:
+    def scrape_tenant_prometheus_metrics(
+        self,
+    ) -> str:
         """
-        Retrieve queue metrics
+        Scrape Prometheus metrics for the tenant. Returns the metrics in Prometheus text format.
 
-        :return: Step run queue metrics for the tenant
+        :return: The metrics, returned in Prometheus text format
         """
         with self.client() as client:
-            return self._ta(client).tenant_get_step_run_queue_metrics(
-                tenant=self.client_config.tenant_id
+            return self._ta(client).tenant_get_prometheus_metrics(
+                tenant=self.client_config.tenant_id,
             )
 
-    async def aio_get_task_metrics(self) -> TenantStepRunQueueMetrics:
+    async def aio_scrape_tenant_prometheus_metrics(
+        self,
+    ) -> str:
         """
-        Retrieve queue metrics
+        Scrape Prometheus metrics for the tenant. Returns the metrics in Prometheus text format.
 
-        :return: Step run queue metrics for the tenant
+        :return: The metrics, returned in Prometheus text format
         """
-        return await asyncio.to_thread(self.get_task_metrics)
+
+        return await asyncio.to_thread(self.scrape_tenant_prometheus_metrics)
+
+    @retry
+    def get_task_metrics(
+        self,
+        since: datetime | None = None,
+        until: datetime | None = None,
+        workflow_ids: list[str] | None = None,
+        parent_task_external_id: str | None = None,
+        triggering_event_external_id: str | None = None,
+    ) -> TaskMetrics:
+        """
+        Retrieve task metrics, grouped by status (queued, running, completed, failed, cancelled).
+
+        :param since: Start time for the metrics query (defaults to the past day if unset)
+        :param until: End time for the metrics query
+        :param workflow_ids: List of workflow IDs to filter the metrics by
+        :param parent_task_external_id: ID of the parent task to filter by (note that parent task here refers to the task that spawned this task as a child)
+        :param triggering_event_external_id: ID of the triggering event to filter by
+        :return: Task metrics
+        """
+
+        since = since or datetime.now(timezone.utc) - timedelta(days=1)
+        until = until or datetime.now(timezone.utc)
+        with self.client() as client:
+            metrics = {
+                m.status.name.lower(): m.count
+                for m in self._taskapi(client).v1_task_list_status_metrics(
+                    tenant=self.client_config.tenant_id,
+                    since=since,
+                    until=until,
+                    workflow_ids=workflow_ids,
+                    parent_task_external_id=parent_task_external_id,
+                    triggering_event_external_id=triggering_event_external_id,
+                )
+            }
+
+            return TaskMetrics.model_validate(metrics)
+
+    async def aio_get_task_metrics(
+        self,
+        since: datetime | None = None,
+        until: datetime | None = None,
+        workflow_ids: list[str] | None = None,
+        parent_task_external_id: str | None = None,
+        triggering_event_external_id: str | None = None,
+    ) -> TaskMetrics:
+        """
+        Retrieve task metrics, grouped by status (queued, running, completed, failed, cancelled).
+
+        :param since: Start time for the metrics query (defaults to the past day if unset)
+        :param until: End time for the metrics query
+        :param workflow_ids: List of workflow IDs to filter the metrics by
+        :param parent_task_external_id: ID of the parent task to filter by (note that parent task here refers to the task that spawned this task as a child)
+        :param triggering_event_external_id: ID of the triggering event to filter by
+        :return: Task metrics
+        """
+        return await asyncio.to_thread(
+            self.get_task_metrics,
+            since,
+            until,
+            workflow_ids,
+            parent_task_external_id,
+            triggering_event_external_id,
+        )
