cfde70767b8f467ce2616f1d71c3a5f04143f00b
mrkaye97 <mrkaye97@gmail.com>
2025-07-10 08:55:26 -0400

    feat: add optional replacement to remove null unicode char

--- sdks/python/hatchet_sdk/utils/serde.py
+++ sdks/python/hatchet_sdk/utils/serde.py
@@ -5,23 +5,27 @@ K = TypeVar("K")
 
 
 @overload
-def remove_null_unicode_character(data: str) -> str: ...
+def remove_null_unicode_character(data: str, replacement: str = "") -> str: ...
 
 
 @overload
-def remove_null_unicode_character(data: dict[K, T]) -> dict[K, T]: ...
+def remove_null_unicode_character(
+    data: dict[K, T], replacement: str = ""
+) -> dict[K, T]: ...
 
 
 @overload
-def remove_null_unicode_character(data: list[T]) -> list[T]: ...
+def remove_null_unicode_character(data: list[T], replacement: str = "") -> list[T]: ...
 
 
 @overload
-def remove_null_unicode_character(data: tuple[T, ...]) -> tuple[T, ...]: ...
+def remove_null_unicode_character(
+    data: tuple[T, ...], replacement: str = ""
+) -> tuple[T, ...]: ...
 
 
 def remove_null_unicode_character(
-    data: str | dict[K, T] | list[T] | tuple[T, ...],
+    data: str | dict[K, T] | list[T] | tuple[T, ...], replacement: str = ""
 ) -> str | dict[K, T] | list[T] | tuple[T, ...]:
     """
     Recursively traverse a dictionary (a task's output) and remove the unicode escape sequence \\u0000 which will cause unexpected behavior in Hatchet.
@@ -29,11 +33,13 @@ def remove_null_unicode_character(
     Needed as Hatchet does not support \\u0000 in task outputs
 
     :param data: The task output (a JSON-serializable dictionary or mapping)
+    :param replacement: The string to replace \\u0000 with.
+
     :return: The same dictionary with all \\u0000 characters removed from strings, and nested dictionaries/lists processed recursively.
     :raises TypeError: If the input is not a string, dictionary, list, or tuple.
     """
     if isinstance(data, str):
-        return data.replace("\u0000", "")
+        return data.replace("\u0000", replacement)
 
     if isinstance(data, dict):
         return {
