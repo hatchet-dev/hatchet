c86a65bb0f1a6a3ae4680b82e1dda531285c9e75
Mohammed Nafees <hello@mnafees.me>
2025-07-11 18:00:30 +0200

    Add new streaming support to Go SDK (#1955)
    * add Go SDK streaming support
    
    * make docs changes for go sdk streaming
    
    * fix git lfs warning
    
    * streaming go example
    
    * fix lint
    
    * fix auto generated snip
    
    * revert poetry lock changes
    
    * some cleanup

---
+++ frontend/app/src/next/lib/docs/generated/snips/go/streaming/index.ts
@@ -0,0 +1,9 @@
+import * as consumer from './consumer';
+import * as server from './server';
+import * as shared from './shared';
+import * as worker from './worker';
+
+export { consumer };
+export { server };
+export { shared };
+export { worker };
