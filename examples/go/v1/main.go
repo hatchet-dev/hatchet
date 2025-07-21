b06fadaa8c516e34cf3ca8107ef917d943dcecb7
Mohammed Nafees <hello@mnafees.me>
2025-07-21 15:40:46 +0200

    add very basic example

---
+++ examples/go/v1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	hatchet "github.com/hatchet-dev/hatchet/sdks/go"
+)
+
+func main() {
+	client, err := hatchet.NewClient()
+	if err != nil {
+		log.Fatalf("failed to create hatchet client: %v", err)
+	}
+
+	workflow := client.NewWorkflow("my-workflow")
+	workflow.NewTask("my-task", func(ctx hatchet.Context, input any) (any, error) {
+		return "Hello, World!", nil
+	})
+
+	worker, err := client.NewWorker("my-worker", hatchet.WithWorkflows(workflow))
+	if err != nil {
+		log.Fatalf("failed to create hatchet worker: %v", err)
+	}
+
+	worker.StartBlocking(context.Background())
+}
