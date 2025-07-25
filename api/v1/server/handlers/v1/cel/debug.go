fcd65a4400ebd76ade4185e3f4855c9af50daeba
mrkaye97 <mrkaye97@gmail.com>
2025-07-25 16:58:22 -0400

    feat: log debug failure

--- api/v1/server/handlers/v1/cel/debug.go
+++ api/v1/server/handlers/v1/cel/debug.go
@@ -1,13 +1,22 @@
 package celv1
 
 import (
+	"fmt"
+
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/transformers/v1"
 	"github.com/hatchet-dev/hatchet/internal/cel"
+	msgqueue "github.com/hatchet-dev/hatchet/internal/msgqueue/v1"
+	tasktypes "github.com/hatchet-dev/hatchet/internal/services/shared/tasktypes/v1"
+	"github.com/hatchet-dev/hatchet/pkg/repository/postgres/dbsqlc"
+	v1 "github.com/hatchet-dev/hatchet/pkg/repository/v1"
+	"github.com/hatchet-dev/hatchet/pkg/repository/v1/sqlcv1"
 	"github.com/labstack/echo/v4"
 )
 
 func (c *V1CELService) V1CelDebug(ctx echo.Context, request gen.V1CelDebugRequestObject) (gen.V1CelDebugResponseObject, error) {
+	tenant := ctx.Get("tenant").(*dbsqlc.Tenant)
+
 	additionalMetadata := make(map[string]interface{})
 	if request.Body.AdditionalMetadata != nil {
 		additionalMetadata = *request.Body.AdditionalMetadata
@@ -24,6 +33,26 @@ func (c *V1CELService) V1CelDebug(ctx echo.Context, request gen.V1CelDebugReques
 		cel.WithPayload(filterPayload),
 	),
 	)
+
+	msg, err := tasktypes.CELEvaluationFailureMessage(
+		tenant.ID.String(),
+		[]v1.CELEvaluationFailure{
+			{
+				Source:       sqlcv1.V1CelEvaluationFailureSourceDEBUG,
+				ErrorMessage: err.Error(),
+			},
+		},
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create CEL evaluation failure message: %w", err)
+	}
+
+	err = c.config.MessageQueueV1.SendMessage(ctx.Request().Context(), msgqueue.TASK_PROCESSING_QUEUE, msg)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to send CEL evaluation failure message: %w", err)
+	}
 
 	var output *bool
 	var errorMessage *string
