be33d7e29bd6a336ecfbaacdbcacff223b10ef9f
mrkaye97 <mrkaye97@gmail.com>
2025-07-30 12:34:28 -0400

    fix: dont log ingest failure

--- api/v1/server/handlers/v1/cel/debug.go
+++ api/v1/server/handlers/v1/cel/debug.go
@@ -1,19 +1,13 @@
 package celv1
 
 import (
-	"fmt"
-
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/transformers/v1"
 	"github.com/hatchet-dev/hatchet/internal/cel"
-	"github.com/hatchet-dev/hatchet/pkg/repository/postgres/dbsqlc"
-	"github.com/hatchet-dev/hatchet/pkg/repository/v1/sqlcv1"
 	"github.com/labstack/echo/v4"
 )
 
 func (c *V1CELService) V1CelDebug(ctx echo.Context, request gen.V1CelDebugRequestObject) (gen.V1CelDebugResponseObject, error) {
-	tenant := ctx.Get("tenant").(*dbsqlc.Tenant)
-
 	additionalMetadata := make(map[string]interface{})
 	if request.Body.AdditionalMetadata != nil {
 		additionalMetadata = *request.Body.AdditionalMetadata
@@ -30,19 +24,6 @@ func (c *V1CELService) V1CelDebug(ctx echo.Context, request gen.V1CelDebugReques
 		cel.WithPayload(filterPayload),
 	),
 	)
-
-	if err != nil {
-		ingestErr := c.config.Ingestor.IngestCELEvaluationFailure(
-			ctx.Request().Context(),
-			tenant.ID.String(),
-			err.Error(),
-			sqlcv1.V1CelEvaluationFailureSourceDEBUG,
-		)
-
-		if ingestErr != nil {
-			return nil, fmt.Errorf("failed to ingest CEL evaluation failure: %w", ingestErr)
-		}
-	}
 
 	var output *bool
 	var errorMessage *string
