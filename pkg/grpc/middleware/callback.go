1f57430a74997ed867a3af4bd2ed9beaa9e12c95
Mohammed Nafees <hello@mnafees.me>
2025-07-31 15:38:41 +0200

    update comment

--- pkg/grpc/middleware/callback.go
+++ pkg/grpc/middleware/callback.go
@@ -25,7 +25,7 @@ func TriggerCallback(ctx context.Context, correlationID string) {
 // CallbackInterceptor creates an interceptor that can receive callbacks from handlers
 func CallbackInterceptor(logger *zerolog.Logger, onCallback func(ctx context.Context, method, correlationID string) error) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		// Store method name in context
+		// Store method name in context for callback
 		ctx = context.WithValue(ctx, "grpc_method", info.FullMethod)
 
 		ctx = WithCallback(ctx, func(method, correlationID string) {
