94e2e6d8b9fd4697f0ef6962050da8074465fe81
mrkaye97 <mrkaye97@gmail.com>
2025-08-04 16:22:25 -0400

    feat: nuke the next dir

--- frontend/app/src/next/lib/docs/generated/snips/python/api/index.ts
+++
@@ -1,5 +0,0 @@
-import api from './api';
-import async_api from './async_api';
-
-export { api };
-export { async_api };
--- frontend/app/src/next/lib/docs/generated/snips/python/child/index.ts
+++
@@ -1,9 +0,0 @@
-import bulk from './bulk';
-import simple_fanout from './simple-fanout';
-import trigger from './trigger';
-import worker from './worker';
-
-export { bulk };
-export { simple_fanout };
-export { trigger };
-export { worker };
