14d3716f0b6cf3b7d84b09e2ceb900cfa8add01f
Mohammed Nafees <hello@mnafees.me>
2025-08-11 13:30:02 +0200

    check for retry counts

--- cmd/hatchet-migrate/migrate/migrations/20250811130258_v1_0_36.sql
+++ cmd/hatchet-migrate/migrate/migrations/20250811130258_v1_0_36.sql
@@ -7,7 +7,7 @@ WHERE priority > 3;
 
 UPDATE v1_queue_item
 SET priority = 3
-WHERE priority > 3;
+WHERE priority > 3 AND retry_count = 0;
 
 UPDATE v1_workflow_concurrency_slot
 SET priority = 3
@@ -15,7 +15,7 @@ WHERE priority > 3;
 
 UPDATE v1_concurrency_slot
 SET priority = 3
-WHERE priority > 3;
+WHERE priority > 3 AND task_retry_count = 0;
 
 ALTER TABLE v1_task
 ADD CONSTRAINT v1_task_priority_user_limit
