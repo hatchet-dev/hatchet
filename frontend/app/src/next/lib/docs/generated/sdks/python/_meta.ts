5f9d4353a447ae655d75e11c95a7dfc3578deaf1
mrkaye97 <mrkaye97@gmail.com>
2025-08-12 14:08:20 -0400

    fix: add back sync docs script

---
+++ frontend/app/src/next/lib/docs/generated/sdks/python/_meta.ts
@@ -0,0 +1,32 @@
+// Generated from frontend/docs/pages/sdks/python/_meta.js
+const meta = {
+  client: {
+    title: 'Client',
+    theme: {
+      toc: true,
+    },
+    href: '/sdks/python/client',
+  },
+  context: {
+    title: 'Context',
+    theme: {
+      toc: true,
+    },
+    href: '/sdks/python/context',
+  },
+  feature_clients: {
+    title: 'Feature Clients',
+    theme: {
+      toc: true,
+    },
+    href: '/sdks/python/feature-clients',
+  },
+  runnables: {
+    title: 'Runnables',
+    theme: {
+      toc: true,
+    },
+    href: '/sdks/python/runnables',
+  },
+};
+export default meta;
---
+++ frontend/app/src/next/lib/docs/snips.ts
@@ -0,0 +1,5 @@
+import * as snippets from './generated/snips/index';
+import { Snippet as SnippetType } from './generated/snips/types';
+
+export type Snippet = SnippetType;
+export default snippets;
