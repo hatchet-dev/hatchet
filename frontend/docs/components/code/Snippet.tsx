73ed556e6ce9fd0c08cf86a2e927841516f797e4
mrkaye97 <mrkaye97@gmail.com>
2025-08-15 15:32:41 -0500

    fix: more snippets

--- frontend/docs/components/code/Snippet.tsx
+++ frontend/docs/components/code/Snippet.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CodeBlock } from "./CodeBlock";
 import { type Snippet as SnippetType } from "@/lib/snippet";
 
+export const Snippet = ({ src }: { src: SnippetType }) => {
   return (
     <CodeBlock
       source={{
