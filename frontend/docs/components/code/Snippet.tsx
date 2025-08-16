18c97c12940b48aa209d040f9d2f973ff0030e8c
mrkaye97 <mrkaye97@gmail.com>
2025-08-16 07:51:53 -0500

    fix: highlight

--- frontend/docs/components/code/Snippet.tsx
+++ frontend/docs/components/code/Snippet.tsx
@@ -2,13 +2,31 @@ import React from "react";
 import { CodeBlock } from "./CodeBlock";
 import { type Snippet as SnippetType } from "@/lib/snippet";
 
+type Language = SnippetType["language"];
+
+// See the list of supported languages for how to define these:
+// https://highlightjs.readthedocs.io/en/latest/supported-languages.html
+const languageToHighlightAbbreviation = (language: Language) => {
+  switch (language) {
+    case "python":
+      return "py";
+    case "typescript":
+      return "ts";
+    case "go":
+      return "go";
+    default:
+      const exhaustiveCheck: never = language;
+      throw new Error(`Unsupported language: ${exhaustiveCheck}`);
+  }
+};
+
 export const Snippet = ({ src }: { src: SnippetType }) => {
   return (
     <CodeBlock
       source={{
         githubUrl: src.githubUrl,
         raw: src.content,
-        language: src.language,
+        language: languageToHighlightAbbreviation'(src.language),
       }}
     />
   );
