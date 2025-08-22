413473ee2597a2036be0a6333f65b9158cea0e8c
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-08-22 08:40:53 -0400

    review comments

--- pkg/analytics/analytics.go
+++ pkg/analytics/analytics.go
@@ -1,17 +1,17 @@
 package analytics
 
 type Analytics interface {
-	// Enqueue queues an analytics event for processing
-	// @param event The name of the event to track
-	// @param userId The ID of the user performing the action
-	// @param tenantId Optional tenant ID to associate with this event
-	// @param set Key-value pairs to set on the user/group profile (e.g. email, name, etc.)
-	// @param metadata Additional metadata to attach to the event
+	// Enqueue queues an analytics event for processing.
+	// event is the name of the event to track.
+	// userId is the ID of the user performing the action.
+	// tenantId is an optional tenant ID to associate with this event.
+	// set contains key-value pairs to set on the user/group profile (e.g. email, name, etc.).
+	// metadata contains additional metadata to attach to the event.
 	Enqueue(event string, userId string, tenantId *string, set map[string]interface{}, metadata map[string]interface{})
 
-	// Tenant updates properties for a tenant group
-	// @param tenantId The ID of the tenant to update
-	// @param data Key-value pairs of properties to set on the tenant
+	// Tenant updates properties for a tenant group.
+	// tenantId is the ID of the tenant to update.
+	// data contains key-value pairs of properties to set on the tenant.
 	Tenant(tenantId string, data map[string]interface{})
 }
 
