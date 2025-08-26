65a5fba82f2fac474307774d251bc63c0a1bc4c0
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-08-26 09:14:32 -0400

    invoke

--- frontend/docs/pages/home/_meta.js
+++ frontend/docs/pages/home/_meta.js
@@ -20,7 +20,7 @@ export default {
     title: "Ways of Running Tasks",
     type: "separator",
   },
-  "running-tasks": "Introduction",
+  "running-tasks": "Inter-Service Triggering",
   "run-with-results": "Run and Wait Trigger",
   "run-no-wait": "Run Without Wait Trigger",
   "scheduled-runs": "Scheduled Trigger",
@@ -28,6 +28,17 @@ export default {
   "run-on-event": "Event Trigger",
   "bulk-run": "Bulk Run Many",
   webhooks: "Webhooks",
+  "--deploying-workers": {
+    title: "Deploying Workers",
+    type: "separator",
+  },
+  docker: "Running with Docker",
+  compute: {
+    title: "Managed Compute",
+    type: "page",
+    display: "hidden",
+  },
+  "worker-healthchecks": "Worker Health Checks",
   "--flow-control": {
     title: "Flow Control",
     type: "separator",
@@ -89,13 +100,6 @@ export default {
   logging: "Logging",
   opentelemetry: "OpenTelemetry",
   "prometheus-metrics": "Prometheus Metrics",
-  "--deploying-workers": {
-    title: "Deploying Workers",
-    type: "separator",
-  },
-  docker: "Running with Docker",
-  compute: "Managed Compute",
-  "worker-healthchecks": "Worker Health Checks",
   "--advanced-tasks": {
     title: "Advanced Task Features",
     type: "separator",
