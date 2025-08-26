39d7389380d7eda06d8147f212852f9fd75e0093
mrkaye97 <mrkaye97@gmail.com>
2025-08-26 14:26:44 -0400

    feat: example of stubs

---
+++ sdks/python/examples/stubs/stub_trigger.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from pydantic import BaseModel
+
+from hatchet_sdk import Context, Hatchet
+
+
+# > Define models
+class TaskInput(BaseModel):
+    user_id: int
+
+
+class TaskOutput(BaseModel):
+    ok: bool
+
+
+# !!
+
+
+async def main() -> None:
+    hatchet = Hatchet()
+
+    # > Create a stub task
+    stub = hatchet.stubs.task(
+        name="externally-triggered-task",
+        input_validator=TaskInput,
+        output_validator=TaskOutput,
+    )
+    # !!
+
+    # > Trigger the task
+    # input type checks properly
+    result = await stub.aio_run(input=TaskInput(user_id=1234))
+
+    # `result.ok` type checks properly
+    print("Is successful:", result.ok)
+    # !!
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
