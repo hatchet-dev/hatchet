60389346b2b61c3c1bba8daac212e70beac65d0e
mrkaye97 <mrkaye97@gmail.com>
2025-09-05 12:38:02 -0400

    fix: batch i

--- sdks/python/examples/events/event.py
+++ sdks/python/examples/events/event.py
@@ -1,7 +1,19 @@
-from hatchet_sdk import Hatchet
+from hatchet_sdk import Hatchet, PushEventOptions
 
 hatchet = Hatchet()
 
 # > Event trigger
 hatchet.event.push("user:create", {"should_skip": False})
+# !!
+
+# > Event trigger with metadata
+hatchet.event.push(
+    "user:create",
+    {"userId": "1234", "should_skip": False},
+    options=PushEventOptions(
+        additional_metadata={
+            "source": "api"  # Arbitrary key-value pair
+        }
+    ),
+)
 # !!
--- sdks/python/examples/fanout/trigger.py
+++ sdks/python/examples/fanout/trigger.py
@@ -1,8 +1,9 @@
 import asyncio
 
-from examples.fanout.worker import ParentInput, parent_wf
+from examples.fanout.worker import ParentInput, parent_wf, child_wf, ChildInput
 from hatchet_sdk import Hatchet
 from hatchet_sdk.clients.admin import TriggerWorkflowOptions
+from typing import Any
 
 hatchet = Hatchet()
 
@@ -13,6 +14,20 @@ async def main() -> None:
         options=TriggerWorkflowOptions(additional_metadata={"hello": "moon"}),
     )
 
+
+# > Bulk run children
+async def run_child_workflows(n: int) -> list[dict[str, Any]]:
+    return await child_wf.aio_run_many(
+        [
+            child_wf.create_bulk_run_item(
+                input=ChildInput(a=str(i)),
+            )
+            for i in range(n)
+        ]
+    )
+
+
+# !!
 
 if __name__ == "__main__":
     asyncio.run(main())
