4c37ca276c3015e08f8399c738abea9bb6c5ca3a
mrkaye97 <mrkaye97@gmail.com>
2025-09-10 14:31:27 -0400

    feat: form + schema

--- frontend/app/src/pages/main/v1/filters/index.tsx
+++ frontend/app/src/pages/main/v1/filters/index.tsx
@@ -4,6 +4,7 @@ import {
   scopeKey,
   workflowIdKey,
 } from './components/filter-columns';
+import { FilterCreateButton } from './components/filter-create-form';
 import { useState } from 'react';
 import { RowSelectionState, VisibilityState } from '@tanstack/react-table';
 import { ToolbarType } from '@/components/v1/molecules/data-table/data-table-toolbar';
@@ -11,9 +12,12 @@ import { Button } from '@/components/v1/ui/button';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { DataTable } from '@/components/v1/molecules/data-table/data-table';
 import { useFilters } from './hooks/use-filters';
+import { V1Filter } from '@/lib/api';
+import { useSidePanel } from '@/hooks/use-side-panel';
 
 export default function Filters() {
   const [rotate, setRotate] = useState(false);
+  const sidePanel = useSidePanel();
 
   const {
     pagination,
@@ -28,6 +32,7 @@ export default function Filters() {
     setColumnFilters,
     workflowIdToName,
     workflowNameFilters,
+    mutations,
   } = useFilters({
     key: 'table',
   });
@@ -36,9 +41,24 @@ export default function Filters() {
 
   const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
 
-  const tableColumns = filterColumns(workflowIdToName);
+  const handleRowClick = (filter: V1Filter) => {
+    sidePanel.open({
+      type: 'filter-detail',
+      content: {
+        filter,
+      },
+    });
+  };
+
+  const tableColumns = filterColumns(workflowIdToName, handleRowClick);
 
   const actions = [
+    <FilterCreateButton
+      key="create"
+      workflowNameFilters={workflowNameFilters}
+      onCreate={mutations.create.perform}
+      isCreating={mutations.create.isPending}
+    />,
     <Button
       key="refresh"
       className="h-8 px-2 lg:px-3"
@@ -48,7 +68,7 @@ export default function Filters() {
         setRotate(!rotate);
       }}
       variant={'outline'}
-      aria-label="Refresh events list"
+      aria-label="Refresh filters list"
     >
       <ArrowPathIcon
         className={`h-4 w-4 transition-transform ${rotate ? 'rotate-180' : ''}`}
