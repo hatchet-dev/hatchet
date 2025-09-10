9f1fa13f1be20176516fe0c27a1b9d922fd5c372
mrkaye97 <mrkaye97@gmail.com>
2025-09-10 09:13:57 -0400

    fix: rm id checkboxes for events and filters

--- frontend/app/src/pages/main/v1/events/components/event-columns.tsx
+++ frontend/app/src/pages/main/v1/events/components/event-columns.tsx
@@ -26,30 +26,6 @@ export const columns = ({
 }): ColumnDef<V1Event>[] => {
   return [
     {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && 'indeterminate')
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-          className="translate-y-[2px]"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-          className="translate-y-[2px]"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
       accessorKey: 'EventId',
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Event Id" />
--- frontend/app/src/pages/main/v1/filters/components/filter-columns.tsx
+++ frontend/app/src/pages/main/v1/filters/components/filter-columns.tsx
@@ -36,30 +36,6 @@ export const filterColumns = (
 ): ColumnDef<V1Filter>[] => {
   return [
     {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && 'indeterminate')
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-          className="translate-y-[2px]"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-          className="translate-y-[2px]"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
       accessorKey: idKey,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title={FilterColumn.id} />
