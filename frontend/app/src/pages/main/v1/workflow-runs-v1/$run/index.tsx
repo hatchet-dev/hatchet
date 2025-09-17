3a58c4bf8b0b2eaffb535b43e6fe5159eb3fc99e
mrkaye97 <mrkaye97@gmail.com>
2025-09-17 12:32:47 -0400

    fix: open side panel from mini map

--- frontend/app/src/pages/main/v1/workflow-runs-v1/$run/index.tsx
+++ frontend/app/src/pages/main/v1/workflow-runs-v1/$run/index.tsx
@@ -14,7 +14,7 @@ import {
   TabsTrigger,
 } from '@/components/v1/ui/tabs';
 import { StepRunEvents } from './v2components/step-run-events-for-workflow-run';
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import {
   TabOption,
   TaskRunDetail,
@@ -188,9 +188,18 @@ function ExpandedTaskRun({ id }: { id: string }) {
 
 function ExpandedWorkflowRun({ id }: { id: string }) {
   const { open } = useSidePanel();
+  const executingRef = useRef(false);
 
   const handleTaskRunExpand = useCallback(
     (taskRunId: string) => {
+      // hack to prevent click handler from firing multiple times,
+      // causing index offset issues
+      if (executingRef.current) {
+        return;
+      }
+
+      executingRef.current = true;
+
       open({
         type: 'task-run-details',
         content: {
@@ -199,6 +208,10 @@ function ExpandedWorkflowRun({ id }: { id: string }) {
           showViewTaskRunButton: true,
         },
       });
+
+      setTimeout(() => {
+        executingRef.current = false;
+      }, 100);
     },
     [open],
   );
