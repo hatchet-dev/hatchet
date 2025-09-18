df10480f9ab501f32b9bb330772b6c26011a510a
Mohammed Nafees <hello@mnafees.me>
2025-09-18 12:57:42 +0200

    run with results example

--- examples/go/simple/main.go
+++ examples/go/simple/main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/hatchet-dev/hatchet/pkg/cmdutils"
+	v0Worker "github.com/hatchet-dev/hatchet/pkg/worker"
 	hatchet "github.com/hatchet-dev/hatchet/sdks/go"
 )
 
@@ -75,6 +77,82 @@ func main() {
 		}
 
 		fmt.Println(resultOutput.Result)
+
+		workflow := client.NewWorkflow("parent-workflow")
+
+		// > Spawning tasks from within a task
+		parent := workflow.NewTask("parent-task", func(ctx hatchet.Context, input SimpleInput) (*SimpleOutput, error) {
+			// Run the child task
+			_, err := ctx.SpawnWorkflow(task.GetName(), SimpleInput{Message: input.Message}, &v0Worker.SpawnWorkflowOpts{})
+			if err != nil {
+				return nil, err
+			}
+
+			return &SimpleOutput{
+				Result: "Processed: " + input.Message,
+			}, nil
+		})
+
+		_ = parent
+
+		// > Running Multiple Tasks
+		var results []string
+		var resultsMutex sync.Mutex
+		var errs []error
+		var errsMutex sync.Mutex
+
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			result, err := task.Run(context.Background(), SimpleInput{
+				Message: "Hello, World!",
+			})
+
+			if err != nil {
+				errsMutex.Lock()
+				errs = append(errs, err)
+				errsMutex.Unlock()
+				return
+			}
+
+			resultsMutex.Lock()
+
+			var resultOutput SimpleOutput
+			err = result.Into(&resultOutput)
+			if err != nil {
+				return
+			}
+			results = append(results, resultOutput.Result)
+			resultsMutex.Unlock()
+		}()
+
+		go func() {
+			defer wg.Done()
+			result, err := task.Run(context.Background(), SimpleInput{
+				Message: "Hello, Moon!",
+			})
+
+			if err != nil {
+				errsMutex.Lock()
+				errs = append(errs, err)
+				errsMutex.Unlock()
+				return
+			}
+
+			resultsMutex.Lock()
+
+			var resultOutput SimpleOutput
+			err = result.Into(&resultOutput)
+			if err != nil {
+				return
+			}
+			results = append(results, resultOutput.Result)
+			resultsMutex.Unlock()
+		}()
+
+		wg.Wait()
 
 		return nil
 	}
