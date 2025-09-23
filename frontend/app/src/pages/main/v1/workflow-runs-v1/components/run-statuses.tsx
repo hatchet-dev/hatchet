1a33a1f96f82d9ed25b3098b2324a5dab73d1cfc
mrkaye97 <mrkaye97@gmail.com>
2025-09-23 13:36:15 -0400

    fix: more colors

--- frontend/app/src/pages/main/v1/workflow-runs-v1/components/run-statuses.tsx
+++ frontend/app/src/pages/main/v1/workflow-runs-v1/components/run-statuses.tsx
@@ -136,11 +136,13 @@ const indicatorVariants = {
 export function createV2IndicatorVariant(eventType: V1TaskStatus | undefined) {
   switch (eventType) {
     case V1TaskStatus.CANCELLED:
+      return 'border-transparent rounded-full bg-orange-500';
     case V1TaskStatus.FAILED:
       return 'border-transparent rounded-full bg-red-500';
     case V1TaskStatus.RUNNING:
-    case V1TaskStatus.QUEUED:
       return 'border-transparent rounded-full bg-yellow-500';
+    case V1TaskStatus.QUEUED:
+      return 'border-transparent rounded-full bg-fuchsia-500';
     case V1TaskStatus.COMPLETED:
       return 'border-transparent rounded-full bg-green-500';
     default:
