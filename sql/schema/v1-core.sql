95da95e3e3086e468554d32e749e0af144ffd976
mrkaye97 <mrkaye97@gmail.com>
2025-10-01 08:25:41 -0400

    fix: add default operation

--- sql/schema/v1-core.sql
+++ sql/schema/v1-core.sql
@@ -1658,7 +1658,7 @@ CREATE TABLE v1_payload_wal (
     payload_id BIGINT NOT NULL,
     payload_inserted_at TIMESTAMPTZ NOT NULL,
     payload_type v1_payload_type NOT NULL,
-    operation v1_payload_wal_operation NOT NULL,
+    operation v1_payload_wal_operation NOT NULL DEFAULT 'CREATE',
 
     PRIMARY KEY (offload_at, tenant_id, payload_id, payload_inserted_at, payload_type),
     CONSTRAINT "v1_payload_wal_payload" FOREIGN KEY (payload_id, payload_inserted_at, payload_type, tenant_id) REFERENCES v1_payload (id, inserted_at, type, tenant_id) ON DELETE CASCADE
