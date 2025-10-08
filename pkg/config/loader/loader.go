13ce4b05f0e56e92e5ed977f25c435bf9fbc15bc
Mohammed Nafees <hello@mnafees.me>
2025-10-08 14:26:39 +0200

    helper func

--- pkg/config/loader/loader.go
+++ pkg/config/loader/loader.go
@@ -141,12 +141,13 @@ func (c *ConfigLoader) InitDataLayer() (res *database.Layer, err error) {
 		return nil, err
 	}
 
-	// ref: https://github.com/jackc/pgx/issues/1549
 	config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
 		// Set timezone to UTC for all connections
 		if _, err := conn.Exec(ctx, "SET TIME ZONE 'UTC'"); err != nil {
 			return err
 		}
+
+		// ref: https://github.com/jackc/pgx/issues/1549
 
 		t, err := conn.LoadType(ctx, "v1_readable_status_olap")
 		if err != nil {
@@ -193,37 +194,17 @@ func (c *ConfigLoader) InitDataLayer() (res *database.Layer, err error) {
 		config.BeforeAcquire = debugger.beforeAcquire
 	}
 
+	// Check database instance timezone if enforcement is enabled
+	if cf.EnforceUTCTimezone {
+		if err := checkDatabaseTimezone(config.ConnConfig, cf.PostgresDbName, "primary database", &l); err != nil {
+			return nil, err
+		}
+	}
+
 	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to database: %w", err)
-	}
-
-	// Check database instance timezone if enforcement is enabled
-	if cf.EnforceUTCTimezone {
-		var dbTimezone string
-		err = pool.QueryRow(context.Background(), "SHOW timezone").Scan(&dbTimezone)
-		if err != nil {
-			return nil, fmt.Errorf("could not query database timezone: %w", err)
-		}
-
-		if dbTimezone != "UTC" {
-			dbName := cf.PostgresDbName
-			if dbName == "" {
-				dbName = "<your_database_name>"
-			}
-			return nil, fmt.Errorf(
-				"database instance timezone is set to '%s' but must be 'UTC'\n"+
-					"This check ensures time-based operations work correctly across all sessions\n"+
-					"To fix this issue, you have two options:\n"+
-					"  1. Set your PostgreSQL instance timezone to UTC by running: ALTER DATABASE %s SET TIMEZONE='UTC'\n"+
-					"  2. Disable this check by setting the environment variable: DATABASE_ENFORCE_UTC_TIMEZONE=false\n"+
-					"Note: Disabling this check is not recommended as it may lead to timezone-related issues",
-				dbTimezone, dbName,
-			)
-		}
-
-		l.Info().Msgf("Database instance timezone verified: %s", dbTimezone)
 	}
 
 	// a pool for read replicas, if enabled
@@ -251,6 +232,13 @@ func (c *ConfigLoader) InitDataLayer() (res *database.Layer, err error) {
 		readReplicaConfig.MaxConnLifetime = 15 * 60 * time.Second
 		readReplicaConfig.ConnConfig.Tracer = otelpgx.NewTracer()
 
+		// Check read replica database instance timezone if enforcement is enabled
+		if cf.EnforceUTCTimezone {
+			if err := checkDatabaseTimezone(readReplicaConfig.ConnConfig, "", "read replica database", &l); err != nil {
+				return nil, err
+			}
+		}
+
 		// Set timezone to UTC for read replica connections
 		readReplicaConfig.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
 			_, err := conn.Exec(ctx, "SET TIME ZONE 'UTC'")
@@ -261,29 +249,6 @@ func (c *ConfigLoader) InitDataLayer() (res *database.Layer, err error) {
 
 		if err != nil {
 			return nil, fmt.Errorf("could not connect to read replica database: %w", err)
-		}
-
-		// Check read replica database instance timezone if enforcement is enabled
-		if cf.EnforceUTCTimezone {
-			var dbTimezone string
-			err = readReplicaPool.QueryRow(context.Background(), "SHOW timezone").Scan(&dbTimezone)
-			if err != nil {
-				return nil, fmt.Errorf("could not query read replica database timezone: %w", err)
-			}
-
-			if dbTimezone != "UTC" {
-				return nil, fmt.Errorf(
-					"read replica database instance timezone is set to '%s' but must be 'UTC'\n"+
-						"This check ensures time-based operations work correctly across all sessions\n"+
-						"To fix this issue, you have two options:\n"+
-						"  1. Set your PostgreSQL read replica instance timezone to UTC by running: ALTER DATABASE <db_name> SET TIMEZONE='UTC'\n"+
-						"  2. Disable this check by setting the environment variable: DATABASE_ENFORCE_UTC_TIMEZONE=false\n"+
-						"Note: Disabling this check is not recommended as it may lead to timezone-related issues",
-					dbTimezone,
-				)
-			}
-
-			l.Info().Msgf("Read replica database instance timezone verified: %s", dbTimezone)
 		}
 	}
 
@@ -879,4 +844,37 @@ func loadInternalClient(l *zerolog.Logger, conf *server.InternalClientTLSConfigF
 		clientv1.WithTLS(tlsConfig),
 		clientv1.WithLogger(l),
 	), nil
+}
+
+// checkDatabaseTimezone validates that the database instance timezone is set to UTC.
+// It creates a temporary connection to check the timezone without using the AfterConnect hook.
+func checkDatabaseTimezone(connConfig *pgx.ConnConfig, dbName string, dbLabel string, l *zerolog.Logger) error {
+	tempConn, err := pgx.ConnectConfig(context.Background(), connConfig)
+	if err != nil {
+		return fmt.Errorf("could not create temporary connection to %s to check timezone: %w", dbLabel, err)
+	}
+	defer tempConn.Close(context.Background())
+
+	var dbTimezone string
+	if err := tempConn.QueryRow(context.Background(), "SHOW timezone").Scan(&dbTimezone); err != nil {
+		return fmt.Errorf("could not query %s timezone: %w", dbLabel, err)
+	}
+
+	if dbTimezone != "UTC" {
+		if dbName == "" {
+			dbName = "<your_database_name>"
+		}
+		return fmt.Errorf(
+			"%s instance timezone is set to '%s' but must be 'UTC'\n"+
+				"This check ensures time-based operations work correctly across all sessions\n"+
+				"To fix this issue, you have two options:\n"+
+				"  1. Set your PostgreSQL instance timezone to UTC by running: ALTER DATABASE %s SET TIMEZONE='UTC'\n"+
+				"  2. Disable this check by setting the environment variable: DATABASE_ENFORCE_UTC_TIMEZONE=false\n"+
+				"Note: Disabling this check is not recommended as it may lead to timezone-related issues",
+			dbLabel, dbTimezone, dbName,
+		)
+	}
+
+	l.Info().Msgf("%s instance timezone verified: %s", dbLabel, dbTimezone)
+	return nil
 }
