0b71b39c3a58aaa4bdb8fde87e6e05bf0a0d30b0
mrkaye97 <mrkaye97@gmail.com>
2025-10-10 18:24:04 -0400

    debug: log lines for pubs

--- pkg/repository/v1/payloadstore.go
+++ pkg/repository/v1/payloadstore.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"sort"
 	"time"
@@ -475,12 +476,24 @@ func (p *payloadStoreRepositoryImpl) ProcessPayloadWAL(ctx context.Context, part
 		})
 	}
 
+	tj, _ := json.MarshalIndent(tenantIdToPayloads, "", " ")
+	fmt.Println("tenant to payloads", string(tj))
+
 	for tenantId, payloads := range tenantIdToPayloads {
 		msg, err := OLAPPayloadOffloadMessage(tenantId, payloads)
+
 		if err != nil {
 			return false, fmt.Errorf("failed to create OLAP payload offload message: %w", err)
 		}
-		pubBuffer.Pub(ctx, msgqueue.OLAP_QUEUE, msg, false)
+
+		mj, _ := json.Marshal(msg)
+		fmt.Println("publishing OLAP payload offload message", string(mj))
+
+		err = pubBuffer.Pub(ctx, msgqueue.OLAP_QUEUE, msg, false)
+
+		if err != nil {
+			return false, fmt.Errorf("failed to publish OLAP payload offload message: %w", err)
+		}
 	}
 
 	if err := commit(ctx); err != nil {
