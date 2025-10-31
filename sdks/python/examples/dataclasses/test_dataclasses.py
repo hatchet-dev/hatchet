3632f980c80c57ea615008cdb29838455a469dcb
mrkaye97 <mrkaye97@gmail.com>
2025-10-31 23:25:02 +0100

    fix: ugh

--- sdks/python/examples/dataclasses/test_dataclasses.py
+++ sdks/python/examples/dataclasses/test_dataclasses.py
@@ -1,39 +1,41 @@
 import pytest
 
 from examples.dataclasses.worker import Input, Output, say_hello
-from hatchet_sdk.runnables.workflow import Standalone
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_dataclass_usage(
-    task: Standalone[Input, Output],
-) -> None:
+async def test_dataclass_usage() -> None:
     input = Input(name="Hatchet")
-    x1 = task.run(input)
-    x2 = await task.aio_run(input)
+    x1 = say_hello.run(input)
+    x2 = await say_hello.aio_run(input)
 
-    x3 = task.run_many([task.create_bulk_run_item(input)])[0]
-    x4 = (await task.aio_run_many([task.create_bulk_run_item(input)]))[0]
+    x3 = say_hello.run_many([say_hello.create_bulk_run_item(input)])[0]
+    x4 = (await say_hello.aio_run_many([say_hello.create_bulk_run_item(input)]))[0]
 
-    x5 = task.run_no_wait(input).result()
-    x6 = (await task.aio_run_no_wait(input)).result()
+    x5 = say_hello.run_no_wait(input).result()
+    x6 = (await say_hello.aio_run_no_wait(input)).result()
     x7 = [
-        x.result() for x in task.run_many_no_wait([task.create_bulk_run_item(input)])
+        x.result()
+        for x in say_hello.run_many_no_wait([say_hello.create_bulk_run_item(input)])
     ][0]
     x8 = [
         x.result()
-        for x in await task.aio_run_many_no_wait([task.create_bulk_run_item(input)])
+        for x in await say_hello.aio_run_many_no_wait(
+            [say_hello.create_bulk_run_item(input)]
+        )
     ][0]
 
-    x9 = await task.run_no_wait(input).aio_result()
-    x10 = await (await task.aio_run_no_wait(input)).aio_result()
+    x9 = await say_hello.run_no_wait(input).aio_result()
+    x10 = await (await say_hello.aio_run_no_wait(input)).aio_result()
     x11 = [
         await x.aio_result()
-        for x in task.run_many_no_wait([task.create_bulk_run_item(input)])
+        for x in say_hello.run_many_no_wait([say_hello.create_bulk_run_item(input)])
     ][0]
     x12 = [
         await x.aio_result()
-        for x in await task.aio_run_many_no_wait([task.create_bulk_run_item(input)])
+        for x in await say_hello.aio_run_many_no_wait(
+            [say_hello.create_bulk_run_item(input)]
+        )
     ][0]
 
     assert all(
