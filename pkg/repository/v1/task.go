051f01fcbaddf427143940b6a742e2bbe353593f
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-11-02 17:50:49 +0100

    cache probability stuff

--- pkg/repository/v1/task.go
+++ pkg/repository/v1/task.go
@@ -6,7 +6,10 @@ import (
 	"errors"
 	"fmt"
 	"hash/fnv"
+	"math/rand/v2"
+	"os"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -273,9 +276,17 @@ type TaskRepositoryImpl struct {
 	reassignLimit         int
 	retryQueueLimit       int
 	durableSleepLimit     int
+	probabilityFactor     float64
 }
 
 func newTaskRepository(s *sharedRepository, taskRetentionPeriod time.Duration, maxInternalRetryCount int32, timeoutLimit, reassignLimit, retryQueueLimit, durableSleepLimit int) TaskRepository {
+	p := os.Getenv("CACHE_PROBABILITY_FACTOR")
+	probabilityFactor, err := strconv.ParseFloat(p, 64)
+	if err != nil {
+		s.l.Error().Msgf("failed to parse CACHE_PROBABILITY_FACTOR: %v", err)
+		probabilityFactor = 0.0
+	}
+
 	return &TaskRepositoryImpl{
 		sharedRepository:      s,
 		taskRetentionPeriod:   taskRetentionPeriod,
@@ -284,6 +295,7 @@ func newTaskRepository(s *sharedRepository, taskRetentionPeriod time.Duration, m
 		reassignLimit:         reassignLimit,
 		retryQueueLimit:       retryQueueLimit,
 		durableSleepLimit:     durableSleepLimit,
+		probabilityFactor:     probabilityFactor,
 	}
 }
 
@@ -379,7 +391,11 @@ func (r *TaskRepositoryImpl) UpdateTablePartitions(ctx context.Context) error {
 }
 
 func (r *TaskRepositoryImpl) GetTaskByExternalId(ctx context.Context, tenantId, taskExternalId string, skipCache bool) (*sqlcv1.FlattenExternalIdsRow, error) {
-	if !skipCache {
+
+	ctx, span := telemetry.NewSpan(ctx, "TaskRepositoryImpl.GetTaskByExternalId")
+	defer span.End()
+
+	if !skipCache && rand.Float64() < r.probabilityFactor {
 		// check the cache first
 		key := taskExternalIdTenantIdTuple{
 			externalId: taskExternalId,
@@ -387,6 +403,7 @@ func (r *TaskRepositoryImpl) GetTaskByExternalId(ctx context.Context, tenantId, 
 		}
 
 		if val, ok := r.taskLookupCache.Get(key); ok {
+			span.SetAttributes(attribute.Bool("cache_hit", true))
 			return val, nil
 		}
 	}
