1f387b870cfaf4b5d83697c980d1f1374513d952
mrkaye97 <mrkaye97@gmail.com>
2025-11-17 14:47:51 -0500

    fix: factor out

--- frontend/app/src/components/molecules/analytics-provider.tsx
+++ frontend/app/src/components/molecules/analytics-provider.tsx
@@ -17,8 +17,8 @@ const AnalyticsProvider: React.FC<
   // see: https://posthog.com/tutorials/cross-domain-tracking
   // for setup instructions and more details
   const hashParams = new URLSearchParams(window.location.hash.substring(1));
-  const distinct_id = hashParams.get('distinct_id') ?? undefined;
-  const session_id = hashParams.get('session_id') ?? undefined;
+  const distinct_id = hashParams.get('distinct_id');
+  const session_id = hashParams.get('session_id');
 
   const [loaded, setLoaded] = React.useState(false);
 
@@ -47,17 +47,23 @@ const AnalyticsProvider: React.FC<
 
     console.log('Initializing Analytics, opt out in settings.');
     setLoaded(true);
+
+    const bootstrapConfig =
+      distinct_id && session_id
+        ? `bootstrap: {
+        sessionID: '${session_id}',
+        distinctID: '${distinct_id}'
+      },`
+        : '';
+
     const posthogScript = `
 !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
 posthog.init('${config.apiKey}',{
   api_host:'${config.apiHost}',
+  ${bootstrapConfig}
   session_recording: {
       maskAllInputs: true,
       maskTextSelector: "*"
-  },
-  bootstrap: {
-    sessionID: '${session_id || ''}',
-    distinctID: '${distinct_id || ''}'
   }
 })
 `;
