d74eec52c312d2a85c95ea72e1718e2b6b0305d0
mrkaye97 <mrkaye97@gmail.com>
2025-11-18 12:34:29 -0500

    feat: partitioning migration for v1_dag_data

---
+++ cmd/hatchet-migrate/migrate/migrations/20251118172545_v1_0_54.sql
@@ -0,0 +1,73 @@
+-- +goose Up
+-- +goose StatementBegin
+-- +goose no transaction
+BEGIN;
+
+CREATE TABLE v1_dag_data_partitioned (
+    dag_id BIGINT NOT NULL,
+    dag_inserted_at TIMESTAMPTZ NOT NULL,
+    input JSONB NOT NULL,
+    additional_metadata JSONB,
+    PRIMARY KEY (dag_id, dag_inserted_at)
+) PARTITION BY RANGE(dag_inserted_at);
+
+DO $$
+DECLARE
+    new_table_name TEXT;
+BEGIN
+    -- We want to attach all of the existing data as today's partition, which
+    -- includes everything up until the _end_ of today.
+    -- The new partition will be named with today's date, but the end time will be tomorrow (midnight).
+    new_table_name := 'v1_dag_data_' || TO_CHAR(NOW()::DATE, 'YYYYMMDD');
+
+    RAISE NOTICE 'Renaming existing table to %', new_table_name;
+
+    EXECUTE format('ALTER TABLE v1_dag_data RENAME TO %I', new_table_name);
+    EXECUTE format('ALTER INDEX v1_dag_data_pkey RENAME TO %I', new_table_name || '_pkey');
+
+    EXECUTE
+        format('ALTER TABLE %s SET (
+            autovacuum_vacuum_scale_factor = ''0.1'',
+            autovacuum_analyze_scale_factor=''0.05'',
+            autovacuum_vacuum_threshold=''25'',
+            autovacuum_analyze_threshold=''25'',
+            autovacuum_vacuum_cost_delay=''10'',
+            autovacuum_vacuum_cost_limit=''1000''
+        )', new_table_name);
+
+    EXECUTE
+        format(
+            'ALTER TABLE v1_dag_data_partitioned ATTACH PARTITION %s FOR VALUES FROM (''19700101'') TO (''%s'')',
+            new_table_name,
+            TO_CHAR((NOW() + INTERVAL '1 day')::DATE, 'YYYYMMDD')
+        );
+END $$;
+
+ALTER TABLE v1_dag_data_partitioned RENAME TO v1_dag_data;
+ALTER INDEX v1_dag_data_partitioned_pkey RENAME TO v1_dag_data_pkey;
+
+SELECT create_v1_range_partition('v1_dag_data', (NOW() + INTERVAL '1 day')::DATE);
+COMMIT;
+
+BEGIN;
+ANALYZE v1_dag_data;
+COMMIT;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+CREATE TABLE v1_dag_data_original (
+    dag_id BIGINT NOT NULL,
+    dag_inserted_at TIMESTAMPTZ NOT NULL,
+    input JSONB NOT NULL,
+    additional_metadata JSONB,
+    PRIMARY KEY (dag_id, dag_inserted_at)
+);
+
+INSERT INTO v1_dag_data_original
+SELECT * FROM v1_dag_data;
+
+DROP TABLE v1_dag_data;
+ALTER TABLE v1_dag_data_original RENAME TO v1_dag_data;
+ALTER INDEX v1_dag_data_original_pkey RENAME TO v1_dag_data_pkey;
+-- +goose StatementEnd
