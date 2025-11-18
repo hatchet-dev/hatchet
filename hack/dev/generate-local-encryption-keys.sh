68d25c55f97dac3bc2d0cf95b772e9ca6731f043
Sid Premkumar <sid.premkumar@gmail.com>
2025-11-18 07:55:16 -0800

    revert

--- hack/dev/generate-local-encryption-keys.sh
+++ hack/dev/generate-local-encryption-keys.sh
@@ -5,8 +5,8 @@ set -eux
 
 ENCRYPTION_KEYS_DIR=./encryption-keys
 
-# Read ENCRYPTION_KEYS_DIR from args if exists
-if [ -n "${1:-}" ]; then
+# Read CERTS_DIR from args if exists
+if [ -n "$1" ]; then
     ENCRYPTION_KEYS_DIR=$1
 fi
 
