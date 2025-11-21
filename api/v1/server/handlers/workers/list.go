1080df08fb3c59121a1b37377f84ac072aa57283
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-11-21 13:50:25 -0500

    add spans

--- api/v1/server/handlers/workers/list.go
+++ api/v1/server/handlers/workers/list.go
@@ -1,6 +1,7 @@
 package workers
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -12,33 +13,60 @@ import (
 	"github.com/hatchet-dev/hatchet/pkg/repository"
 	"github.com/hatchet-dev/hatchet/pkg/repository/postgres/dbsqlc"
 	"github.com/hatchet-dev/hatchet/pkg/repository/postgres/sqlchelpers"
+	"github.com/hatchet-dev/hatchet/pkg/telemetry"
 )
 
 func (t *WorkerService) WorkerList(ctx echo.Context, request gen.WorkerListRequestObject) (gen.WorkerListResponseObject, error) {
 	tenant := ctx.Get("tenant").(*dbsqlc.Tenant)
 
+	reqCtx, span := telemetry.NewSpan(ctx.Request().Context(), "GET /api/v1/tenants/{tenant}/worker")
+	defer span.End()
+
+	telemetry.WithAttributes(span,
+		telemetry.AttributeKV{Key: "tenant.id", Value: tenant.ID},
+		telemetry.AttributeKV{Key: "tenant.version", Value: string(tenant.Version)},
+	)
+
+	ctx.SetRequest(ctx.Request().WithContext(reqCtx))
+
 	switch tenant.Version {
 	case dbsqlc.TenantMajorEngineVersionV0:
-		return t.workerListV0(ctx, tenant, request)
+		return t.workerListV0(ctx, reqCtx, tenant, request)
 	case dbsqlc.TenantMajorEngineVersionV1:
-		return t.workerListV1(ctx, tenant, request)
+		return t.workerListV1(ctx, reqCtx, tenant, request)
 	default:
-		return nil, fmt.Errorf("unsupported tenant version: %s", string(tenant.Version))
+		err := fmt.Errorf("unsupported tenant version: %s", string(tenant.Version))
+		span.RecordError(err)
+		return nil, err
 	}
 }
 
-func (t *WorkerService) workerListV0(ctx echo.Context, tenant *dbsqlc.Tenant, request gen.WorkerListRequestObject) (gen.WorkerListResponseObject, error) {
+func (t *WorkerService) workerListV0(ctx echo.Context, reqCtx context.Context, tenant *dbsqlc.Tenant, request gen.WorkerListRequestObject) (gen.WorkerListResponseObject, error) {
 	tenantId := sqlchelpers.UUIDToStr(tenant.ID)
 
 	sixSecAgo := time.Now().Add(-24 * time.Hour)
 
-	workers, err := t.config.APIRepository.Worker().ListWorkers(tenantId, &repository.ListWorkersOpts{
+	opts := &repository.ListWorkersOpts{
 		LastHeartbeatAfter: &sixSecAgo,
-	})
+	}
+
+	_, listSpan := telemetry.NewSpan(reqCtx, "worker-service.v0.list-workers")
+	defer listSpan.End()
+
+	telemetry.WithAttributes(listSpan,
+		telemetry.AttributeKV{Key: "tenant.id", Value: tenant.ID},
+	)
+
+	workers, err := t.config.APIRepository.Worker().ListWorkers(tenantId, opts)
 
 	if err != nil {
+		listSpan.RecordError(err)
 		return nil, err
 	}
+
+	telemetry.WithAttributes(listSpan,
+		telemetry.AttributeKV{Key: "workers.count", Value: len(workers)},
+	)
 
 	rows := make([]gen.Worker, len(workers))
 
@@ -56,18 +84,32 @@ func (t *WorkerService) workerListV0(ctx echo.Context, tenant *dbsqlc.Tenant, re
 	), nil
 }
 
-func (t *WorkerService) workerListV1(ctx echo.Context, tenant *dbsqlc.Tenant, request gen.WorkerListRequestObject) (gen.WorkerListResponseObject, error) {
+func (t *WorkerService) workerListV1(ctx echo.Context, reqCtx context.Context, tenant *dbsqlc.Tenant, request gen.WorkerListRequestObject) (gen.WorkerListResponseObject, error) {
 	tenantId := sqlchelpers.UUIDToStr(tenant.ID)
 
 	sixSecAgo := time.Now().Add(-24 * time.Hour)
 
-	workers, err := t.config.V1.Workers().ListWorkers(tenantId, &repository.ListWorkersOpts{
+	opts := &repository.ListWorkersOpts{
 		LastHeartbeatAfter: &sixSecAgo,
-	})
+	}
+
+	listCtx, listSpan := telemetry.NewSpan(reqCtx, "worker-service.v1.list-workers")
+	defer listSpan.End()
+
+	telemetry.WithAttributes(listSpan,
+		telemetry.AttributeKV{Key: "tenant.id", Value: tenant.ID},
+	)
+
+	workers, err := t.config.V1.Workers().ListWorkers(tenantId, opts)
 
 	if err != nil {
+		listSpan.RecordError(err)
 		return nil, err
 	}
+
+	telemetry.WithAttributes(listSpan,
+		telemetry.AttributeKV{Key: "workers.count", Value: len(workers)},
+	)
 
 	workerIdSet := make(map[string]struct{})
 
@@ -80,14 +122,26 @@ func (t *WorkerService) workerListV1(ctx echo.Context, tenant *dbsqlc.Tenant, re
 		workerIds = append(workerIds, workerId)
 	}
 
+	_, actionsSpan := telemetry.NewSpan(listCtx, "worker-service.v1.get-worker-actions")
+	defer actionsSpan.End()
+
+	telemetry.WithAttributes(actionsSpan,
+		telemetry.AttributeKV{Key: "workers.unique_ids.count", Value: len(workerIds)},
+	)
+
 	workerIdToActionIds, err := t.config.APIRepository.Worker().GetWorkerActionsByWorkerId(
 		sqlchelpers.UUIDToStr(tenant.ID),
 		workerIds,
 	)
 
 	if err != nil {
+		actionsSpan.RecordError(err)
 		return nil, err
 	}
+
+	telemetry.WithAttributes(actionsSpan,
+		telemetry.AttributeKV{Key: "worker_actions.mappings.count", Value: len(workerIdToActionIds)},
+	)
 
 	rows := make([]gen.Worker, len(workers))
 
