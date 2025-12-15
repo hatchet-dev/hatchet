059a4e562cd7feb5cc5728c14b04974decd72400
matt <mrkaye97@gmail.com>
2025-12-15 14:09:55 -0500

    Fix: Root redirect with last tenant atom (#2664)
    * fix: root redirect
    
    * chore: lint

--- frontend/app/src/pages/root-redirect.tsx
+++ frontend/app/src/pages/root-redirect.tsx
@@ -3,9 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 import { queries } from '@/lib/api';
 import { Loading } from '@/components/v1/ui/loading.tsx';
 import { useEffect } from 'react';
+import { useAtom } from 'jotai';
+import { lastTenantAtom } from '@/lib/atoms';
 
 export default function RootRedirect() {
   const navigate = useNavigate();
+  const [lastTenant] = useAtom(lastTenantAtom);
 
   const listMembershipsQuery = useQuery({
     ...queries.user.listTenantMemberships,
@@ -13,6 +16,14 @@ export default function RootRedirect() {
   });
 
   useEffect(() => {
+    if (lastTenant) {
+      navigate(`/tenants/${lastTenant.metadata.id}`, {
+        replace: true,
+      });
+
+      return;
+    }
+
     if (
       listMembershipsQuery.data?.rows &&
       listMembershipsQuery.data.rows.length > 0
@@ -22,9 +33,13 @@ export default function RootRedirect() {
         navigate(`/tenants/${firstTenant.metadata.id}`, {
           replace: true,
         });
+
+        return;
       }
+
+      return;
     }
-  }, [listMembershipsQuery.data, navigate]);
+  }, [listMembershipsQuery.data, navigate, lastTenant]);
 
   return <Loading />;
 }
