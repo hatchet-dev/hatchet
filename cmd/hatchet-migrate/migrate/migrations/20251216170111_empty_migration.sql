2de5bb5fd35b732a64f57ad134e1bd698ee2df23
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-12-17 08:52:06 -0500

    cleanup migrations

--- cmd/hatchet-migrate/migrate/migrations/20251216170111_empty_migration.sql
+++
@@ -1,7 +0,0 @@
--- +goose NO TRANSACTION
--- +goose Up
-CREATE INDEX IF NOT EXISTS v1_task_events_olap_tenant_time_idx ON v1_task_events_olap (tenant_id, task_inserted_at)
-    WITH (timescaledb.transaction_per_chunk);
-
--- +goose Down
-DROP INDEX IF EXISTS v1_task_events_olap_tenant_time_idx;
