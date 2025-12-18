fe6583fc414cd50718d324182e615b08a36a517b
abelanger5 <belanger@sas.upenn.edu>
2025-12-18 13:05:01 -0500

    fix: rare cases of duplicate writes causing stuck updates (#2681)
    * fix: rare cases of duplicate writes causing stuck updates
    
    * update to go 1.25
    
    * fix: add version to sdk-go.yml

--- sql/schema/v1-olap.sql
+++ sql/schema/v1-olap.sql
@@ -595,6 +595,30 @@ REFERENCING NEW TABLE AS new_rows
 FOR EACH STATEMENT
 EXECUTE FUNCTION v1_tasks_olap_insert_function();
 
+CREATE OR REPLACE FUNCTION v1_tasks_olap_delete_function()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    DELETE FROM v1_runs_olap r
+    USING old_rows o
+    WHERE
+        r.inserted_at = o.inserted_at
+        AND r.id = o.id
+        AND r.readable_status = o.readable_status
+        AND r.kind = 'TASK'
+        AND o.dag_id IS NULL;
+
+    RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER v1_tasks_olap_status_delete_trigger
+AFTER DELETE ON v1_tasks_olap
+REFERENCING OLD TABLE AS old_rows
+FOR EACH STATEMENT
+EXECUTE FUNCTION v1_tasks_olap_delete_function();
+
 CREATE OR REPLACE FUNCTION v1_tasks_olap_status_update_function()
 RETURNS TRIGGER AS
 $$
@@ -687,6 +711,29 @@ AFTER INSERT ON v1_dags_olap
 REFERENCING NEW TABLE AS new_rows
 FOR EACH STATEMENT
 EXECUTE FUNCTION v1_dags_olap_insert_function();
+
+CREATE OR REPLACE FUNCTION v1_dags_olap_delete_function()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    DELETE FROM v1_runs_olap r
+    USING old_rows o
+    WHERE
+        r.inserted_at = o.inserted_at
+        AND r.id = o.id
+        AND r.readable_status = o.readable_status
+        AND r.kind = 'DAG';
+
+    RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER v1_dags_olap_status_delete_trigger
+AFTER DELETE ON v1_dags_olap
+REFERENCING OLD TABLE AS old_rows
+FOR EACH STATEMENT
+EXECUTE FUNCTION v1_dags_olap_delete_function();
 
 CREATE OR REPLACE FUNCTION v1_dags_olap_status_update_function()
 RETURNS TRIGGER AS
