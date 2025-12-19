af0ec26eedb436712db0c6ae507dbf3af30a588b
mrkaye97 <mrkaye97@gmail.com>
2025-12-19 14:09:16 -0500

    feat: new function for diffing rows

--- sql/schema/v1-olap.sql
+++ sql/schema/v1-olap.sql
@@ -1105,6 +1105,56 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION diff_payloads_olap_source_and_target_partitions(
+    partition_date date,
+    max_rows int
+) RETURNS TABLE (
+    tenant_id UUID,
+    external_id UUID,
+    inserted_at TIMESTAMPTZ,
+    location v1_payload_location,
+    external_location_key TEXT,
+    inline_content JSONB,
+    updated_at TIMESTAMPTZ
+)
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    partition_date_str varchar;
+    source_partition_name varchar;
+    temp_partition_name varchar;
+    query text;
+BEGIN
+    IF partition_date IS NULL THEN
+        RAISE EXCEPTION 'partition_date parameter cannot be NULL';
+    END IF;
+
+    SELECT to_char(partition_date, 'YYYYMMDD') INTO partition_date_str;
+    SELECT format('v1_payloads_olap_%s', partition_date_str) INTO source_partition_name;
+    SELECT format('v1_payloads_olap_offload_tmp_%s', partition_date_str) INTO temp_partition_name;
+
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = source_partition_name) THEN
+        RAISE EXCEPTION 'Partition % does not exist', source_partition_name;
+    END IF;
+
+    query := format('
+        SELECT *
+        FROM %I source
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM %I AS target
+            WHERE
+                source.tenant_id = target.tenant_id
+                AND source.external_id = target.external_id
+                AND source.inserted_at = target.inserted_at
+        )
+        LIMIT $1
+    ', source_partition_name, temp_partition_name);
+
+    RETURN QUERY EXECUTE query USING max_rows;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION swap_v1_payloads_olap_partition_with_temp(
     partition_date date
 ) RETURNS text
