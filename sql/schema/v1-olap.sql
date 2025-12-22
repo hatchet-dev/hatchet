bcd74f2c2f5edc5260d2baaf9e4ead1226c29e60
mrkaye97 <mrkaye97@gmail.com>
2025-12-22 17:15:16 -0500

    chore: schema

--- sql/schema/v1-olap.sql
+++ sql/schema/v1-olap.sql
@@ -1153,6 +1153,47 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION compute_olap_payload_batch_size(
+    partition_date DATE
+) RETURNS BIGINT
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    partition_date_str TEXT;
+    source_partition_name TEXT;
+    query TEXT;
+    result_size BIGINT;
+BEGIN
+    IF partition_date IS NULL THEN
+        RAISE EXCEPTION 'partition_date parameter cannot be NULL';
+    END IF;
+
+    SELECT to_char(partition_date, 'YYYYMMDD') INTO partition_date_str;
+    SELECT format('v1_payloads_olap_%s', partition_date_str) INTO source_partition_name;
+
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = source_partition_name) THEN
+        RAISE EXCEPTION 'Partition % does not exist', source_partition_name;
+    END IF;
+
+    query := format('
+        WITH candidates AS (
+            SELECT *
+            FROM %I
+            WHERE (tenant_id, external_id, inserted_at) >= ($1::UUID, $2::UUID, $3::TIMESTAMPTZ)
+            ORDER BY tenant_id, external_id, inserted_at
+            LIMIT $4::INT
+        )
+
+        SELECT SUM(pg_column_size(inline_content)) AS total_size_bytes
+        FROM candidates
+    ', source_partition_name);
+
+    EXECUTE query INTO result_size;
+
+    RETURN result_size;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION swap_v1_payloads_olap_partition_with_temp(
     partition_date date
 ) RETURNS text
