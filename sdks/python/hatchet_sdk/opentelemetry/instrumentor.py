9d46ea0624450c596a3bd7b473bd4ab160770b6c
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-12-23 15:06:37 -0500

    lint

--- sdks/python/hatchet_sdk/opentelemetry/instrumentor.py
+++ sdks/python/hatchet_sdk/opentelemetry/instrumentor.py
@@ -8,7 +8,9 @@ from hatchet_sdk.utils.typing import JSONSerializableMapping
 
 try:
     from opentelemetry.context import Context
-    from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+    from opentelemetry.instrumentation.instrumentor import (  # type: ignore[attr-defined]
+        BaseInstrumentor,
+    )
     from opentelemetry.instrumentation.utils import unwrap
     from opentelemetry.metrics import MeterProvider, NoOpMeterProvider, get_meter
     from opentelemetry.trace import (
