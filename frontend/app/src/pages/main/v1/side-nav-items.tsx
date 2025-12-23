ac3f87067d46325e1555fe17041332789195c7ff
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-12-23 09:23:36 -0500

    caps

--- frontend/app/src/pages/main/v1/side-nav-items.tsx
+++ frontend/app/src/pages/main/v1/side-nav-items.tsx
@@ -33,7 +33,7 @@ export function sideNavItems(opts: {
     },
     {
       key: 'tenant-settings-github',
-      name: 'Github',
+      name: 'GitHub',
       to: appRoutes.tenantSettingsGithubRoute.to,
     },
     {
