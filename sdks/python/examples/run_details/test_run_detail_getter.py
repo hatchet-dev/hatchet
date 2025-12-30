b9b295d4934bafd85185546feaa3001586c4539f
mrkaye97 <mrkaye97@gmail.com>
2025-12-30 14:15:49 -0700

    fix: test done flag

--- sdks/python/examples/run_details/test_run_detail_getter.py
+++ sdks/python/examples/run_details/test_run_detail_getter.py
@@ -14,54 +14,57 @@ async def test_run(hatchet: Hatchet) -> None:
 
     await asyncio.sleep(2)
 
-    payloads = hatchet.runs.get_details(ref.workflow_run_id)
+    details = hatchet.runs.get_details(ref.workflow_run_id)
 
-    assert payloads.status == V1TaskStatus.RUNNING
-    assert payloads.input == mock_input.model_dump()
-    assert len(payloads.task_runs) == 4
+    assert details.status == V1TaskStatus.RUNNING
+    assert details.input == mock_input.model_dump()
+    assert len(details.task_runs) == 4
     assert all(
         r.status in [V1TaskStatus.RUNNING, V1TaskStatus.QUEUED]
-        for r in payloads.task_runs.values()
+        for r in details.task_runs.values()
     )
-    assert all(r.error is None for r in payloads.task_runs.values())
-    assert all(r.output is None for r in payloads.task_runs.values())
-    assert "step3" not in payloads.task_runs
-    assert "step4" not in payloads.task_runs
+    assert all(r.error is None for r in details.task_runs.values())
+    assert all(r.output is None for r in details.task_runs.values())
+    assert "step3" not in details.task_runs
+    assert "step4" not in details.task_runs
+    assert details.done is False
 
     try:
         await ref.aio_result()
     except Exception:
         pass
 
-    payloads = hatchet.runs.get_details(ref.workflow_run_id)
+    details = hatchet.runs.get_details(ref.workflow_run_id)
 
-    assert payloads.status == V1TaskStatus.FAILED
-    assert payloads.input == mock_input.model_dump()
-    assert len(payloads.task_runs) == 6
+    assert details.status == V1TaskStatus.FAILED
+    assert details.input == mock_input.model_dump()
+    assert len(details.task_runs) == 6
 
-    assert payloads.task_runs["step1"].status == V1TaskStatus.COMPLETED
-    assert payloads.task_runs["step2"].status == V1TaskStatus.COMPLETED
-    assert payloads.task_runs["step3"].status == V1TaskStatus.COMPLETED
-    assert payloads.task_runs["step4"].status == V1TaskStatus.COMPLETED
-    assert payloads.task_runs["fail_step"].status == V1TaskStatus.FAILED
-    assert payloads.task_runs["cancel_step"].status == V1TaskStatus.CANCELLED
+    assert details.task_runs["step1"].status == V1TaskStatus.COMPLETED
+    assert details.task_runs["step2"].status == V1TaskStatus.COMPLETED
+    assert details.task_runs["step3"].status == V1TaskStatus.COMPLETED
+    assert details.task_runs["step4"].status == V1TaskStatus.COMPLETED
+    assert details.task_runs["fail_step"].status == V1TaskStatus.FAILED
+    assert details.task_runs["cancel_step"].status == V1TaskStatus.CANCELLED
 
     assert (
-        payloads.task_runs["step1"].output["random_number"]  # type: ignore[index]
-        + payloads.task_runs["step2"].output["random_number"]  # type: ignore[index]
-        == payloads.task_runs["step3"].output["sum"]  # type: ignore[index]
+        details.task_runs["step1"].output["random_number"]  # type: ignore[index]
+        + details.task_runs["step2"].output["random_number"]  # type: ignore[index]
+        == details.task_runs["step3"].output["sum"]  # type: ignore[index]
     )
 
-    assert payloads.task_runs["step4"].output == {"step4": "step4"}
+    assert details.task_runs["step4"].output == {"step4": "step4"}
 
-    assert payloads.task_runs["fail_step"].error is not None
-    assert payloads.task_runs["fail_step"].output is None
+    assert details.task_runs["fail_step"].error is not None
+    assert details.task_runs["fail_step"].output is None
 
-    assert payloads.task_runs["cancel_step"].error is None
-    assert payloads.task_runs["cancel_step"].output is None
+    assert details.task_runs["cancel_step"].error is None
+    assert details.task_runs["cancel_step"].output is None
 
     assert all(
         r.error is None
-        for name, r in payloads.task_runs.items()
+        for name, r in details.task_runs.items()
         if name not in ["fail_step"]
     )
+
+    assert details.done is True
