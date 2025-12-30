663dbf660f5dbca330d8b9bbfd3fbd44dc664ca5
mrkaye97 <mrkaye97@gmail.com>
2025-12-30 14:16:40 -0700

    fix: cleanup

--- sdks/python/examples/run_details/test_run_detail_getter.py
+++ sdks/python/examples/run_details/test_run_detail_getter.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 import pytest
 
-from examples.run_details.worker import MockInput, hatchet, run_detail_test_workflow
+from examples.run_details.worker import MockInput, run_detail_test_workflow
 from hatchet_sdk import Hatchet, V1TaskStatus
 
 
