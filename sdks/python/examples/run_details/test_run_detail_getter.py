6242bf49962101bac32948d609a1ad9d36efc75d
mrkaye97 <mrkaye97@gmail.com>
2025-12-30 15:33:14 -0700

    fix: export run status

--- sdks/python/examples/run_details/test_run_detail_getter.py
+++ sdks/python/examples/run_details/test_run_detail_getter.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 import pytest
 
 from examples.run_details.worker import MockInput, run_detail_test_workflow
-from hatchet_sdk import Hatchet, V1TaskStatus
+from hatchet_sdk import Hatchet, RunStatus, V1TaskStatus
 
 
 @pytest.mark.asyncio(loop_scope="session")
@@ -16,7 +16,7 @@ async def test_run(hatchet: Hatchet) -> None:
 
     details = hatchet.runs.get_details(ref.workflow_run_id)
 
-    assert details.status == V1TaskStatus.RUNNING
+    assert details.status == RunStatus.RUNNING
     assert details.input == mock_input.model_dump()
     assert len(details.task_runs) == 4
     assert all(
@@ -34,7 +34,7 @@ async def test_run(hatchet: Hatchet) -> None:
 
     details = hatchet.runs.get_details(ref.workflow_run_id)
 
-    assert details.status == V1TaskStatus.FAILED
+    assert details.status == RunStatus.FAILED
     assert details.input == mock_input.model_dump()
     assert len(details.task_runs) == 6
 
