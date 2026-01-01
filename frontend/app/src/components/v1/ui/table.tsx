76160d5b5da11cc227cf600e9805b3f979a1f7be
mrkaye97 <mrkaye97@gmail.com>
2026-01-01 15:52:56 -0700

    fix: bottom border

--- frontend/app/src/components/v1/ui/table.tsx
+++ frontend/app/src/components/v1/ui/table.tsx
@@ -29,7 +29,7 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn('[&_tr:last-child]:border-0', className)}
+    className={cn(className)}
     {...props}
   />
 ));
