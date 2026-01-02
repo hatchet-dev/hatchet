ef895b42906665735eddff743a603b5821c80e15
mrkaye97 <mrkaye97@gmail.com>
2026-01-01 19:48:04 -0700

    chore: rm dead code

--- frontend/app/src/pages/main/v1/tenant-settings/members/components/members-columns.tsx
+++ frontend/app/src/pages/main/v1/tenant-settings/members/components/members-columns.tsx
@@ -1,18 +1,14 @@
 import { ConfirmDialog } from '@/components/v1/molecules/confirm-dialog';
-import { DataTableColumnHeader } from '@/components/v1/molecules/data-table/data-table-column-header';
 import { DataTableRowActions } from '@/components/v1/molecules/data-table/data-table-row-actions';
-import RelativeDate from '@/components/v1/molecules/relative-date';
 import { useCurrentTenantId } from '@/hooks/use-tenant';
 import api, { TenantMember, queries } from '@/lib/api';
 import { useApiError } from '@/lib/hooks';
 import { UserContextType } from '@/lib/outlet';
 import { useOutletContext } from '@/lib/router-helpers';
-import { capitalize } from '@/lib/utils';
 import useApiMeta from '@/pages/auth/hooks/use-api-meta';
 import useCloud from '@/pages/auth/hooks/use-cloud';
 import queryClient from '@/query-client';
 import { useMutation } from '@tanstack/react-query';
-import { ColumnDef } from '@tanstack/react-table';
 import { useState } from 'react';
 
 // Component for handling member actions
@@ -104,60 +100,3 @@ export function MemberActions({
     </>
   );
 }
-
-export const columns = ({
-  onChangePasswordClick,
-  onEditRoleClick,
-}: {
-  onChangePasswordClick: (row: TenantMember) => void;
-  onEditRoleClick: (row: TenantMember) => void;
-}): ColumnDef<TenantMember>[] => {
-  return [
-    {
-      accessorKey: 'name',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Name" />
-      ),
-      cell: ({ row }) => <div>{row.original.user.name}</div>,
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
-      accessorKey: 'email',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Email" />
-      ),
-      cell: ({ row }) => <div>{row.original.user.email}</div>,
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
-      accessorKey: 'role',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Role" />
-      ),
-      cell: ({ row }) => (
-        <div className="font-medium">{capitalize(row.getValue('role'))}</div>
-      ),
-    },
-    {
-      accessorKey: 'joined',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Joined" />
-      ),
-      cell: ({ row }) => (
-        <RelativeDate date={row.original.metadata.createdAt} />
-      ),
-    },
-    {
-      id: 'actions',
-      cell: ({ row }) => (
-        <MemberActions
-          member={row.original}
-          onChangePasswordClick={onChangePasswordClick}
-          onEditRoleClick={onEditRoleClick}
-        />
-      ),
-    },
-  ];
-};
