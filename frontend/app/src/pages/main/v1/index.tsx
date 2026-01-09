2a953b33dee4ab3d38786371f5d5541bdea5144d
sebastiangraz <graz@live.se>
2026-01-09 19:09:04 +0100

    Add comment

--- frontend/app/src/pages/main/v1/index.tsx
+++ frontend/app/src/pages/main/v1/index.tsx
@@ -41,6 +41,7 @@ function Main() {
       mainContainerType="inline-size"
     >
       {/* TODO-DESIGN: replace the color with a tailwind color */}
+      {/* NOTE: shadow is used instead of border to avoid dom layout shift within inline-size containers */}
       <div className="shadow-[inset_1px_0_0_0_hsl(var(--border)/0.5),inset_0_1px_0_0_hsl(var(--border)/0.5)] md:rounded-tl-xl p-4 h-full dark:bg-[#050A23] bg-background overflow-y-auto">
         <OutletWithContext context={childCtx} />
       </div>
