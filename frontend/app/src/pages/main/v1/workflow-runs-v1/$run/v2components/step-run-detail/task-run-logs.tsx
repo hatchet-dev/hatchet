6c0cc1727ecc808a005d3b245093e8c43d5f6dc9
mrkaye97 <mrkaye97@gmail.com>
2026-01-20 17:22:31 -0500

    feat: add a flag to enable the new experience

--- frontend/app/src/pages/main/v1/workflow-runs-v1/$run/v2components/step-run-detail/task-run-logs.tsx
+++ frontend/app/src/pages/main/v1/workflow-runs-v1/$run/v2components/step-run-detail/task-run-logs.tsx
@@ -2,6 +2,9 @@ import { LogSearchInput } from '@/components/v1/cloud/logging/log-search/log-sea
 import { useLogs } from '@/components/v1/cloud/logging/log-search/use-logs';
 import { LogViewer } from '@/components/v1/cloud/logging/log-viewer';
 import { V1TaskSummary } from '@/lib/api';
+import useCloud from '@/pages/auth/hooks/use-cloud';
+import { appRoutes } from '@/router';
+import { useParams } from '@tanstack/react-router';
 
 export function TaskRunLogs({
   taskRun,
@@ -10,14 +13,19 @@ export function TaskRunLogs({
   taskRun: V1TaskSummary;
   resetTrigger?: number;
 }) {
+  const { tenant: tenantId } = useParams({ from: appRoutes.tenantRoute.to });
   const { logs, queryString, setQueryString, handleScroll } = useLogs({
     taskRun,
     resetTrigger,
   });
+  const { featureFlags } = useCloud(tenantId);
+  const isLogSearchEnabled = featureFlags?.enable_log_search === 'true';
 
   return (
     <div className="my-4 flex flex-col gap-y-2">
-      <LogSearchInput value={queryString} onChange={setQueryString} />
+      {isLogSearchEnabled && (
+        <LogSearchInput value={queryString} onChange={setQueryString} />
+      )}
       <LogViewer logs={logs} onScroll={handleScroll} />
     </div>
   );
