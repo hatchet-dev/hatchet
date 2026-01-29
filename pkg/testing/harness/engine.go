43fcdd7ecda97c970160cfedd72fb7cb725266fc
Alexander Belanger <alexander@hatchet.run>
2026-01-29 15:55:01 -0500

    add optimistic to testing matrix, remove pg-only mode

--- pkg/testing/harness/engine.go
+++ pkg/testing/harness/engine.go
@@ -28,7 +28,7 @@ import (
 	"github.com/hatchet-dev/hatchet/pkg/random"
 )
 
-func getEnvConfig() (string, bool, string) {
+func getEnvConfig() (string, bool, string, bool) {
 	// Get migration strategy: penultimate or latest
 	migrateStrategy := os.Getenv("TESTING_MATRIX_MIGRATE")
 	if migrateStrategy == "" {
@@ -44,7 +44,10 @@ func getEnvConfig() (string, bool, string) {
 		pgVersion = "16-alpine" // Default value
 	}
 
-	return migrateStrategy, rabbitmqEnabled, pgVersion
+	// Get whether optimistic scheduling is enabled
+	isOptimistic := strings.ToLower(os.Getenv("TESTING_MATRIX_OPTIMISTIC_SCHEDULING")) == "true"
+
+	return migrateStrategy, rabbitmqEnabled, pgVersion, isOptimistic
 }
 
 func RunTestWithEngine(m *testing.M) {
@@ -75,6 +78,7 @@ func RunTestWithEngine(m *testing.M) {
 			goleak.IgnoreTopFunction("github.com/rabbitmq/amqp091-go.(*Connection).heartbeater"),
 			goleak.IgnoreTopFunction("github.com/rabbitmq/amqp091-go.(*consumers).buffer"),
 			goleak.IgnoreTopFunction("google.golang.org/grpc/internal/transport.(*http2Server).keepalive"),
+			goleak.IgnoreTopFunction("github.com/hashicorp/golang-lru/v2/expirable.NewLRU[...].func1"),
 		); err != nil {
 			fmt.Fprintf(os.Stderr, "goleak: Errors on successful test run: %v\n", err)
 			exitCode = 1
@@ -90,7 +94,7 @@ func startEngine() func() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	// Get configuration values from environment
-	migrateStrategy, rabbitmqEnabled, pgVersion := getEnvConfig()
+	migrateStrategy, rabbitmqEnabled, pgVersion, isOptimistic := getEnvConfig()
 
 	log.Printf("Starting engine with migration strategy: %s, RabbitMQ enabled: %t, PostgreSQL version: %s", migrateStrategy, rabbitmqEnabled, pgVersion)
 
@@ -128,6 +132,12 @@ func startEngine() func() {
 	} else {
 		os.Setenv("SERVER_MSGQUEUE_KIND", "postgres")
 		cleanupRabbitMQ = func() error { return nil }
+	}
+
+	if isOptimistic {
+		os.Setenv("SERVER_OPTIMISTIC_SCHEDULING_ENABLED", "true")
+	} else {
+		os.Setenv("SERVER_OPTIMISTIC_SCHEDULING_ENABLED", "false")
 	}
 
 	// Run migrations
