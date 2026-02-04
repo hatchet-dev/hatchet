1201c20ee385c17122082732d5295c241cdbef4b
mrkaye97 <mrkaye97@gmail.com>
2026-02-04 13:08:32 -0500

    chore: fix doc generator

--- sdks/python/docs/generator/generate.py
+++ sdks/python/docs/generator/generate.py
@@ -6,7 +6,7 @@ from typing import cast
 from docs.generator.llm import parse_markdown
 from docs.generator.paths import crawl_directory, find_child_paths
 from docs.generator.shared import TMP_GEN_PATH
-from docs.generator.types import Document
+from docs.generator.doc_types import Document
 from docs.generator.utils import gather_max_concurrency, rm_rf
 
 
