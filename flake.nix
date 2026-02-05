27e43529d5ffdbfa24a23f6b2aa97954e3d0dd4e
Ivan Malison <IvanMalison@gmail.com>
2026-02-04 21:52:09 -0800

    dev: add lint tools to nix shell

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        pkgsUnstable = import nixpkgs-unstable { inherit system; };
         go = pkgs.go;
       in
       {
@@ -18,10 +20,13 @@
             go
             pkgs.gopls
             pkgs.gotools
-            pkgs.golangci-lint
+            pkgsUnstable.golangci-lint
+            pkgs.go-task
             pkgs.protobuf
             pkgs.protoc-gen-go
             pkgs.protoc-gen-go-grpc
+            pkgs.nodejs_20
+            pkgs.pnpm
             pkgs.python312
             pkgs.poetry
             pkgs.gcc
