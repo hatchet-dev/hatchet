96275036d39b251fffddd154d36283482df82247
mrkaye97 <mrkaye97@gmail.com>
2026-02-06 12:48:10 -0500

    chore: fix grpc version

--- sdks/python/hatchet_sdk/contracts/v1/workflows_pb2_grpc.py
+++ sdks/python/hatchet_sdk/contracts/v1/workflows_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from hatchet_sdk.contracts.v1 import workflows_pb2 as v1_dot_workflows__pb2
 
-GRPC_GENERATED_VERSION = '1.78.0'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
