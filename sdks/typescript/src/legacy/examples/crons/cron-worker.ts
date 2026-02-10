f440aa7491fc0fce717b0ca086daa54e25ebda53
Gabe Ruttner <gabriel.ruttner@gmail.com>
2026-02-10 07:56:45 -0500

    chore: restore legacy examples

---
+++ sdks/typescript/src/legacy/examples/crons/cron-worker.ts
@@ -0,0 +1,37 @@
+import Hatchet from '../../../sdk';
+import { Workflow } from '../../../workflow';
+
+const hatchet = Hatchet.init();
+
+// > Workflow Definition Cron Trigger
+// Adding a cron trigger to a workflow is as simple as adding a `cron expression` to the `on` prop of the workflow definition
+
+export const simpleCronWorkflow: Workflow = {
+  id: 'simple-cron-workflow',
+  on: {
+    // 👀 define the cron expression to run every minute
+    cron: '* * * * *',
+  },
+  // ... normal workflow definition
+  description: 'return the current time every minute',
+  steps: [
+    {
+      name: 'what-time-is-it',
+      run: (ctx) => {
+        return { time: new Date().toISOString() };
+      },
+    },
+  ],
+  // ,
+};
+// !!
+
+async function main() {
+  const worker = await hatchet.worker('example-worker');
+  await worker.registerWorkflow(simpleCronWorkflow);
+  worker.start();
+}
+
+if (require.main === module) {
+  main();
+}
---
+++ sdks/typescript/src/legacy/examples/on-failure.ts
@@ -0,0 +1,53 @@
+import Hatchet from '../../sdk';
+import { Workflow } from '../../workflow';
+
+const hatchet = Hatchet.init();
+
+// > OnFailure Step
+// This workflow will fail because the step will throw an error
+// we define an onFailure step to handle this case
+
+const workflow: Workflow = {
+  // ... normal workflow definition
+  id: 'on-failure-example',
+  description: 'test',
+  on: {
+    event: 'user:create',
+  },
+  // ,
+  steps: [
+    {
+      name: 'step1',
+      run: async (ctx) => {
+        // 👀 this step will always throw an error
+        throw new Error('Step 1 failed');
+      },
+    },
+  ],
+  // 👀 After the workflow fails, this special step will run
+  onFailure: {
+    name: 'on-failure-step',
+    run: async (ctx) => {
+      // 👀 we can do things like perform cleanup logic
+      // or notify a user here
+
+      // 👀 you can access the error from the failed step(s) like this
+      console.log(ctx.errors());
+
+      return { onFailure: 'step' };
+    },
+  },
+};
+// !!
+
+// > OnFailure With Details
+// Coming soon to TypeScript! https://github.com/hatchet-dev/hatchet-typescript/issues/447
+// !!
+
+async function main() {
+  const worker = await hatchet.worker('example-worker', 1);
+  await worker.registerWorkflow(workflow);
+  worker.start();
+}
+
+main();
