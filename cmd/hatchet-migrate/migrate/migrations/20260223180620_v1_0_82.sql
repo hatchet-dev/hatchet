22b2ceed51157cd8d962c3e19b4bded457b6f5d3
Mohammed Nafees <hello@mnafees.me>
2026-02-23 18:08:49 +0100

    add constraint and migration

---
+++ cmd/hatchet-migrate/migrate/migrations/20260223180620_v1_0_82.sql
@@ -0,0 +1,12 @@
+-- +goose NO TRANSACTION
+
+-- +goose Up
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS "WorkflowTriggerCronRef_pkey"
+    ON "WorkflowTriggerCronRef" ("id");
+
+ALTER TABLE "WorkflowTriggerCronRef"
+    ADD CONSTRAINT "WorkflowTriggerCronRef_pkey"
+    PRIMARY KEY USING INDEX "WorkflowTriggerCronRef_pkey";
+
+-- +goose Down
+ALTER TABLE "WorkflowTriggerCronRef" DROP CONSTRAINT IF EXISTS "WorkflowTriggerCronRef_pkey";
