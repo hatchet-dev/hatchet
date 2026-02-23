ba67de15277362233d2db1ce0336018dc9ce198f
GitHub Action <action@github.com>
2026-02-23 12:32:36 +0000

    chore: regenerate examples

--- examples/typescript/middleware/run.ts
+++ examples/typescript/middleware/run.ts
@@ -1,16 +1,16 @@
-import { taskWithMiddleware } from "./workflow";
+import { taskWithMiddleware } from './workflow';
 
 async function main() {
   // > Running a task with middleware
   const result = await taskWithMiddleware.run({
-      message: 'hello', // string  (from TaskInput)
-      first: 1, // number  (from GlobalInputType)
-      second: 2, // number  (from GlobalInputType)
+    message: 'hello', // string  (from TaskInput)
+    first: 1, // number  (from GlobalInputType)
+    second: 2, // number  (from GlobalInputType)
   });
 
   console.log('result', result.message); // string  (from TaskOutput)
-  console.log('result', result.extra);   // number  (from GlobalOutputType)
-  console.log('result', result.additionalData);   // number  (from Post Middleware)
+  console.log('result', result.extra); // number  (from GlobalOutputType)
+  console.log('result', result.additionalData); // number  (from Post Middleware)
 }
 
 if (require.main === module) {
