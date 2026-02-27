85752483266831cf4cb16817ed201fa542a6251e
Gabe Ruttner <gabriel.ruttner@gmail.com>
2026-02-27 11:48:26 -0500

    refactor: analyze parent tables in migraiton

--- cmd/hatchet-migrate/migrate/migrations/20260226120000_v1_0_83_olap_evicted_status.sql
+++ cmd/hatchet-migrate/migrate/migrations/20260226120000_v1_0_83_olap_evicted_status.sql
@@ -40,13 +40,70 @@ $$;
 -- +goose StatementEnd
 
 -- +goose StatementBegin
-DO $$
+WITH partitions AS (
+      SELECT inhrelid::regclass::text AS partition_name
+      FROM pg_inherits
+      WHERE inhparent = parent_table::regclass
+)
+
+SELECT create_v1_partition_with_status(partition_name, 'EVICTED')
+FROM partitions
+-- +goose StatementEnd
+
+ALTER TABLE v1_task_events_olap ADD COLUMN IF NOT EXISTS durable_invocation_count INT NOT NULL DEFAULT 0;
+
+ANALYZE v1_tasks_olap;
+ANALYZE v1_dags_olap;
+ANALYZE v1_runs_olap;
+ANALYZE v1_task_events_olap_tmp;
+ANALYZE v1_task_events_olap;
+ANALYZE v1_incoming_webhook_validation_failures_olap;
+ANALYZE v1_payloads_olap;
+ANALYZE v1_task_status_updates_tmp;
+ANALYZE v1_events_olap;
+ANALYZE v1_event_lookup_table_olap;
+ANALYZE v1_event_to_run_olap;
+ANALYZE v1_cel_evaluation_failures_olap;
+
+-- +goose Down
+-- +goose NO TRANSACTION
+
+ALTER TABLE v1_task_events_olap DROP COLUMN IF EXISTS durable_invocation_count;
+
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION create_v1_olap_partition_with_date_and_status(
+    targetTableName text,
+    targetDate date
+) RETURNS integer
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    targetDateStr varchar;
+    targetDatePlusOneDayStr varchar;
+    newTableName varchar;
 BEGIN
-    PERFORM create_v1_partition_with_status(inhrelid::regclass::text, 'EVICTED')
-    FROM pg_inherits
-    WHERE inhparent = ANY(ARRAY['v1_tasks_olap'::regclass, 'v1_runs_olap'::regclass, 'v1_dags_olap'::regclass]);
+    SELECT to_char(targetDate, 'YYYYMMDD') INTO targetDateStr;
+    SELECT to_char(targetDate + INTERVAL '1 day', 'YYYYMMDD') INTO targetDatePlusOneDayStr;
+    SELECT format('%s_%s', targetTableName, targetDateStr) INTO newTableName;
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = newTableName) THEN
+        EXECUTE format('CREATE TABLE %s (LIKE %s INCLUDING INDEXES) PARTITION BY LIST (readable_status)', newTableName, targetTableName);
+    END IF;
+
+    PERFORM create_v1_partition_with_status(newTableName, 'QUEUED');
+    PERFORM create_v1_partition_with_status(newTableName, 'RUNNING');
+    PERFORM create_v1_partition_with_status(newTableName, 'COMPLETED');
+    PERFORM create_v1_partition_with_status(newTableName, 'CANCELLED');
+    PERFORM create_v1_partition_with_status(newTableName, 'FAILED');
+
+    IF NOT EXISTS (SELECT 1 FROM pg_inherits WHERE inhrelid = newTableName::regclass) THEN
+        EXECUTE format('ALTER TABLE %s ATTACH PARTITION %s FOR VALUES FROM (''%s'') TO (''%s'')', targetTableName, newTableName, targetDateStr, targetDatePlusOneDayStr);
+    END IF;
+
+    RETURN 1;
 END;
 $$;
 -- +goose StatementEnd
 
-ALTER TABLE v1_task_events_olap ADD COLUMN IF NOT EXISTS durable_invocation_count INT NOT NULL DEFAULT 0;
+-- NOTE: Postgres does not support removing enum values.
+-- The 'EVICTED' value in v1_readable_status_olap cannot be reverted.
+-- Any EVICTED partitions created by this migration would need to be merged/dropped separately.
