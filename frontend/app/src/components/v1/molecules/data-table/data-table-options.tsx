24fc6e53865af596253c46989f5ce0bed6eb67d4
mrkaye97 <mrkaye97@gmail.com>
2026-02-28 09:46:06 -0500

    fix: more problematic z indexes

--- frontend/app/src/components/v1/molecules/data-table/data-table-options.tsx
+++ frontend/app/src/components/v1/molecules/data-table/data-table-options.tsx
@@ -132,7 +132,7 @@ function FilterControl<TData>({ column, filter }: FilterControlProps<TData>) {
                 <SelectTrigger className="h-8 text-xs">
                   <SelectValue placeholder="Choose time range" />
                 </SelectTrigger>
-                <SelectContent className="z-[80]">
+                <SelectContent>
                   <SelectItem value="1h">1 hour</SelectItem>
                   <SelectItem value="6h">6 hours</SelectItem>
                   <SelectItem value="1d">1 day</SelectItem>
