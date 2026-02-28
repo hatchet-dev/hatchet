9c8143c07579a1699826a749d71227b04c054918
Gabe Ruttner <gabriel.ruttner@gmail.com>
2026-02-28 13:31:38 -0500

    fix: release version

--- sdks/typescript/src/step.ts
+++ sdks/typescript/src/step.ts
@@ -1,7 +1,7 @@
 export * from './legacy/step';
 
 console.warn(
-  '\x1b[31mDeprecation warning: The v0 sdk, including the step module has been deprecated and has been removed in release v1.12.0.\x1b[0m'
+  '\x1b[31mDeprecation warning: The v0 sdk, including the step module has been deprecated and has been removed in release v1.14.0.\x1b[0m'
 );
 console.warn(
   '\x1b[32mPlease migrate to v1 SDK instead: https://docs.hatchet.run/home/v1-sdk-improvements\x1b[0m'
--- sdks/typescript/src/workflow.ts
+++ sdks/typescript/src/workflow.ts
@@ -5,7 +5,7 @@ export * from './legacy/workflow';
 export { ConcurrencyLimitStrategy, StickyStrategy };
 
 console.warn(
-  '\x1b[31mDeprecation warning: The v0 sdk, including the workflow module has been deprecated and has been removed in release v1.12.0.\x1b[0m'
+  '\x1b[31mDeprecation warning: The v0 sdk, including the workflow module has been deprecated and has been removed in release v1.14.0.\x1b[0m'
 );
 console.warn(
   '\x1b[31mPlease migrate to v1 SDK instead: https://docs.hatchet.run/home/v1-sdk-improvements\x1b[0m'
