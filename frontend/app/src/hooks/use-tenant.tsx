e3deab96afc0f09b7226c3370a6dfa828e9ea178
Josh Duff <me@JoshDuff.com>
2026-03-03 16:40:02 -0600

    invalidate the user universe when creating a new tenant

--- frontend/app/src/hooks/use-tenant.tsx
+++ frontend/app/src/hooks/use-tenant.tsx
@@ -8,6 +8,7 @@ import { BillingContext, lastTenantAtom } from '@/lib/atoms';
 import { Evaluate } from '@/lib/can/shared/permission.base';
 import useCloud from '@/pages/auth/hooks/use-cloud';
 import { useAppContext } from '@/providers/app-context';
+import { useUserUniverse } from '@/providers/user-universe';
 import { appRoutes } from '@/router';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useMatchRoute, useNavigate, useParams } from '@tanstack/react-router';
@@ -41,7 +42,7 @@ export function useTenantDetails() {
   const tenantId = appContext.tenantId;
   const tenant = appContext.tenant;
   const membership = appContext.membership;
-
+  const { invalidate: invalidateUserUniverse } = useUserUniverse();
   const queryClient = useQueryClient();
   const matchRoute = useMatchRoute();
   const navigate = useNavigate();
@@ -97,6 +98,7 @@ export function useTenantDetails() {
     },
     onSuccess: async (data) => {
       await queryClient.invalidateQueries({ queryKey: ['user:*'] });
+      await invalidateUserUniverse();
       if (data.metadata.id) {
         setTenant(data);
       }
