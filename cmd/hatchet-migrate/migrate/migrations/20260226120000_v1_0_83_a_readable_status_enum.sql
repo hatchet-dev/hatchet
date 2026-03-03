cebab8769573ee4f610f9eea86df6c6af713758e
mrkaye97 <mrkaye97@gmail.com>
2026-03-03 17:29:46 -0500

    fix: combine migrations

--- cmd/hatchet-migrate/migrate/migrations/20260226120000_v1_0_83_a_readable_status_enum.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TYPE v1_readable_status_olap ADD VALUE IF NOT EXISTS 'EVICTED';
-
--- +goose Down
--- NOTE: Postgres does not support removing enum values.
--- The 'EVICTED' value in v1_readable_status_olap cannot be reverted.
--- Any EVICTED partitions created by this migration would need to be merged/dropped separately.
