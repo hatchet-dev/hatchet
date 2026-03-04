8b520b19ec21d925df8fd312ecf99f0c01d2ad3b
mrkaye97 <mrkaye97@gmail.com>
2026-03-04 10:50:20 -0500

    chore: gen

--- examples/ruby/runtime_affinity/worker.rb
+++ examples/ruby/runtime_affinity/worker.rb
@@ -6,7 +6,7 @@ require "optparse"
 HATCHET = Hatchet::Client.new(debug: true) unless defined?(HATCHET)
 
 AFFINITY_EXAMPLE_TASK = HATCHET.task(name: :affinity_example_task) do |_input, ctx|
-  { "worker_id" => ctx.worker.id }
+  { "worker_id" => ctx.worker_id }
 end
 
 if __FILE__ == $PROGRAM_NAME
