7c1c240b5dc74fccd5bd7b1cdac416d2788de690
Josh Duff <me@JoshDuff.com>
2026-03-05 11:30:49 -0600

    fix docs link

--- frontend/app/src/pages/main/v1/workflow-runs-v1/components/runs-table.tsx
+++ frontend/app/src/pages/main/v1/workflow-runs-v1/components/runs-table.tsx
@@ -257,7 +257,7 @@ export function RunsTable({ leftLabel }: { leftLabel?: string }) {
               <p className="text-lg font-semibold">No runs found</p>
               <div className="w-fit">
                 <DocsButton
-                  doc={docsPages.v1.quickstart}
+                  doc={docsPages.home['your-first-task']}
                   label={'Learn more about tasks'}
                 />
               </div>
