9e3aecf5933992fa4e8e95bc1450fca68504b57b
mrkaye97 <mrkaye97@gmail.com>
2026-03-05 14:03:22 -0500

    fix: update

--- pkg/repository/sqlcv1/tasks.sql
+++ pkg/repository/sqlcv1/tasks.sql
@@ -9,7 +9,8 @@ SELECT
     create_v1_weekly_range_partition('v1_event_lookup_table', @date::date),
     create_v1_range_partition('v1_event_to_run', @date::date),
     create_v1_range_partition('v1_durable_event_log_file', @date::date),
-    create_v1_range_partition('v1_durable_event_log_entry', @date::date, 80)
+    create_v1_range_partition('v1_durable_event_log_entry', @date::date, 80),
+    create_v1_range_partition('v1_durable_event_log_branch_point', @date::date, 80)
 ;
 
 -- name: EnsureTablePartitionsExist :one
@@ -32,6 +33,8 @@ WITH tomorrow_date AS (
     SELECT 'v1_durable_event_log_file_' || to_char((SELECT date FROM tomorrow_date), 'YYYYMMDD')
     UNION ALL
     SELECT 'v1_durable_event_log_entry_' || to_char((SELECT date FROM tomorrow_date), 'YYYYMMDD')
+    UNION ALL
+    SELECT 'v1_durable_event_log_branch_point_' || to_char((SELECT date FROM tomorrow_date), 'YYYYMMDD')
 ), partition_check AS (
     SELECT
         COUNT(*) AS total_tables,
@@ -67,6 +70,8 @@ WITH task_partitions AS (
     SELECT 'v1_durable_event_log_file' AS parent_table, p::text as partition_name FROM get_v1_partitions_before_date('v1_durable_event_log_file', @date::date) AS p
 ), durable_event_log_entry_partitions AS (
     SELECT 'v1_durable_event_log_entry' AS parent_table, p::text as partition_name FROM get_v1_partitions_before_date('v1_durable_event_log_entry', @date::date) AS p
+), durable_event_log_branch_point_partitions AS (
+    SELECT 'v1_durable_event_log_branch_point' AS parent_table, p::text as partition_name FROM get_v1_partitions_before_date('v1_durable_event_log_branch_point', @date::date) AS p
 )
 
 SELECT
@@ -136,6 +141,13 @@ SELECT
     *
 FROM
     durable_event_log_entry_partitions
+
+UNION ALL
+
+SELECT
+    *
+FROM
+    durable_event_log_branch_point_partitions
 ;
 
 -- name: DefaultTaskActivityGauge :one
--- pkg/repository/sqlcv1/tasks.sql.go
+++ pkg/repository/sqlcv1/tasks.sql.go
@@ -235,7 +235,8 @@ SELECT
     create_v1_weekly_range_partition('v1_event_lookup_table', $1::date),
     create_v1_range_partition('v1_event_to_run', $1::date),
     create_v1_range_partition('v1_durable_event_log_file', $1::date),
-    create_v1_range_partition('v1_durable_event_log_entry', $1::date, 80)
+    create_v1_range_partition('v1_durable_event_log_entry', $1::date, 80),
+    create_v1_range_partition('v1_durable_event_log_branch_point', $1::date, 80)
 `
 
 func (q *Queries) CreatePartitions(ctx context.Context, db DBTX, date pgtype.Date) error {
@@ -335,6 +336,8 @@ WITH tomorrow_date AS (
     SELECT 'v1_durable_event_log_file_' || to_char((SELECT date FROM tomorrow_date), 'YYYYMMDD')
     UNION ALL
     SELECT 'v1_durable_event_log_entry_' || to_char((SELECT date FROM tomorrow_date), 'YYYYMMDD')
+    UNION ALL
+    SELECT 'v1_durable_event_log_branch_point_' || to_char((SELECT date FROM tomorrow_date), 'YYYYMMDD')
 ), partition_check AS (
     SELECT
         COUNT(*) AS total_tables,
@@ -1320,6 +1323,8 @@ WITH task_partitions AS (
     SELECT 'v1_durable_event_log_file' AS parent_table, p::text as partition_name FROM get_v1_partitions_before_date('v1_durable_event_log_file', $1::date) AS p
 ), durable_event_log_entry_partitions AS (
     SELECT 'v1_durable_event_log_entry' AS parent_table, p::text as partition_name FROM get_v1_partitions_before_date('v1_durable_event_log_entry', $1::date) AS p
+), durable_event_log_branch_point_partitions AS (
+    SELECT 'v1_durable_event_log_branch_point' AS parent_table, p::text as partition_name FROM get_v1_partitions_before_date('v1_durable_event_log_branch_point', $1::date) AS p
 )
 
 SELECT
@@ -1389,6 +1394,13 @@ SELECT
     parent_table, partition_name
 FROM
     durable_event_log_entry_partitions
+
+UNION ALL
+
+SELECT
+    parent_table, partition_name
+FROM
+    durable_event_log_branch_point_partitions
 `
 
 type ListPartitionsBeforeDateRow struct {
