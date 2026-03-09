b77caf8c9266658010cec2b03cbc6ccf584e34fd
mrkaye97 <mrkaye97@gmail.com>
2026-03-09 17:11:53 -0400

    feat: basic ttl cache

--- sdks/python/hatchet_sdk/utils/cache.py
+++ sdks/python/hatchet_sdk/utils/cache.py
@@ -1,5 +1,8 @@
+import asyncio
 from collections import OrderedDict
-from typing import TypeVar
+from dataclasses import dataclass
+from datetime import UTC, datetime, timedelta
+from typing import Generic, TypeVar
 
 K = TypeVar("K")
 V = TypeVar("V")
@@ -18,3 +21,35 @@ class BoundedDict(OrderedDict[K, V]):
 
         if len(self) > self.maxsize:
             self.popitem(last=False)
+
+
+@dataclass
+class TTLCacheEntry(Generic[V]):
+    value: V
+    expires_at: datetime
+
+
+class TTLCache(dict[K, V]):
+    def __init__(self, ttl: timedelta) -> None:
+        self.ttl = ttl
+        self.cache: dict[K, TTLCacheEntry[V]] = {}
+
+        self.eviction_job = asyncio.create_task(self._start_eviction_job())
+
+    def __setitem__(self, key: K, value: V) -> None:
+        self.cache[key] = TTLCacheEntry(
+            value=value, expires_at=datetime.now(tz=UTC) + self.ttl
+        )
+
+    def __getitem__(self, key: K) -> V:
+        return self.cache[key].value
+
+    async def _start_eviction_job(self) -> None:
+        while True:
+            now = datetime.now(tz=UTC)
+
+            for key, entry in self.cache.items():
+                if entry.expires_at <= now:
+                    del self.cache[key]
+
+            await asyncio.sleep(self.ttl.total_seconds())
