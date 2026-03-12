3bd3a014bdcc2a14299ce2346d5f19d8fca759cd
Mohammed Nafees <hello@mnafees.me>
2026-03-12 23:03:45 +0100

    gif again

--- sdks/python/examples/simple/schedule.py
+++ sdks/python/examples/simple/schedule.py
@@ -4,7 +4,9 @@ from examples.simple.worker import simple
 
 # > Schedule a Task
 
-tomorrow_noon = datetime.now(tz=timezone.utc).replace(hour=12, minute=0, second=0, microsecond=0) + timedelta(days=1)
+tomorrow_noon = datetime.now(tz=timezone.utc).replace(
+    hour=12, minute=0, second=0, microsecond=0
+) + timedelta(days=1)
 
 scheduled_run = simple.schedule(tomorrow_noon, input={"Message": "hello"})
 
