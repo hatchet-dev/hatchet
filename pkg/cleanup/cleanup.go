ad3c0be8c671f7a30d68ff2532b042dd2ec1d1cd
Julius Park <juliusgeo@gmail.com>
2026-03-13 12:04:09 -0400

    add buffering

--- pkg/cleanup/cleanup.go
+++ pkg/cleanup/cleanup.go
@@ -13,12 +13,14 @@ type CleanupFn struct {
 }
 
 type Cleanup struct {
-	Fns []CleanupFn
+	Fns       []CleanupFn
+	TimeLimit time.Duration
 }
 
 func New() Cleanup {
 	return Cleanup{
-		Fns: []CleanupFn{},
+		Fns:       []CleanupFn{},
+		TimeLimit: time.Second * 30,
 	}
 }
 
@@ -30,15 +32,23 @@ func (c *Cleanup) Add(fn func() error, name string) {
 }
 
 func (c *Cleanup) Run(l *zerolog.Logger) error {
-	l.Warn().Msgf("waiting for all other services to gracefully exit...")
+	lines := []string{}
+	start := time.Now()
+	lines = append(lines, "waiting for all other services to gracefully exit...")
 	for i, fn := range c.Fns {
-		l.Warn().Msgf("shutting down %s (%d/%d)\n", fn.Name, i+1, len(c.Fns))
+		lines = append(lines, fmt.Sprintf("shutting down %s (%d/%d)", fn.Name, i+1, len(c.Fns)))
 		before := time.Now()
 		if err := fn.Fn(); err != nil {
 			return fmt.Errorf("could not teardown %s: %w", fn.Name, err)
 		}
-		l.Warn().Msgf("successfully shutdown %s in %s (%d/%d)\n", fn.Name, time.Since(before), i+1, len(c.Fns))
+		lines = append(lines, fmt.Sprintf("successfully shutdown %s in %s (%d/%d)\n", fn.Name, time.Since(before), i+1, len(c.Fns)))
 	}
-	l.Warn().Msgf("all services have successfully gracefully exited\n")
+	lines = append(lines, "all services have successfully gracefully exited")
+	if time.Since(start) > c.TimeLimit {
+		for _, line := range lines {
+			l.Warn().Msg(line)
+		}
+		return fmt.Errorf("cleanup exceeded time limit of %d seconds", c.TimeLimit)
+	}
 	return nil
 }
