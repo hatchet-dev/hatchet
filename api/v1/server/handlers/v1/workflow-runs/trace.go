16e7ddac508f23de6c9a66d98298d5931e2ecf33
mrkaye97 <mrkaye97@gmail.com>
2026-03-20 14:29:22 -0400

    fix: api wiring

--- api/v1/server/handlers/v1/workflow-runs/trace.go
+++ api/v1/server/handlers/v1/workflow-runs/trace.go
@@ -1,6 +1,7 @@
 package workflowruns
 
 import (
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
@@ -14,6 +15,18 @@ func (t *V1WorkflowRunsService) V1WorkflowRunGetTrace(ctx echo.Context, request 
 	}
 
 	tenant := ctx.Get("tenant").(*sqlcv1.Tenant)
+
+	tasks, err := t.config.V1.Tasks().FlattenExternalIds(ctx.Request().Context(), tenant.ID, []uuid.UUID{request.Params.RunExternalId})
+
+	isDag := len(tasks) > 1
+
+	var taskRunExternalID, workflowRunExternalId *uuid.UUID
+
+	if isDag {
+		workflowRunExternalId = &request.Params.RunExternalId
+	} else {
+		taskRunExternalID = &request.Params.RunExternalId
+	}
 
 	limit := int64(1000)
 	offset := int64(0)
@@ -34,7 +47,7 @@ func (t *V1WorkflowRunsService) V1WorkflowRunGetTrace(ctx echo.Context, request 
 		offset = 0
 	}
 
-	result, err := t.config.V1.OTelCollector().ListSpansByRunExternalID(ctx.Request().Context(), tenant.ID, &request.Params.RunExternalId, nil, offset, limit)
+	result, err := t.config.V1.OTelCollector().ListSpansByRunExternalID(ctx.Request().Context(), tenant.ID, taskRunExternalID, workflowRunExternalId, offset, limit)
 	if err != nil {
 		return nil, err
 	}
