d32eab2c755039cb7c393b53feb3be88ae49ab9f
mrkaye97 <mrkaye97@gmail.com>
2026-04-15 16:36:02 -0400

    fix: nil handling

--- api/v1/server/handlers/v1/webhooks/create.go
+++ api/v1/server/handlers/v1/webhooks/create.go
@@ -130,7 +130,7 @@ func (w *V1WebhooksService) constructCreateOpts(tenantId uuid.UUID, request gen.
 		params.Name = basicAuth.Name
 		params.Eventkeyexpression = basicAuth.EventKeyExpression
 		params.ScopeExpression = basicAuth.ScopeExpression
-		params.ReturnEventAsResponsePayload = basicAuth.ReturnEventAsResponsePayload != nil && *basicAuth.ReturnEventAsResponsePayload
+		params.ReturnEventAsResponsePayload = returnEventResponsePayloadOrDefault(basicAuth.ReturnEventAsResponsePayload)
 		if basicAuth.StaticPayload != nil {
 			payloadBytes, err := json.Marshal(basicAuth.StaticPayload)
 			if err != nil {
@@ -166,7 +166,7 @@ func (w *V1WebhooksService) constructCreateOpts(tenantId uuid.UUID, request gen.
 		params.Name = apiKeyAuth.Name
 		params.Eventkeyexpression = apiKeyAuth.EventKeyExpression
 		params.ScopeExpression = apiKeyAuth.ScopeExpression
-		params.ReturnEventAsResponsePayload = apiKeyAuth.ReturnEventAsResponsePayload != nil && *apiKeyAuth.ReturnEventAsResponsePayload
+		params.ReturnEventAsResponsePayload = returnEventResponsePayloadOrDefault(apiKeyAuth.ReturnEventAsResponsePayload)
 		if apiKeyAuth.StaticPayload != nil {
 			payloadBytes, err := json.Marshal(apiKeyAuth.StaticPayload)
 			if err != nil {
@@ -202,7 +202,7 @@ func (w *V1WebhooksService) constructCreateOpts(tenantId uuid.UUID, request gen.
 		params.Name = hmacAuth.Name
 		params.Eventkeyexpression = hmacAuth.EventKeyExpression
 		params.ScopeExpression = hmacAuth.ScopeExpression
-		params.ReturnEventAsResponsePayload = hmacAuth.ReturnEventAsResponsePayload != nil && *hmacAuth.ReturnEventAsResponsePayload
+		params.ReturnEventAsResponsePayload = returnEventResponsePayloadOrDefault(hmacAuth.ReturnEventAsResponsePayload)
 		if hmacAuth.StaticPayload != nil {
 			payloadBytes, err := json.Marshal(hmacAuth.StaticPayload)
 			if err != nil {
@@ -216,4 +216,12 @@ func (w *V1WebhooksService) constructCreateOpts(tenantId uuid.UUID, request gen.
 	}
 
 	return params, nil
+}
+
+func returnEventResponsePayloadOrDefault(requestReturnEventAsResponsePayload *bool) bool {
+	if requestReturnEventAsResponsePayload == nil {
+		return true
+	}
+
+	return *requestReturnEventAsResponsePayload
 }
