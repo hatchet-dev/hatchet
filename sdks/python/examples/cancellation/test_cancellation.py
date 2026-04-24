3b7ceafde9418a56a051e3a201a02beb202b71cf
matt <mrkaye97@gmail.com>
2026-04-24 10:35:54 -0400

    Fix: Python cancellation test (#3700)
    * fix: cancellation test
    
    * chore: lint

--- sdks/python/examples/cancellation/test_cancellation.py
+++ sdks/python/examples/cancellation/test_cancellation.py
@@ -3,26 +3,21 @@ import asyncio
 import pytest
 
 from examples.cancellation.worker import cancellation_workflow
-from hatchet_sdk import Hatchet
-from hatchet_sdk.clients.rest.models.v1_task_status import V1TaskStatus
+from hatchet_sdk import Hatchet, RunStatus
 
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_cancellation(hatchet: Hatchet) -> None:
     ref = await cancellation_workflow.aio_run(wait_for_result=False)
 
-    """Sleep for a long time since we only need cancellation to happen _eventually_"""
-    await asyncio.sleep(10)
+    for _ in range(30):
+        run = await hatchet.runs.aio_get_details(ref.workflow_run_id)
 
-    for i in range(30):
-        run = await hatchet.runs.aio_get(ref.workflow_run_id)
-
-        if run.run.status == V1TaskStatus.RUNNING:
+        if run.status in [RunStatus.RUNNING, RunStatus.QUEUED]:
             await asyncio.sleep(1)
             continue
 
-        assert run.run.status == V1TaskStatus.CANCELLED
-        assert not run.run.output
+        assert run.status == RunStatus.CANCELLED
 
         break
     else:
