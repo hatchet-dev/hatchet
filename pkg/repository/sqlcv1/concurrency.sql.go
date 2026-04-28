7963e62a8384c6dc73575cd9676ba52e62f57611
mrkaye97 <mrkaye97@gmail.com>
2026-04-28 19:11:51 -0400

    fix: tenant id filter

--- pkg/repository/sqlcv1/concurrency.sql.go
+++ pkg/repository/sqlcv1/concurrency.sql.go
@@ -117,7 +117,9 @@ WITH tenant_step_concurrencies AS (
         AND sc.is_active = TRUE
         AND NOT EXISTS (
             SELECT 1 FROM v1_concurrency_slot cs
-            WHERE cs.strategy_id = sc.id
+            WHERE
+                cs.strategy_id = sc.id
+                AND tenant_id = $1::UUID -- tenant id filter to force index usage
         )
         AND NOT EXISTS (
             SELECT 1 FROM v1_concurrency_slot cs
