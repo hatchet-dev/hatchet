24ed07a0837a9de29f4ce7dadac0030111e6225a
jishnundth <jishnun789@gmail.com>
2026-05-08 18:38:21 +0000

    fix: go sdk api response status code validation

--- sdks/go/features/utils.go
+++ sdks/go/features/utils.go
@@ -12,8 +12,8 @@ func validateJSON200Response[T any](statusCode int, body []byte, json200 *T) err
 }
 
 func validateStatusCodeResponse(statusCode int, body []byte) error {
-	if statusCode != 200 {
-		return errors.Newf("received non-200 response from server. got status %d with body '%s'", statusCode, string(body))
+	if statusCode < 200 || statusCode >= 300 {
+		return errors.Newf("received non-2xx response from server. got status %d with body '%s'", statusCode, string(body))
 	}
 	return nil
 }
