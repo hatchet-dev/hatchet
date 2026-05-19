aea2b00bb85d633be90d57bbe73085210f571858
mrkaye97 <mrkaye97@gmail.com>
2026-05-19 12:30:33 -0400

    chore: lint

--- sdks/python/examples/bug_tests/durable_child_key_duplicate_child/test_durable_child_key_duped_child.py
+++ sdks/python/examples/bug_tests/durable_child_key_duplicate_child/test_durable_child_key_duped_child.py
@@ -18,9 +18,9 @@ async def test_durable_child_key_duplicate_bug_all_duped(hatchet: Hatchet) -> No
 
     runs = await hatchet.runs.aio_list(parent_task_external_id=run_id)
 
-    assert len(runs.rows) == 1, (
-        "should only have one child since the `child_key` is set"
-    )
+    assert (
+        len(runs.rows) == 1
+    ), "should only have one child since the `child_key` is set"
 
     run = runs.rows[0]
 
@@ -38,18 +38,18 @@ async def test_durable_child_key_duplicate_bug_second_unique(hatchet: Hatchet) -
 
     runs = await hatchet.runs.aio_list(parent_task_external_id=run_id)
 
-    assert len(runs.rows) == 2, (
-        "should only have one child since the `child_key` is set"
-    )
+    assert (
+        len(runs.rows) == 2
+    ), "should only have one child since the `child_key` is set"
 
     first, second = runs.rows
 
     assert first.status == V1TaskStatus.COMPLETED
     assert second.status == V1TaskStatus.COMPLETED
 
-    assert first.workflow_run_external_id != second.workflow_run_external_id, (
-        "second should be different than first"
-    )
+    assert (
+        first.workflow_run_external_id != second.workflow_run_external_id
+    ), "second should be different than first"
 
 
 @pytest.mark.asyncio(loop_scope="session")
@@ -63,15 +63,15 @@ async def test_durable_child_key_duplicate_bug_third_unique(hatchet: Hatchet) ->
 
     runs = await hatchet.runs.aio_list(parent_task_external_id=run_id)
 
-    assert len(runs.rows) == 2, (
-        "should only have two children since only the third `child_key` is unique"
-    )
+    assert (
+        len(runs.rows) == 2
+    ), "should only have two children since only the third `child_key` is unique"
 
     first, second = runs.rows
 
     assert first.status == V1TaskStatus.COMPLETED
     assert second.status == V1TaskStatus.COMPLETED
 
-    assert first.workflow_run_external_id != second.workflow_run_external_id, (
-        "second should be different than first"
-    )
+    assert (
+        first.workflow_run_external_id != second.workflow_run_external_id
+    ), "second should be different than first"
