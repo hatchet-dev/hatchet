397e2bc66c5415fc74a8d573aed6821f5baffb6b
mrkaye97 <mrkaye97@gmail.com>
2026-05-18 20:59:50 -0400

    feat: expand tests

--- sdks/python/examples/bug_tests/durable_child_key_duplicate_child/worker.py
+++ sdks/python/examples/bug_tests/durable_child_key_duplicate_child/worker.py
@@ -1,7 +1,13 @@
-# > Simple
 from hatchet_sdk import Context, DurableContext, EmptyModel, Hatchet
+from pydantic import BaseModel
+from typing import Literal
+from uuid import uuid4
 
 hatchet = Hatchet()
+
+
+class Input(BaseModel):
+    scenario: Literal["third_unique", "second_unique", "all_duped"]
 
 
 @hatchet.task()
@@ -9,13 +15,19 @@ async def child_child_key_bug(_i: EmptyModel, ctx: Context) -> dict[str, str]:
     return {"id": ctx.workflow_run_id}
 
 
-@hatchet.durable_task()
+@hatchet.durable_task(input_validator=Input)
 async def durable_parent_child_key_bug(
-    _i: EmptyModel, ctx: DurableContext
+    input: Input, ctx: DurableContext
 ) -> dict[str, str]:
-    for _ in range(2):
-        await child_child_key_bug.aio_run(
-            child_key=ctx.workflow_run_id + f"-child",
-        )
+    for i in range(1, 4):
+        child_key = ctx.workflow_run_id + f"-child"
+
+        if input.scenario == "third_unique" and i == 3:
+            child_key += str(uuid4())
+
+        if input.scenario == "second_unique" and i == 2:
+            child_key += str(uuid4())
+
+        await child_child_key_bug.aio_run(child_key=child_key)
 
     return {"result": "Hello, world!"}
