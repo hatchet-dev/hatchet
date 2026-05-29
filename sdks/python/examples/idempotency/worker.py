5b432bbaf884d96cdf46a34396ec4f2a7ca11e0e
mrkaye97 <mrkaye97@gmail.com>
2026-05-29 14:59:32 -0400

    feat: new example

---
+++ sdks/python/examples/idempotency/worker.py
@@ -0,0 +1,29 @@
+from hatchet_sdk import Context, Hatchet, IdempotencyConfig
+from datetime import timedelta
+from pydantic import BaseModel
+
+hatchet = Hatchet()
+
+
+class IdempotencyInput(BaseModel):
+    id: int
+
+
+@hatchet.task(
+    idempotency=IdempotencyConfig(key_expression="input.id", ttl=timedelta(minutes=1)),
+    input_validator=IdempotencyInput,
+)
+async def idempotent_task(input: IdempotencyInput, ctx: Context) -> dict[str, str]:
+    return {"result": f"Hello, world from task {input.id}"}
+
+
+def main() -> None:
+    worker = hatchet.worker(
+        "test-worker",
+        workflows=[idempotent_task],
+    )
+    worker.start()
+
+
+if __name__ == "__main__":
+    main()
