e6d1f252552935828c7e746fe27311b7651084cc
Julius Park <Juliusgeo@gmail.com>
2026-05-30 10:46:07 -0400

    fix: Use leases to protect concurrent partition creation (un-revert) (#4051)
    * fix: Use leases to protect concurrent partition creation (#4044)
    
    * remove migration
    
    * restore the right one
    
    * use if not exists

--- cmd/hatchet-migrate/migrate/migrations/20260529140000_v1_0_115.sql
+++ cmd/hatchet-migrate/migrate/migrations/20260529140000_v1_0_115.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TYPE "LeaseKind" ADD VALUE 'TABLE_PARTITION_MAINTENANCE';
+ALTER TYPE "LeaseKind" ADD VALUE IF NOT EXISTS 'TABLE_PARTITION_MAINTENANCE';
 -- +goose StatementEnd
 
 -- +goose Down
