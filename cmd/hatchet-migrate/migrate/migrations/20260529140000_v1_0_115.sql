20e66ac2175b1b29696cb58fac659bf7c46a494d
Julius Park <juliusgeo@gmail.com>
2026-05-30 10:23:23 -0400

    remove migration

--- cmd/hatchet-migrate/migrate/migrations/20260529140000_v1_0_115.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TYPE "LeaseKind" ADD VALUE 'TABLE_PARTITION_MAINTENANCE';
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
--- +goose StatementEnd
