12ae9613f7ee3ad43b92f7750254cdc0dcc276e3
mrkaye97 <mrkaye97@gmail.com>
2026-06-02 13:37:47 -0400

    feat: wire up scheduled trigger now api

--- api/v1/server/handlers/workflows/trigger_scheduled.go
+++ api/v1/server/handlers/workflows/trigger_scheduled.go
@@ -1,11 +1,14 @@
 package workflows
 
 import (
+	"time"
+
 	"github.com/labstack/echo/v4"
 
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/apierrors"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/gen"
 	"github.com/hatchet-dev/hatchet/api/v1/server/oas/transformers"
+	"github.com/hatchet-dev/hatchet/pkg/repository"
 	"github.com/hatchet-dev/hatchet/pkg/repository/sqlcv1"
 )
 
@@ -14,6 +17,19 @@ func (t *WorkflowService) WorkflowScheduledTrigger(ctx echo.Context, request gen
 
 	if scheduled == nil {
 		return gen.WorkflowScheduledTrigger404JSONResponse(apierrors.NewAPIErrors("Scheduled workflow not found.")), nil
+	}
+
+	err := t.config.V1.Ticker().RunScheduledWorkflow(ctx.Request().Context(), scheduled.TenantId, repository.RunScheduledWorkflowV1Opts{
+		Id:                 scheduled.ID,
+		Input:              scheduled.Input,
+		AdditionalMetadata: scheduled.AdditionalMetadata,
+		Priority:           &scheduled.Priority,
+		TriggerAt:          time.Now().UTC(),
+		WorkflowName:       scheduled.Name,
+	})
+
+	if err != nil {
+		return gen.WorkflowScheduledTrigger400JSONResponse(apierrors.NewAPIErrors("Failed to trigger scheduled workflow.")), nil
 	}
 
 	return gen.WorkflowScheduledTrigger200JSONResponse(
