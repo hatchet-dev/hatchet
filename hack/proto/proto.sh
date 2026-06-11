d045154f9ba2dc7c58f35bd1401ade18d6f824ec
Gabe Ruttner <gabriel.ruttner@gmail.com>
2026-06-11 14:20:35 -0400

    feat: add connect and generate

--- hack/proto/proto.sh
+++ hack/proto/proto.sh
@@ -4,6 +4,7 @@
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+go install connectrpc.com/connect/cmd/protoc-gen-connect-go@v1.20.0
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 
@@ -28,6 +29,20 @@ protoc --proto_path=api-contracts \
     --go-grpc_out=./internal/services/shared/proto/v1 \
     --go-grpc_opt=module=github.com/hatchet-dev/hatchet/internal/services/shared/proto/v1 \
     v1/workflows.proto
+
+protoc --proto_path=api-contracts \
+    --go_out=./internal/services/shared/proto/v2 \
+    --go_opt=module=github.com/hatchet-dev/hatchet/internal/services/shared/proto/v2 \
+    --connect-go_out=./internal/services/shared/proto/v2 \
+    --connect-go_opt=module=github.com/hatchet-dev/hatchet/internal/services/shared/proto/v2 \
+    --connect-go_opt=simple=true \
+    v2/shared/conditions.proto \
+    v2/shared/task_config.proto \
+    v2/task.proto \
+    v2/worker.proto \
+    v2/durable_task.proto \
+    v2/events.proto \
+    v2/logs.proto
 
 protoc --proto_path=api-contracts/dispatcher --go_out=./internal/services/dispatcher/contracts --go_opt=paths=source_relative \
     --go-grpc_out=./internal/services/dispatcher/contracts --go-grpc_opt=paths=source_relative \
