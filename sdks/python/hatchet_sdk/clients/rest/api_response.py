e74794a8995d825c4c72839177d56b7ece4f4c56
mrkaye97 <mrkaye97@gmail.com>
2026-06-12 15:37:25 -0400

    chore: don't lint api_response

--- sdks/python/hatchet_sdk/clients/rest/api_response.py
+++ sdks/python/hatchet_sdk/clients/rest/api_response.py
@@ -1,13 +1,8 @@
 """API response object."""
 
 from __future__ import annotations
-
-from typing import TYPE_CHECKING, Generic, TypeVar
-
-from pydantic import BaseModel, Field, StrictBytes, StrictInt
-
-if TYPE_CHECKING:
-    from collections.abc import Mapping
+from typing import Optional, Generic, Mapping, TypeVar
+from pydantic import Field, StrictInt, StrictBytes, BaseModel
 
 T = TypeVar("T")
 
@@ -18,7 +13,7 @@ class ApiResponse(BaseModel, Generic[T]):
     """
 
     status_code: StrictInt = Field(description="HTTP status code")
-    headers: Mapping[str, str] | None = Field(None, description="HTTP headers")
+    headers: Optional[Mapping[str, str]] = Field(None, description="HTTP headers")
     data: T = Field(description="Deserialized data given the data type")
     raw_data: StrictBytes = Field(description="Raw data (HTTP response body)")
 
