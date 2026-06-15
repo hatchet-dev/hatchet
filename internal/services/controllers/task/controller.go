dbfcf8db6320f0430faa5b8ba235088faffca914
mrkaye97 <mrkaye97@gmail.com>
2026-06-15 14:22:27 -0400

    chore: lint

--- internal/services/controllers/task/controller.go
+++ internal/services/controllers/task/controller.go
@@ -42,29 +42,29 @@ type TasksController interface {
 }
 
 type TasksControllerImpl struct {
-	mq                                    msgqueue.MessageQueue
-	pubBuffer                             *msgqueue.MQPubBuffer
-	l                                     *zerolog.Logger
-	queueLogger                           *zerolog.Logger
-	pgxStatsLogger                        *zerolog.Logger
-	repov1                                v1.Repository
-	dv                                    datautils.DataDecoderValidator
-	s                                     gocron.Scheduler
-	a                                     *hatcheterrors.Wrapped
-	p                                     *partition.Partition
-	celParser                             *cel.CELParser
-	opsPoolPollInterval                   time.Duration
-	opsPoolJitter                         time.Duration
-	timeoutTaskOperations                 *operation.TenantOperationPool
-	reassignTaskOperations                *operation.TenantOperationPool
-	retryTaskOperations                   *operation.TenantOperationPool
-	emitSleepOperations                   *operation.TenantOperationPool
-	evictExpiredIdempotencyKeysOperations         *operation.TenantOperationPool
-	deactivateStaleStepConcurrencyOperations      *operation.TenantOperationPool
-	replayEnabled                         bool
-	analyzeCronInterval                   time.Duration
-	signaler                              *signal.OLAPSignaler
-	tw                                    *trigger.TriggerWriter
+	mq                                       msgqueue.MessageQueue
+	pubBuffer                                *msgqueue.MQPubBuffer
+	l                                        *zerolog.Logger
+	queueLogger                              *zerolog.Logger
+	pgxStatsLogger                           *zerolog.Logger
+	repov1                                   v1.Repository
+	dv                                       datautils.DataDecoderValidator
+	s                                        gocron.Scheduler
+	a                                        *hatcheterrors.Wrapped
+	p                                        *partition.Partition
+	celParser                                *cel.CELParser
+	opsPoolPollInterval                      time.Duration
+	opsPoolJitter                            time.Duration
+	timeoutTaskOperations                    *operation.TenantOperationPool
+	reassignTaskOperations                   *operation.TenantOperationPool
+	retryTaskOperations                      *operation.TenantOperationPool
+	emitSleepOperations                      *operation.TenantOperationPool
+	evictExpiredIdempotencyKeysOperations    *operation.TenantOperationPool
+	deactivateStaleStepConcurrencyOperations *operation.TenantOperationPool
+	replayEnabled                            bool
+	analyzeCronInterval                      time.Duration
+	signaler                                 *signal.OLAPSignaler
+	tw                                       *trigger.TriggerWriter
 }
 
 type TasksControllerOpt func(*TasksControllerOpts)
