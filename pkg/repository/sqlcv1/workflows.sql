7c3686011aba4cb7c291060dc658919bd1f6cbb6
Mohammed Nafees <hello@mnafees.me>
2026-06-23 08:36:19 +0200

    make sure we use the partial index when creating scheduled and cron refs

--- pkg/repository/sqlcv1/workflows.sql
+++ pkg/repository/sqlcv1/workflows.sql
@@ -657,6 +657,7 @@ WHERE "id" = @cronTriggerId::uuid
 WITH latest_version AS (
     SELECT "id" FROM "WorkflowVersion"
     WHERE "workflowId" = @workflowId::uuid
+        AND "deletedAt" IS NULL
     ORDER BY "order" DESC
     LIMIT 1
 ),
