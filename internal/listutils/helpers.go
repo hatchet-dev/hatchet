223b231075cbf74664aaf06284cc75de777e6e77
mrkaye97 <mrkaye97@gmail.com>
2026-06-25 14:24:50 -0400

    feat: add UniqBy, add elixir doc links

--- internal/listutils/helpers.go
+++ internal/listutils/helpers.go
@@ -2,6 +2,7 @@ package listutils
 
 import "slices"
 
+// inspiration: uniq/1 https://elixir.hexdocs.pm/Enum.html#uniq/1
 func Uniq[T comparable](xs []T) []T {
 	seen := make(map[T]struct{})
 	result := make([]T, 0)
@@ -9,6 +10,22 @@ func Uniq[T comparable](xs []T) []T {
 	for _, x := range xs {
 		if _, ok := seen[x]; !ok {
 			seen[x] = struct{}{}
+			result = append(result, x)
+		}
+	}
+
+	return result
+}
+
+// inspiration: uniq_by/2 https://elixir.hexdocs.pm/Enum.html#uniq_by/2
+func UniqBy[T comparable, K comparable](xs []T, fn func(x T) K) []T {
+	seen := make(map[K]struct{})
+	result := make([]T, 0)
+
+	for _, x := range xs {
+		cmp := fn(x)
+		if _, ok := seen[cmp]; !ok {
+			seen[cmp] = struct{}{}
 			result = append(result, x)
 		}
 	}
@@ -29,6 +46,7 @@ func All[T comparable](xs []T, target T) bool {
 	return true
 }
 
+// inspiration: max/3 https://elixir.hexdocs.pm/Enum.html#max/3
 func MaxOf[T any](xs []T, fn func(T) int) int {
 	m := 0
 	for _, x := range xs {
