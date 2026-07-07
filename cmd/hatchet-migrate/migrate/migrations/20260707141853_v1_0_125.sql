094be55e1637100e8a8bbebe3947a4132c268160
mrkaye97 <mrkaye97@gmail.com>
2026-07-07 12:24:41 -0400

    chore: lint migration

--- cmd/hatchet-migrate/migrate/migrations/20260707141853_v1_0_125.sql
+++ cmd/hatchet-migrate/migrate/migrations/20260707141853_v1_0_125.sql
@@ -1,8 +1,7 @@
 -- +goose Up
 -- +goose NO TRANSACTION
-
 CREATE INDEX CONCURRENTLY IF NOT EXISTS ix_user_session_cre_at_exp_at ON "UserSession" ("createdAt") INCLUDE ("expiresAt");
+
 -- +goose Down
 -- +goose NO TRANSACTION
-
 DROP INDEX CONCURRENTLY IF EXISTS ix_user_session_cre_at_exp_at;
