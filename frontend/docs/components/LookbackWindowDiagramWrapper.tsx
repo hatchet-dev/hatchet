02dd1bee1963b6b3720175680a1e446db4e99c73
Alexander Belanger <alexander@hatchet.run>
2026-07-07 09:23:01 -0400

    fix: pr review

--- frontend/docs/components/LookbackWindowDiagramWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const LookbackWindowDiagram = dynamic(() => import("./LookbackWindowDiagram"), {
-  ssr: false,
-});
-
-export default LookbackWindowDiagram;
--- frontend/docs/components/TaskEvictionDiagramWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const TaskEvictionDiagram = dynamic(() => import("./TaskEvictionDiagram"), {
-  ssr: false,
-});
-
-export default TaskEvictionDiagram;
