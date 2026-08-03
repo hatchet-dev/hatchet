b7b49ff26b14c2cd1de33ebf8d36ddec46812de8
mrkaye97 <mrkaye97@gmail.com>
2026-08-03 16:05:58 -0400

    feat: add eventkeys helpers + enum

---
+++ cmd/hatchet-loadtest/eventkeys/eventkeys.go
@@ -0,0 +1,29 @@
+package eventkeys
+
+import "slices"
+
+type EventKey string
+
+const (
+	EventKeyDefault EventKey = "load-test:event"
+	EventKeyBatch   EventKey = "load-test:batch-event"
+	EventKeyDurable EventKey = "load-test:durable-event"
+)
+
+var All = []EventKey{EventKeyDefault, EventKeyBatch, EventKeyDurable}
+
+func IsKnown(key EventKey) bool {
+	return slices.Contains(All, key)
+}
+
+func (k EventKey) String() string {
+	return string(k)
+}
+
+func Strings(keys []EventKey) []string {
+	out := make([]string, len(keys))
+	for i, k := range keys {
+		out[i] = string(k)
+	}
+	return out
+}
