6d0b4307ad3c22a92d39f17e8910ef4b31753796
Mohammed Nafees <hello@mnafees.me>
2026-08-05 14:31:12 +0200

    regen

--- sdks/typescript/src/clients/rest/generated/data-contracts.ts
+++ sdks/typescript/src/clients/rest/generated/data-contracts.ts
@@ -1321,6 +1321,11 @@ export interface APIMeta {
    * @example "eyJhbGciOiJFUzI1NiIs..."
    */
   authDisabledToken?: string;
+  /**
+   * whether this instance is running in embedded mode
+   * @example false
+   */
+  embedded?: boolean;
 }
 
 export interface APIMetaIntegration {
