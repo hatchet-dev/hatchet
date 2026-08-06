0730ded1846b29e515ccb999298fd7106015d826
Mohammed Nafees <hello@mnafees.me>
2026-08-06 10:24:25 +0200

    head prepend the base path code lines

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -33,12 +33,12 @@ export default defineConfig({
               {
                 tag: 'base',
                 attrs: { href: '{{ .BasePath }}' },
-                injectTo: 'head',
+                injectTo: 'head-prepend',
               },
               {
                 tag: 'script',
                 children: 'window.__CONFIG__ = { BASE_PATH: "{{ .BasePath }}" };',
-                injectTo: 'head',
+                injectTo: 'head-prepend',
               },
             ],
           };
