231907a27ef7c186cf810dc5e63ddde8572a569b
mrkaye97 <mrkaye97@gmail.com>
2026-08-12 15:10:20 -0400

    fix: expand the set of errors that would cause us to swallow heartbeat miss errors

--- sdks/python/hatchet_sdk/clients/dispatcher/action_listener.py
+++ sdks/python/hatchet_sdk/clients/dispatcher/action_listener.py
@@ -112,10 +112,12 @@ class ActionListener:
                 self.missed_heartbeats = self.missed_heartbeats + 1
                 self.last_heartbeat_succeeded = False
 
-                if (
-                    e.code() == grpc.StatusCode.UNAVAILABLE
-                    or e.code() == grpc.StatusCode.FAILED_PRECONDITION
-                ):
+                if e.code() in [
+                    grpc.StatusCode.UNAVAILABLE,
+                    grpc.StatusCode.FAILED_PRECONDITION,
+                    grpc.StatusCode.CANCELLED,
+                    grpc.StatusCode.DEADLINE_EXCEEDED,
+                ]:
                     # todo case on "recvmsg:Connection reset by peer" for updates?
                     if self.missed_heartbeats >= 3:
                         # we don't reraise the error here, as we don't want to stop the heartbeat thread
