dc8e8bd09762f5fa51d14d8ec16706644d1af5f3
Mohammed Nafees <hello@mnafees.me>
2026-08-17 13:03:03 +0200

    chore: update quickstart CLI functionality (#4693)
    * update quickstart func
    
    * comments
    
    * bump quickstarts tag

--- cmd/hatchet-cli/cli/internal/templater/templater.go
+++ cmd/hatchet-cli/cli/internal/templater/templater.go
@@ -14,6 +14,7 @@ import (
 type Data struct {
 	Name           string
 	PackageManager string
+	HatchetVersion string
 }
 
 // Process reads all files from the specified directory within the provided filesystem,
