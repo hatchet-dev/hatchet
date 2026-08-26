5ba0c9fcd70c68c77daaf7e4165c04515a02909d
mrkaye97 <mrkaye97@gmail.com>
2026-08-25 20:57:01 -0600

    chore: add todo comment on migration

--- cmd/hatchet-migrate/migrate/migrations/20260825203952_v1_0_148.sql
+++ cmd/hatchet-migrate/migrate/migrations/20260825203952_v1_0_148.sql
@@ -1,5 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
+
+-- todo: make sure that adding this here is safe (and doesn't bork updates on either side of the migration)
 ALTER TABLE v1_dags_olap
     ADD COLUMN latest_retry_count INT NOT NULL DEFAULT 0;
 -- +goose StatementEnd
