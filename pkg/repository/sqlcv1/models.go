7b46fb65ed61bb2abeb76f0f4ada5a579e1ed81c
matt <mrkaye97@gmail.com>
2026-09-18 12:37:33 -0400

    fix(engine): add `is_dag_operator` column to `v1_dags_olap` (#4984)
    * feat: add is_dag_operator column to v1_dags_olap
    
    * feat: wire up writes
    
    * fix: exclude operators correctly
    
    * fix: thanks greptile

--- pkg/repository/sqlcv1/models.go
+++ pkg/repository/sqlcv1/models.go
@@ -3337,6 +3337,7 @@ type V1DagsOlap struct {
 	TotalTasks           int32                `json:"total_tasks"`
 	IdempotencyKey       pgtype.Text          `json:"idempotency_key"`
 	LatestRetryCount     int32                `json:"latest_retry_count"`
+	IsDagOperator        bool                 `json:"is_dag_operator"`
 }
 
 type V1DurableEventLogBranchPoint struct {
